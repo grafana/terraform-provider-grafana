90cb3626e20b07c38589639eb1e55c32f41afef3
anGie44 <angelinepinilla@gmail.com>
2020-06-08 00:11:34 -0400

    add playlist resource

---
+++ grafana/resource_playlist_test.go
@@ -0,0 +1,220 @@
+package grafana
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"testing"
+
+	gapi "github.com/nytm/go-grafana-api"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccPlaylist_basic(t *testing.T) {
+	var playlist gapi.Playlist
+	rn := "grafana_playlist.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccPlaylistDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPlaylistConfigBasic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccPlaylistCheckExists(rn, &playlist),
+					resource.TestMatchResourceAttr(rn, "id", regexp.MustCompile(`\d+`)),
+					resource.TestCheckResourceAttr(rn, "name", rName),
+					resource.TestCheckResourceAttr(rn, "item.#", "2"),
+					resource.TestCheckResourceAttr(rn, "item.3264172724.order", "1"),
+					resource.TestCheckResourceAttr(rn, "item.3264172724.title", "Terraform Dashboard By Tag"),
+					resource.TestCheckResourceAttr(rn, "item.3536342863.order", "2"),
+					resource.TestCheckResourceAttr(rn, "item.3536342863.title", "Terraform Dashboard By ID"),
+				),
+			},
+			{
+				ResourceName:      rn,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccPlaylist_update(t *testing.T) {
+	var playlist gapi.Playlist
+	rn := "grafana_playlist.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+	updatedName := "updated name"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccPlaylistDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPlaylistConfigBasic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccPlaylistCheckExists(rn, &playlist),
+				),
+			},
+			{
+				Config: testAccPlaylistConfigUpdate(updatedName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccPlaylistCheckExists(rn, &playlist),
+					resource.TestMatchResourceAttr(rn, "id", regexp.MustCompile(`\d+`)),
+					resource.TestCheckResourceAttr(rn, "name", updatedName),
+					resource.TestCheckResourceAttr(rn, "item.#", "1"),
+					resource.TestMatchResourceAttr(rn, "id", regexp.MustCompile(`\d+`)),
+					resource.TestCheckResourceAttr(rn, "item.1966873348.order", "1"),
+					resource.TestCheckResourceAttr(rn, "item.1966873348.title", "Terraform Dashboard By ID"),
+					resource.TestCheckResourceAttr(rn, "item.1966873348.type", "dashboard_by_id"),
+					resource.TestCheckResourceAttr(rn, "item.1966873348.value", "3"),
+				),
+			},
+			{
+				ResourceName:      rn,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccPlaylist_disappears(t *testing.T) {
+	var playlist gapi.Playlist
+	rn := "grafana_playlist.test"
+	rName := acctest.RandomWithPrefix("tf-acc-test")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccPlaylistDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPlaylistConfigBasic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccPlaylistCheckExists(rn, &playlist),
+					testAccPlaylistDisappears(rn),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func testAccPlaylistCheckExists(rn string, playlist *gapi.Playlist) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[rn]
+		if !ok {
+			return fmt.Errorf("resource not found: %s\n %#v", rn, s.RootModule().Resources)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("resource id not set")
+		}
+
+		client := testAccProvider.Meta().(*gapi.Client)
+		id, err := strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		gotPlaylist, err := client.Playlist(id)
+		if err != nil {
+			return fmt.Errorf("error getting playlist: %s", err)
+		}
+
+		*playlist = *gotPlaylist
+
+		return nil
+	}
+}
+
+func testAccPlaylistDisappears(rn string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[rn]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", rn)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("resource id not set")
+		}
+
+		client := testAccProvider.Meta().(*gapi.Client)
+		id, err := strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		err = client.DeletePlaylist(id)
+		return err
+	}
+}
+
+func testAccPlaylistDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*gapi.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "grafana_playlist" {
+			continue
+		}
+
+		id, err := strconv.Atoi(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		gotPlaylist, err := client.Playlist(id)
+		if err != nil {
+			if err.Error() == "404 Not Found" {
+				return nil
+			}
+			return err
+		}
+		if gotPlaylist != nil {
+			return fmt.Errorf("Playlist still exists")
+		}
+	}
+	return nil
+}
+
+func testAccPlaylistConfigBasic(name string) string {
+	return fmt.Sprintf(`
+resource "grafana_playlist" "test" {
+	name = "%s"
+	interval = "5m"
+	
+	item {
+		order = 1
+		title = "Terraform Dashboard By Tag"
+	}
+
+	item {
+		order = 2
+		title = "Terraform Dashboard By ID"
+	}
+}
+`, name)
+}
+
+func testAccPlaylistConfigUpdate(name string) string {
+	return fmt.Sprintf(`
+resource "grafana_playlist" "test" {
+	name = "%s"
+	interval = "5m"
+	
+	item {
+		order = 1
+		title = "Terraform Dashboard By ID"
+		type = "dashboard_by_id"
+		value = "3"
+	}
+}
+`, name)
+}
---
+++ website/docs/r/playlist.html.md
@@ -0,0 +1,66 @@
+---
+layout: "grafana"
+page_title: "Grafana: grafana_playlist"
+sidebar_current: "docs-grafana-resource-playlist"
+description: |-
+  The grafana_playlist resource allows a Grafana playlist to be created.
+---
+
+# grafana\_playlist
+
+The playlist resource allows a playlist to be created on a Grafana server.
+
+## Example Usage
+
+```hcl
+resource "grafana_playlist" "playlist" {
+	name = "my playlist"
+	interval = "5m"
+	
+    item {
+		order = 1
+		title = "Terraform Dashboard by Tag"
+        type = "dashboard_by_tag"
+		value = "myTag"
+	}
+
+    item {
+        order = 2
+        title = "Terraform Dashbord By ID"
+		type = "dashboard_by_id"
+		value = "myTag"
+	}
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required, Forces new resource) The name of the playlist.
+* `interval` - (Required) The amount of time for Grafana to stay on a particular dashboard before advancing to the next one on the playlist (e.g `5m`, `1h`)
+* `item` - (Required) The Grafana dashboard(s) to add to the playlist.
+
+`item` supports the following arguments:
+
+* `order` - (Required) The order number in which the dashboard appears in the playlist (e.g. `1`, `2`)
+
+* `title` - (Required) The name of the existing dashboard.
+
+* `type` - (Optional) The description of the existing dashboard.
+
+* `value` - (Optional) A tag associated with the dashboard.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - The playlist ID.
+
+## Import
+
+Existing playlists can be imported using the `id` e.g.
+
+```
+$ terraform import grafana_playlist.playlist 123
+```
