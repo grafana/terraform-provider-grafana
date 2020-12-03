2872ccebb6a0cf5fa84c71e319f8046eaa653756
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2020-12-03 13:13:31 -0500

    Update vendoring

--- vendor/github.com/grafana/grafana-api-golang-client/client.go
+++ vendor/github.com/grafana/grafana-api-golang-client/client.go
@@ -11,6 +11,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"strconv"
 
 	"github.com/hashicorp/go-cleanhttp"
 )
@@ -30,6 +31,8 @@ type Config struct {
 	BasicAuth *url.Userinfo
 	// Client provides an optional HTTP client, otherwise a default will be used.
 	Client *http.Client
+	// OrgID provides an optional organization ID, ignored when using APIKey, BasicAuth defaults to last used org
+	OrgID int64
 }
 
 // New creates a new Grafana client.
@@ -103,6 +106,8 @@ func (c *Client) newRequest(method, requestPath string, query url.Values, body i
 
 	if c.config.APIKey != "" {
 		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.config.APIKey))
+	} else if c.config.OrgID != 0 {
+		req.Header.Add("X-Grafana-Org-Id", strconv.FormatInt(c.config.OrgID, 10))
 	}
 
 	if os.Getenv("GF_LOG") != "" {
