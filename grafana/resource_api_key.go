orendain/terraform-provider-grafanaauth

Add provider, resource and tests.

internal/provider/resource_api_key.go
@@ -1,0 +1,118 @@
+package provider
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	gapi "github.com/orendain/grafana-api-golang-client"
+)
+
+func resourceApiKey() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceApiKeyCreate,
+		ReadContext:   resourceApiKeyRead,
+		DeleteContext: resourceApiKeyDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"role": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"seconds_to_live": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"key": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+			"expiration": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceApiKeyCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	name := d.Get("name").(string)
+	role := d.Get("role").(string)
+	ttl := d.Get("seconds_to_live").(int)
+
+	c := m.(*gapi.Client)
+	request := gapi.CreateApiKeyRequest{Name: name, Role: role, SecondsToLive: int64(ttl)}
+	response, err := c.CreateApiKey(request)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("key", response.Key)
+
+	// Fill the true resource's state after a create by performing a read.
+	resourceApiKeyRead(ctx, d, m)
+
+	return diags
+}
+
+func resourceApiKeyRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	c := m.(*gapi.Client)
+	response, err := c.GetApiKeys(true)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	name := d.Get("name").(string)
+	for _, key := range response {
+		if name == key.Name {
+			d.SetId(strconv.FormatInt(key.Id, 10))
+			d.Set("name", key.Name)
+			d.Set("role", key.Role)
+
+			if !key.Expiration.IsZero() {
+				d.Set("expiration", key.Expiration.String())
+			}
+
+			return diags
+		}
+	}
+
+	// Resource was not found via the client. Have Terraform destroy it.
+	d.SetId("")
+
+	return diags
+}
+
+func resourceApiKeyDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	id, err := strconv.ParseInt(d.Id(), 10, 32)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	c := m.(*gapi.Client)
+	_, err = c.DeleteApiKey(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diags
+}

internal/provider/resource_api_key_test.go
@@ -1,0 +1,107 @@
+package provider
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	gapi "github.com/orendain/grafana-api-golang-client"
+)
+
+const testAccGrafanaAuthKeyBasicConfig = `
+resource "grafanaauth_api_key" "foo" {
+	name = "foo-name"
+	role = "Admin"
+}
+`
+
+const testAccGrafanaAuthKeyExpandedConfig = `
+resource "grafanaauth_api_key" "bar" {
+	name 			= "bar-name"
+	role 			= "Viewer"
+	seconds_to_live = 300
+}
+`
+
+func TestAccGrafanaAuthKey(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccGrafanaAuthKeyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGrafanaAuthKeyBasicConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccGrafanaAuthKeyCheckFields("grafanaauth_api_key.foo", "foo-name", "Admin", false),
+				),
+			},
+			{
+				Config: testAccGrafanaAuthKeyExpandedConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccGrafanaAuthKeyCheckFields("grafanaauth_api_key.bar", "bar-name", "Viewer", true),
+				),
+			},
+		},
+	})
+}
+
+func testAccGrafanaAuthKeyDestroy(s *terraform.State) error {
+	c := testAccProvider.Meta().(*gapi.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "grafanaauth_api_key" {
+			continue
+		}
+
+		idStr := rs.Primary.ID
+		id, err := strconv.ParseInt(idStr, 10, 32)
+		if err != nil {
+			return err
+		}
+
+		_, err = c.DeleteApiKey(id)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func testAccGrafanaAuthKeyCheckFields(n string, name string, role string, expires bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		if rs.Primary.Attributes["key"] == "" {
+			return fmt.Errorf("no API key is set")
+		}
+
+		if rs.Primary.Attributes["name"] != name {
+			return fmt.Errorf("incorrect name field found: %s", rs.Primary.Attributes["name"])
+		}
+
+		if rs.Primary.Attributes["role"] != role {
+			return fmt.Errorf("incorrect role field found: %s", rs.Primary.Attributes["role"])
+		}
+
+		expiration := rs.Primary.Attributes["expiration"]
+		if expires && expiration == "" {
+			return fmt.Errorf("no expiration date set")
+		}
+
+		if !expires && expiration != "" {
+			return fmt.Errorf("expiration date set")
+		}
+
+		return nil
+	}
+}

grafana/terraform-provider-grafana

grafana/resource_api_key.go
@@ -1,0 +1,118 @@
+package grafana
+
+import (
+	"context"
+	"strconv"
+
+	gapi "github.com/grafana/grafana-api-golang-client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceApiKey() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceAPIKeyCreate,
+		ReadContext:   resourceAPIKeyRead,
+		DeleteContext: resourceAPIKeyDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"role": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"seconds_to_live": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"key": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+			"expiration": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAPIKeyCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	name := d.Get("name").(string)
+	role := d.Get("role").(string)
+	ttl := d.Get("seconds_to_live").(int)
+
+	c := m.(*client).gapi
+	request := gapi.CreateAPIKeyRequest{Name: name, Role: role, SecondsToLive: int64(ttl)}
+	response, err := c.CreateAPIKey(request)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("key", response.Key)
+
+	// Fill the true resource's state after a create by performing a read.
+	resourceAPIKeyRead(ctx, d, m)
+
+	return diags
+}
+
+func resourceAPIKeyRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	c := m.(*client).gapi
+	response, err := c.GetAPIKeys(true)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	name := d.Get("name").(string)
+	for _, key := range response {
+		if name == key.Name {
+			d.SetId(strconv.FormatInt(key.ID, 10))
+			d.Set("name", key.Name)
+			d.Set("role", key.Role)
+
+			if !key.Expiration.IsZero() {
+				d.Set("expiration", key.Expiration.String())
+			}
+
+			return diags
+		}
+	}
+
+	// Resource was not found via the client. Have Terraform destroy it.
+	d.SetId("")
+
+	return diags
+}
+
+func resourceAPIKeyDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	id, err := strconv.ParseInt(d.Id(), 10, 32)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	c := m.(*client).gapi
+	_, err = c.DeleteAPIKey(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diags
+}

grafana/resource_api_key_test.go
@@ -1,0 +1,106 @@
+package grafana
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+const testAccGrafanaAuthKeyBasicConfig = `
+resource "grafana_api_key" "foo" {
+	name = "foo-name"
+	role = "Admin"
+}
+`
+
+const testAccGrafanaAuthKeyExpandedConfig = `
+resource "grafana_api_key" "bar" {
+	name 			= "bar-name"
+	role 			= "Viewer"
+	seconds_to_live = 300
+}
+`
+
+func TestAccGrafanaAuthKey(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccGrafanaAuthKeyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGrafanaAuthKeyBasicConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccGrafanaAuthKeyCheckFields("grafana_api_key.foo", "foo-name", "Admin", false),
+				),
+			},
+			{
+				Config: testAccGrafanaAuthKeyExpandedConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccGrafanaAuthKeyCheckFields("grafana_api_key.bar", "bar-name", "Viewer", true),
+				),
+			},
+		},
+	})
+}
+
+func testAccGrafanaAuthKeyDestroy(s *terraform.State) error {
+	c := testAccProvider.Meta().(*client).gapi
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "grafana_api_key" {
+			continue
+		}
+
+		idStr := rs.Primary.ID
+		id, err := strconv.ParseInt(idStr, 10, 32)
+		if err != nil {
+			return err
+		}
+
+		_, err = c.DeleteAPIKey(id)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func testAccGrafanaAuthKeyCheckFields(n string, name string, role string, expires bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		if rs.Primary.Attributes["key"] == "" {
+			return fmt.Errorf("no API key is set")
+		}
+
+		if rs.Primary.Attributes["name"] != name {
+			return fmt.Errorf("incorrect name field found: %s", rs.Primary.Attributes["name"])
+		}
+
+		if rs.Primary.Attributes["role"] != role {
+			return fmt.Errorf("incorrect role field found: %s", rs.Primary.Attributes["role"])
+		}
+
+		expiration := rs.Primary.Attributes["expiration"]
+		if expires && expiration == "" {
+			return fmt.Errorf("no expiration date set")
+		}
+
+		if !expires && expiration != "" {
+			return fmt.Errorf("expiration date set")
+		}
+
+		return nil
+	}
+}