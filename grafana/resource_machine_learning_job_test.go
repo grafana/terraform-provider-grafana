15733d5975fdb1583eaf890ed627178dcc34edbb
Chris Marchbanks <csmarchbanks@gmail.com>
2022-01-06 12:14:52 -0700

    Add checks for server ML resources
    Add tests to make sure the server has a Job, and make sure that after
    deletion the server no longer has a Job.

--- grafana/resource_machine_learning_job_test.go
+++ grafana/resource_machine_learning_job_test.go
@@ -1,22 +1,29 @@
 package grafana
 
 import (
+	"context"
+	"fmt"
 	"regexp"
 	"testing"
 
+	"github.com/grafana/machine-learning-go-client/mlapi"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccResourceMachineLearningJob(t *testing.T) {
 	CheckCloudTestsEnabled(t)
 
+	var job mlapi.Job
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheckCloud(t) },
 		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccMLJobCheckDestroy(&job),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccExample(t, "resources/grafana_machine_learning_job/job.tf"),
 				Check: resource.ComposeTestCheckFunc(
+					testAccMLJobCheckExists("grafana_machine_learning_job.test_job", &job),
 					resource.TestCheckResourceAttrSet("grafana_machine_learning_job.test_job", "id"),
 					resource.TestCheckResourceAttr("grafana_machine_learning_job.test_job", "name", "Test Job"),
 					resource.TestCheckResourceAttr("grafana_machine_learning_job.test_job", "metric", "tf_test_job"),
@@ -44,6 +51,45 @@ func TestAccResourceMachineLearningJob(t *testing.T) {
 			},
 		},
 	})
+}
+
+func testAccMLJobCheckExists(rn string, job *mlapi.Job) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[rn]
+		if !ok {
+			return fmt.Errorf("resource not found: %s\n %#v", rn, s.RootModule().Resources)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("resource id not set")
+		}
+
+		client := testAccProvider.Meta().(*client).mlapi
+		gotJob, err := client.Job(context.Background(), rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("error getting job: %s", err)
+		}
+
+		*job = gotJob
+
+		return nil
+	}
+}
+
+func testAccMLJobCheckDestroy(job *mlapi.Job) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		// This check is to make sure that no pointer conversions are incorrect
+		// while mutating job.
+		if job.ID == "" {
+			return fmt.Errorf("checking deletion of empty id")
+		}
+		client := testAccProvider.Meta().(*client).mlapi
+		_, err := client.Job(context.Background(), job.ID)
+		if err == nil {
+			return fmt.Errorf("job still exists on server")
+		}
+		return nil
+	}
 }
 
 const machineLearningJobInvalid = `
