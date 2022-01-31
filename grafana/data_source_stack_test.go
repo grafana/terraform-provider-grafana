957965e96c467e1e9f1f7d4506d64b0cd6e2e440
Nasir Amin <nas.amin@snyk.io>
2022-01-31 16:04:08 +0000

    Fixed stack data source tests

--- grafana/data_source_stack_test.go
+++ grafana/data_source_stack_test.go
@@ -1,38 +1,45 @@
 package grafana
 
 import (
+	"fmt"
 	"testing"
 
 	gapi "github.com/grafana/grafana-api-golang-client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccDatasourceStack(t *testing.T) {
-	CheckCloudTestsEnabled(t)
-
+func TestAccDataSourceStack_Basic(t *testing.T) {
+	resourceName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
 	var stack gapi.Stack
-	checks := []resource.TestCheckFunc{
-		testAccStackCheckExists("grafana_cloud_stack.test", &stack),
-		resource.TestCheckResourceAttr(
-			"data.grafana_cloud_stack.test", "sllug", "test-slug",
-		),
-		resource.TestCheckResourceAttr(
-			"data.grafana_cloud_stack.test", "name", "test-stack",
-		),
-		resource.TestMatchResourceAttr(
-			"data.grafana_cloud_stack.test", "id", idRegexp,
-		),
-	}
-
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
+		PreCheck:          func() { testAccPreCheckCloudStack(t) },
 		ProviderFactories: testAccProviderFactories,
 		CheckDestroy:      testAccStackCheckDestroy(&stack),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccExample(t, "data-sources/grafana_cloud_stack/data-source.tf"),
-				Check:  resource.ComposeTestCheckFunc(checks...),
+				Config: testAccDataSourceStackConfig(resourceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.grafanacloud_stack.test", "id"),
+					resource.TestCheckResourceAttr("data.grafanacloud_stack.test", "name", resourceName),
+					resource.TestCheckResourceAttr("data.grafanacloud_stack.test", "slug", resourceName),
+					resource.TestCheckResourceAttrSet("data.grafanacloud_stack.test", "prometheus_url"),
+					resource.TestCheckResourceAttrSet("data.grafanacloud_stack.test", "prometheus_user_id"),
+					resource.TestCheckResourceAttrSet("data.grafanacloud_stack.test", "alertmanager_user_id"),
+				),
 			},
 		},
 	})
+}
+
+func testAccDataSourceStackConfig(resourceName string) string {
+	return fmt.Sprintf(`
+resource "grafana_cloud_stack" "test" {
+  name = "%s"
+  slug = "%s"
+}
+data "grafana_cloud_stack" "test" {
+  slug = grafana_cloud_stack.test.slug
+}
+`, resourceName, resourceName)
 }
