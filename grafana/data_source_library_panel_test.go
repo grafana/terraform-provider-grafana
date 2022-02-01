d10422dc9c68517b250b435f4656af8770254552
Justin Mai <justin.mai@drillinginfo.com>
2022-02-01 01:40:43 -0800

    add testAccLibraryPanelCheckExists and others

--- grafana/data_source_library_panel_test.go
+++ grafana/data_source_library_panel_test.go
@@ -1,10 +1,12 @@
 package grafana
 
 import (
+	"fmt"
 	"testing"
 
 	gapi "github.com/grafana/grafana-api-golang-client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccDatasourceLibraryPanelFromName(t *testing.T) {
@@ -65,4 +67,73 @@ func TestAccDatasourceLibraryPanelFromUID(t *testing.T) {
 			},
 		},
 	})
+}
+
+func testAccLibraryPanelCheckExists(rn string, panel *gapi.LibraryPanel) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[rn]
+		if !ok {
+			return fmt.Errorf("resource not found: %s", rn)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("resource id not set")
+		}
+		client := testAccProvider.Meta().(*client).gapi
+		gotLibraryPanel, err := client.LibraryPanelByUID(rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("error getting panel: %s", err)
+		}
+		*panel = *gotLibraryPanel
+		return nil
+	}
+}
+
+func testAccLibraryPanelCheckExistsInFolder(panel *gapi.LibraryPanel, folder *gapi.Folder) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if panel.Folder != folder.ID && folder.ID != 0 {
+			return fmt.Errorf("panel.Folder(%d) does not match folder.ID(%d)", panel.Folder, folder.ID)
+		}
+		return nil
+	}
+}
+
+func testAccLibraryPanelCheckDestroy(panel *gapi.LibraryPanel) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*client).gapi
+		_, err := client.LibraryPanelByUID(panel.UID)
+		if err == nil {
+			return fmt.Errorf("panel still exists")
+		}
+		return nil
+	}
+}
+
+func testAccLibraryPanelFolderCheckDestroy(panel *gapi.LibraryPanel, folder *gapi.Folder) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*client).gapi
+		_, err := client.LibraryPanelByUID(panel.UID)
+		if err == nil {
+			return fmt.Errorf("panel still exists")
+		}
+		folder, err = client.Folder(folder.ID)
+		if err == nil {
+			return fmt.Errorf("the following folder still exists: %s", folder.Title)
+		}
+		return nil
+	}
+}
+
+func testAccLibraryPanelDashboardCheckDestroy(panel *gapi.LibraryPanel, dashboard *gapi.Dashboard) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*client).gapi
+		_, err := client.LibraryPanelByUID(panel.UID)
+		if err == nil {
+			return fmt.Errorf("panel still exists")
+		}
+		dashboard, err = client.DashboardByUID(dashboard.Model["uid"].(string))
+		if err == nil {
+			return fmt.Errorf("the following dashboard still exists: %s", dashboard.Model["title"].(string))
+		}
+		return nil
+	}
 }
