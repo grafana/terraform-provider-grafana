651b9831bfab6239c8aceb3ed1c2b0665e84bf4b
Justin Mai <justin.mai@drillinginfo.com>
2022-02-01 02:47:09 -0800

    Revert "add debug info for error"
    This reverts commit 42cbc03582fe9d48dc7e4b04857bf58591ee2777.

--- grafana/resource_dashboard.go
+++ grafana/resource_dashboard.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"strconv"
 	"strings"
 
@@ -264,7 +263,6 @@ func makeDashboard(d *schema.ResourceData) gapi.Dashboard {
 		Message:   d.Get("message").(string),
 	}
 	configJSON := d.Get("config_json").(string)
-	log.Printf("[INFO] configJSON: %#v", configJSON)
 	dashboardJSON, err := unmarshalDashboardConfigJSON(configJSON)
 	if err != nil {
 		return dashboard
