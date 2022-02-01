524ee0d9dce8144ce4bccbeb83db702b925fd129
JM <justintmai@gmail.com>
2022-02-01 10:00:25 -0800

    add data_source_dashboard (#359)
    * Add grafana_library_panel resource
    
    * fix grafana_library_panel resource
    
    * docs update grafana_library_panel
    
    * fix library panel tests
    
    * test type in library panel modelJSON
    
    * add data_source_dashboard
    
    * use Defaults and remove Computed
    
    * config_json use StateFunc ValidateFunc
    
    * ignore negative version check
    
    * re-use ReadDashboard from resource_dashboard.go
    
    * remove encoding/json
    
    * remove StateFunc from config_json
    
    * remove ValidateFunc
    
    * fix Schema add slug make folder_id -> folder
    
    * remove uid from config_json
    
    * retain version and dashboard_id by copying before normalizeJSON
    
    * update docs
    
    * fix lint typo
    
    * fix typo
    
    * add data_source_dashboard
    
    * reuse unmarshalLibraryPanelModelJSON
    
    Co-authored-by: Julien Duchesne <julienduchesne@live.com>
    
    * fix docs for created/updated
    
    * use client v0.2.5
    
    * uid Optional: true
    
    * remove commented connected_dashboards
    
    * return nil or diag in DeleteLibraryPanel
    
    * skip v7 tests with CheckOSSTestsSemver()
    
    Co-authored-by: Julien Duchesne <julienduchesne@live.com>
    
    * fix go.sum
    
    * update docs
    
    * fix CheckOSSTestsSemver
    
    * add example and test for new dashboard with library panel
    
    * fix typo
    
    * add debug info for error
    
    * debug failing test
    
    * fix typo
    
    * use dashboard_id
    
    Co-authored-by: Julien Duchesne <julienduchesne@live.com>
    
    * fix typo
    
    * Revert "fix typo"
    
    This reverts commit ad67e77c08314326b19e799c3cfc31c1e7c14519.
    
    * Revert "fix typo"
    
    This reverts commit a6611a83b0e3bef30a07250f42e4785e4db4ea90.
    
    * Revert "debug failing test"
    
    This reverts commit ec424f8f8cb553759c5e991164c680c9402e78c9.
    
    * Revert "add debug info for error"
    
    This reverts commit 42cbc03582fe9d48dc7e4b04857bf58591ee2777.
    
    * Revert "fix typo"
    
    This reverts commit 67cf749ca6d5836779617d766d996d98e84c7241.
    
    * Revert "add example and test for new dashboard with library panel"
    
    This reverts commit b8a64bf82a021aac4c79bfcf360dcbf2321b73b7.
    
    * test for ExactlyOneOf error
    
    * add ExactlyOneOf to inputs
    
    * use Computed version
    
    * update docs
    
    * update docs
    
    * remove Computed: true for dashboard_id and uid
    
    * fix dashboard_id and version before reading dashboard resource
    
    * fix typo in tests
    
    * fix test ExpectError: regexp.MustCompile
    
    * remove test CheckDestroy for ExpectedError
    
    * go mod tidy
    
    Co-authored-by: Justin Mai <justin.mai@drillinginfo.com>
    Co-authored-by: Julien Duchesne <julienduchesne@live.com>

---
+++ grafana/data_source_dashboard.go
@@ -0,0 +1,125 @@
+package grafana
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	gapi "github.com/grafana/grafana-api-golang-client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func DatasourceDashboard() *schema.Resource {
+	return &schema.Resource{
+		Description: `
+* [Official documentation](https://grafana.com/docs/grafana/latest/dashboards/)
+* [Folder/Dashboard Search HTTP API](https://grafana.com/docs/grafana/latest/http_api/folder_dashboard_search/)
+* [Dashboard HTTP API](https://grafana.com/docs/grafana/latest/http_api/dashboard/)
+`,
+		ReadContext: dataSourceDashboardRead,
+		Schema: map[string]*schema.Schema{
+			"dashboard_id": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      -1,
+				ExactlyOneOf: []string{"dashboard_id", "uid"},
+				Description:  "The numerical ID of the Grafana dashboard. Specify either this or `uid`.",
+			},
+			"uid": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "",
+				ExactlyOneOf: []string{"dashboard_id", "uid"},
+				Description:  "The uid of the Grafana dashboard. Specify either this or `dashboard_id`.",
+			},
+			"config_json": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The complete dashboard model JSON.",
+			},
+			"version": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "The numerical version of the Grafana dashboard.",
+			},
+			"title": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The title of the Grafana dashboard.",
+			},
+			"folder": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "The numerical ID of the folder where the Grafana dashboard is found.",
+			},
+			"is_starred": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Whether or not the Grafana dashboard is starred. Starred Dashboards will show up on your own Home Dashboard by default, and are a convenient way to mark Dashboards that you’re interested in.",
+			},
+			"slug": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "URL slug of the dashboard (deprecated).",
+			},
+		},
+	}
+}
+
+// search dashboards by ID
+func findDashboardWithID(client *gapi.Client, id int64) (*gapi.FolderDashboardSearchResponse, error) {
+	params := map[string]string{
+		"type":         "dash-db",
+		"dashboardIds": strconv.FormatInt(id, 10),
+	}
+	resp, err := client.FolderDashboardSearch(params)
+	if err != nil {
+		return nil, err
+	}
+	for _, d := range resp {
+		if int64(d.ID) == id {
+			return &d, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no dashboard with id %d", id)
+}
+
+func dataSourceDashboardRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var dashboard *gapi.Dashboard
+	client := meta.(*client).gapi
+
+	// get UID from ID if specified
+	id := d.Get("dashboard_id").(int)
+	uid := d.Get("uid").(string)
+	if id > 0 {
+		res, err := findDashboardWithID(client, int64(id))
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		uid = res.UID
+	}
+
+	dashboard, err := client.DashboardByUID(uid)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(uid)
+	d.Set("uid", dashboard.Model["uid"].(string))
+	d.Set("dashboard_id", int64(dashboard.Model["id"].(float64)))
+	configJSONBytes, err := json.Marshal(dashboard.Model)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.Set("config_json", string(configJSONBytes))
+	d.Set("version", int64(dashboard.Model["version"].(float64)))
+	d.Set("title", dashboard.Model["title"].(string))
+	d.Set("folder", dashboard.Folder)
+	d.Set("is_starred", dashboard.Meta.IsStarred)
+	d.Set("slug", dashboard.Meta.Slug)
+
+	return nil
+}
