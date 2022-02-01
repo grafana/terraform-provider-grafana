b8a64bf82a021aac4c79bfcf360dcbf2321b73b7
Justin Mai <justin.mai@drillinginfo.com>
2022-02-01 01:36:50 -0800

    add example and test for new dashboard with library panel

---
+++ examples/resources/grafana_library_panel/_acc_dashboard.tf
@@ -0,0 +1,26 @@
+resource "grafana_library_panel" "test_dashboard" {
+  name       = "test name"
+  model_json = jsonencode({
+    gridPos  = {
+      h      = 8,
+      w      = 12 },
+    id       = 1
+  })
+}
+
+# make a dashboard wth a library panel
+resource "grafana_dashboard" "test" {
+  message          = "inital commit."
+  config_json      = jsonencode({
+    id             = 12345,
+    panels         = [ merge(jsondecode(grafana_library_panel.test.model_json), {
+      libraryPanel = {
+        uid        = grafana_library_panel.test.id } }) ]
+    title          = "Production Overview",
+    tags           = [ "templated" ],
+    timezone       = "browser",
+    schemaVersion  = 16,
+    version        = 0,
+    refresh        = "25s"
+  })
+}
