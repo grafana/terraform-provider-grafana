b3f03733a0ba28fb79fb4787fd35c0d1abcdabb5
Justin Mai <justin.mai@drillinginfo.com>
2022-02-01 01:25:56 -0800

    add example dashboard containing library panel

--- examples/data-sources/grafana_library_panel/data-source.tf
+++ examples/data-sources/grafana_library_panel/data-source.tf
@@ -1,6 +1,11 @@
 resource "grafana_library_panel" "test" {
-  name = "test name"
-  model_json = jsonencode({})
+  name       = "test name"
+  model_json = jsonencode({
+    gridPos  = {
+      h      = 8,
+      w      = 12 },
+    id       = 1
+  })
 }
 
 data "grafana_library_panel" "from_name" {
@@ -9,4 +14,23 @@ data "grafana_library_panel" "from_name" {
 
 data "grafana_library_panel" "from_uid" {
   uid = grafana_library_panel.test.id
+}
+
+
+# make a dashboard wth a library panel
+
+resource "grafana_dashboard" "test" {
+  message          = "inital commit."
+  config_json      = jsonencode({
+    id             = 12345,
+    panels         = [ merge(jsondecode(grafana_library_panel.test.model_json), {
+      libraryPanel = {
+        uid        = grafana_library_panel.test.id } }) ]
+    title          = "Production Overview",
+    tags           = [ "templated" ],
+    timezone       = "browser",
+    schemaVersion  = 16,
+    version        = 0,
+    refresh        = "25s"
+  })
 }
