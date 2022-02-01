81e060ff9d564b492774c94c4bf643bbf2cec445
Justin Mai <justin.mai@drillinginfo.com>
2022-02-01 04:26:45 -0800

    fix test v8.0/v8.1 Error: Provider produced inconsistent final plan

--- examples/data-sources/grafana_library_panel/data-source.tf
+++ examples/data-sources/grafana_library_panel/data-source.tf
@@ -2,9 +2,11 @@ resource "grafana_library_panel" "test" {
   name       = "test name"
   model_json = jsonencode({
     gridPos  = {
-      h      = 8,
-      w      = 12 },
+      h      = 8
+      w      = 12 }
     id       = 1
+    # if not set, Grafana v8.0/v8.1 will error "inconsistent final plan" in dashboard resource
+    title    = "test name"
   })
 }
 
