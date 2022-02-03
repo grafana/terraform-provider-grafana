a959b10bcb7c64c267759fa3f4c7acd1fc514b36
Justin Mai <justin.mai@drillinginfo.com>
2022-02-02 20:15:06 -0800

    clean up test *.tf

--- examples/data-sources/grafana_library_panel/data-source.tf
+++ examples/data-sources/grafana_library_panel/data-source.tf
@@ -1,16 +1,3 @@
-//resource "grafana_library_panel" "test_datasource" {
-  //name       = "test name"
-  //folder_id  = 0
-  //model_json = jsonencode({
-    /* gridPos  = {
-      h      = 8
-      w      = 12 }
-    id       = 1
-    # if not set, Grafana v8.0/v8.1 will error "inconsistent final plan" in dashboard resource
-    title    = "test name" */
-  //})
-//}
-
 resource "grafana_library_panel" "test" {
   name        = "basic"
   folder_id   = 0
