4c86b858389ae55d6cdbc889dd7576a5b71fa240
Vardan Torosyan <vardants@gmail.com>
2022-02-04 13:18:18 +0100

    Update resource_builtin_role_assignment resource to ignore assignments defined in the server side (#369)
    When Grafana starts, by default it creates built-in role assignments for fixed roles. Additionally, users can create built-in role assignments via API. In these scenarios, when the resource is not properly imported, terraform will try to destroy what is in server side, but not in the configuration.
    
    The change ensures that this does not happen as it can cause unintended behaviour for users.

--- grafana/resource_builtin_role_assignment.go
+++ grafana/resource_builtin_role_assignment.go
@@ -102,8 +102,20 @@ func ReadBuiltInRole(ctx context.Context, d *schema.ResourceData, meta interface
 		return nil
 	}
 
+	stateRoles, configRoles, err := collectRoles(d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
 	roles := make([]interface{}, 0)
 	for _, br := range brRole {
+		// It is possible that in the server side there are roles assigned to the built-in role which were never in Terraform state,
+		// and are not in the current configuration. The following check ensures that we only consider roles which are either in the state or in the config.
+		// This prevents unintended behaviour, such as destroying the assignments which were never managed by Terraform.
+		_, isInState := stateRoles[br.UID]
+		_, isInConfig := configRoles[br.UID]
+		if !isInState && !isInConfig {
+			continue
+		}
 		rm := map[string]interface{}{
 			"uid":    br.UID,
 			"global": br.Global,
