718ff79442916c62d4028f4c748e9e729a19d9df
Justin Mai <justin.mai@drillinginfo.com>
2022-02-03 16:48:34 -0800

    initial commit

---
+++ grafana/data_source_dashboards_test.go
@@ -0,0 +1,36 @@
+package grafana
+
+import (
+	"testing"
+
+	gapi "github.com/grafana/grafana-api-golang-client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceDashboardsAll(t *testing.T) {
+	CheckCloudTestsEnabled(t)
+
+	var dashboard1 gapi.Dashboard
+	var dashboard2 gapi.Dashboard
+	var folder gapi.Folder
+
+	checks := []resource.TestCheckFunc{
+		testAccDashboardCheckExists("grafana_dashboard.test1", &dashboard1),
+		testAccDashboardCheckExists("grafana_dashboard.test2", &dashboard2),
+		testAccFolderCheckExists("grafana_folder.test", &folder),
+		// make sure only one dashboard when specifying folder
+		resource.TestCheckResourceAttr("data.grafana_dashboards.with_folder_id", "dashboards.%", "1"),
+		resource.TestCheckResourceAttr("data.grafana_dashboards.all", "dashboards.%", "2"),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccExample(t, "data-sources/grafana_dashboards/data-source.tf"),
+				Check:  resource.ComposeTestCheckFunc(checks...),
+			},
+		},
+	})
+}
