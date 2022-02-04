9310c940d6e176b2505a52b5c9dcca1d0ca13d02
Justin Mai <justin.mai@drillinginfo.com>
2022-02-04 03:55:25 -0800

    add code formatting to attribute descriptions

--- grafana/data_source_dashboards.go
+++ grafana/data_source_dashboards.go
@@ -23,19 +23,19 @@ Datasource for retrieving all dashboards. Specify list of folder IDs to search i
 				Type:        schema.TypeList,
 				Optional:    true,
 				Computed:    true,
-				Description: `Numerical IDs of Grafana folders containing dashboards. Specify to filter for dashboards by folder (eg. [0] for General folder), or leave blank to get all dashboards in all folders.`,
+				Description: "Numerical IDs of Grafana folders containing dashboards. Specify to filter for dashboards by folder (eg. `[0]` for General folder), or leave blank to get all dashboards in all folders.",
 				Elem:        &schema.Schema{Type: schema.TypeInt},
 			},
 			"tags": {
 				Type:        schema.TypeList,
 				Optional:    true,
-				Description: `List of string Grafana dashboard tags to search for, eg. ["prod"]. Used only as search input, i.e., attribute value will remain unchanged.`,
+				Description: "List of string Grafana dashboard tags to search for, eg. `[\"prod\"]`. Used only as search input, i.e., attribute value will remain unchanged.",
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
 			"dashboards": {
 				Type:        schema.TypeMap,
 				Computed:    true,
-				Description: "Map of Grafana dashboard unique identifiers (list of string UIDs as values) to folder IDs (integers as keys).",
+				Description: "Map of Grafana dashboard unique identifiers (list of string UIDs as values) to folder IDs (integers as keys), eg. `{0 = [\"cIBgcSjkk\"]}`.",
 				Elem: &schema.Schema{
 					Type:        schema.TypeList,
 					Description: "List of string dashboard UIDs.",
