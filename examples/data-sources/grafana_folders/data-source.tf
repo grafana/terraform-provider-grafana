16c2d9669921fcc2f0e03d11f4c834b23d785448
Justin Mai <justin.mai@drillinginfo.com>
2022-02-14 16:39:54 -0800

    fix tests

--- examples/data-sources/grafana_folders/data-source.tf
+++ examples/data-sources/grafana_folders/data-source.tf
@@ -12,3 +12,8 @@ data "grafana_folders" "all" {
 data "grafana_folders" "one" {
   limit = 1
 }
+
+// test to make sure folders search worked
+data "grafana_folder" "lookup" {
+  uid = data.grafana_folders.all.folders[0].uid
+}
