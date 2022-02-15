debbb15a8610d3c5d2adcd8b043e86197ca4dbfb
Justin Mai <justin.mai@drillinginfo.com>
2022-02-14 20:38:08 -0800

    fix search type for folders

--- grafana/data_source_folders.go
+++ grafana/data_source_folders.go
@@ -59,7 +59,7 @@ func dataSourceReadFolders(ctx context.Context, d *schema.ResourceData, meta int
 	var diags diag.Diagnostics
 	params := url.Values{
 		"limit": {fmt.Sprint(d.Get("limit"))},
-		"type":  {"folder-db"},
+		"type":  {"dash-folder"},
 	}
 
 	d.SetId(hashDashboardSearchParameters(params))
