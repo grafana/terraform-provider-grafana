ca2eb7f2eae537d58607e0547fcfa3ccd215a986
Justin Mai <justin.mai@drillinginfo.com>
2022-02-14 17:25:47 -0800

    use depends_on

--- examples/data-sources/grafana_folders/data-source.tf
+++ examples/data-sources/grafana_folders/data-source.tf
@@ -6,14 +6,23 @@ resource "grafana_folder" "data_source_folders2" {
   title = "data_source_folders2"
 }
 
+// wait for folder resources to be created before searching
 data "grafana_folders" "all" {
+  depends_on = [
+    grafana_folder.data_source_folders1,
+    grafana_folder.data_source_folders2,
+  ]
 }
 
 data "grafana_folders" "one" {
   limit = 1
+  depends_on = [
+    grafana_folder.data_source_folders1,
+    grafana_folder.data_source_folders2,
+  ]
 }
 
-// test to make sure it worked
+// test to make sure search worked
 data "grafana_folder" "test" {
   uid = data.grafana_folders.all.folders["data_source_folders1"]
 }
