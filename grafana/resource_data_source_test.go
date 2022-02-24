grafana/terraform-provider-grafana

commit bbb20e9a102e9a982b5c7df0d4a71a80c2049226

Fix json_data field types for opentsdb datasource (#398)
* Fix json_data field types for opentsdb datasource

`tsdb_resolution` and `tsdb_version` were declared as strings though
those fields are integers.

* Update client

* Generate docs

Co-authored-by: Yury Bushmelev <yuryb@spgroup.com.sg>

grafana/resource_data_source.go
@@ -318,12 +318,12 @@ source selected (via the 'type' argument).
 							Description: "(Stackdriver) The token URI used, provided in the service account key.",
 						},
 						"tsdb_resolution": {
-							Type:        schema.TypeString,
+							Type:        schema.TypeInt,
 							Optional:    true,
 							Description: "(OpenTSDB) Resolution.",
 						},
 						"tsdb_version": {
-							Type:        schema.TypeString,
+							Type:        schema.TypeInt,
 							Optional:    true,
 							Description: "(OpenTSDB) Version.",
 						},
@@ -622,8 +622,8 @@ func makeJSONData(d *schema.ResourceData) gapi.JSONData {
 		TLSAuthWithCACert:          d.Get("json_data.0.tls_auth_with_ca_cert").(bool),
 		TLSSkipVerify:              d.Get("json_data.0.tls_skip_verify").(bool),
 		TokenURI:                   d.Get("json_data.0.token_uri").(string),
-		TsdbResolution:             d.Get("json_data.0.tsdb_resolution").(string),
-		TsdbVersion:                d.Get("json_data.0.tsdb_version").(string),
+		TsdbResolution:             int64(d.Get("json_data.0.tsdb_resolution").(int)),
+		TsdbVersion:                int64(d.Get("json_data.0.tsdb_version").(int)),
 		Version:                    d.Get("json_data.0.version").(string),
 		Workgroup:                  d.Get("json_data.0.workgroup").(string),
 	}

go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/Masterminds/semver/v3 v3.1.1
-	github.com/grafana/grafana-api-golang-client v0.3.1
+	github.com/grafana/grafana-api-golang-client v0.4.0
 	github.com/grafana/machine-learning-go-client v0.1.1
 	github.com/grafana/synthetic-monitoring-agent v0.6.2
 	github.com/grafana/synthetic-monitoring-api-go-client v0.5.1

go.sum
@@ -455,8 +455,8 @@ github.com/gorilla/context v1.1.1/go.mod h1:kBGZzfjB9CEq2AlWe17Uuf7NDRt0dE0s8S51
 github.com/gorilla/mux v1.6.2/go.mod h1:1lud6UwP+6orDFRuTfBEV8e9/aOM/c4fVVCaMa2zaAs=
 github.com/gorilla/mux v1.7.3/go.mod h1:1lud6UwP+6orDFRuTfBEV8e9/aOM/c4fVVCaMa2zaAs=
 github.com/gorilla/websocket v0.0.0-20170926233335-4201258b820c/go.mod h1:E7qHFY5m1UJ88s3WnNqhKjPHQ0heANvMoAMk2YaljkQ=
-github.com/grafana/grafana-api-golang-client v0.3.1 h1:QrHv0RKrYmhVvDN3Pyr+KcoPWY5HG2FEJDz+6fh7y9Y=
-github.com/grafana/grafana-api-golang-client v0.3.1/go.mod h1:24W29gPe9yl0/3A9X624TPkAOR8DpHno490cPwnkv8E=
+github.com/grafana/grafana-api-golang-client v0.4.0 h1:lUaTh8G4vHtG3mKg4bkZrZsYvraWq8nTeKXWLAY+CoA=
+github.com/grafana/grafana-api-golang-client v0.4.0/go.mod h1:24W29gPe9yl0/3A9X624TPkAOR8DpHno490cPwnkv8E=
 github.com/grafana/machine-learning-go-client v0.1.1 h1:Gw6cX8xAd6IVF2LApkXOIdBK8Gzz07B3jQPukecw7fc=
 github.com/grafana/machine-learning-go-client v0.1.1/go.mod h1:QFfZz8NkqVF8++skjkKQXJEZfpCYd8S0yTWJUpsLLTA=
 github.com/grafana/synthetic-monitoring-agent v0.6.2 h1:JGTdlcu8y/ujwGQwTs1q9b1RLAQUr5oV1v4r7Cz5O9g=

commit b62ecac9e837f271cb5b9b043546d25ae4db0707

Migrate `opentsdb` string fields to int (#401)
This fixes a migration bug introduced by https://github.com/grafana/terraform-provider-grafana/pull/398
We unfortunately have to add a schema migration function to convert the strings to int
Since 1.20.0 is released, we cannot just go back to a string at this point, so a migration is the way forward

Fixes https://github.com/grafana/terraform-provider-grafana/issues/400
We should release 1.20.1 once that's merged

grafana/resource_data_source.go
@@ -26,10 +26,12 @@ The required arguments for this resource vary depending on the type of data
 source selected (via the 'type' argument).
 `,
 
-		CreateContext: CreateDataSource,
-		UpdateContext: UpdateDataSource,
-		DeleteContext: DeleteDataSource,
-		ReadContext:   ReadDataSource,
+		CreateContext:  CreateDataSource,
+		UpdateContext:  UpdateDataSource,
+		DeleteContext:  DeleteDataSource,
+		ReadContext:    ReadDataSource,
+		StateUpgraders: []schema.StateUpgrader{resourceDataSourceV0Upgrader},
+		SchemaVersion:  1,
 
 		// Import either by ID or UID
 		Importer: &schema.ResourceImporter{
@@ -703,4 +705,60 @@ func makeSecureJSONData(d *schema.ResourceData) gapi.SecureJSONData {
 		TLSClientCert:     d.Get("secure_json_data.0.tls_client_cert").(string),
 		TLSClientKey:      d.Get("secure_json_data.0.tls_client_key").(string),
 	}
+}
+
+// TSDB Version and Resolution used to be strings, but now are integers.
+func resourceDataSourceV0Schema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"json_data": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"tsdb_resolution": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "(OpenTSDB) Resolution.",
+						},
+						"tsdb_version": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "(OpenTSDB) Version.",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+var resourceDataSourceV0Upgrader = schema.StateUpgrader{
+	Version: 0,
+	Type:    resourceDataSourceV0Schema().CoreConfigSchema().ImpliedType(),
+	Upgrade: func(ctx context.Context, rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
+		convertToInt := func(m map[string]interface{}, key string) {
+			if value, hasValue := m[key]; hasValue {
+				m[key] = 0
+				if valueStr, ok := value.(string); ok {
+					if valueInt, err := strconv.Atoi(valueStr); err == nil {
+						m[key] = valueInt
+					}
+				}
+			}
+		}
+
+		var jsonData map[string]interface{}
+		if jsonDataList, ok := rawState["json_data"].([]interface{}); ok && len(jsonDataList) > 0 {
+			jsonData = jsonDataList[0].(map[string]interface{})
+		} else if jsonDataMap, ok := rawState["json_data"].(map[string]interface{}); ok {
+			jsonData = jsonDataMap
+		}
+		if jsonData != nil {
+			convertToInt(jsonData, "tsdb_version")
+			convertToInt(jsonData, "tsdb_resolution")
+		}
+
+		return rawState, nil
+	},
 }

grafana/resource_data_source_test.go
@@ -2,6 +2,7 @@ package grafana
 
 import (
 	"fmt"
+	"reflect"
 	"strconv"
 	"testing"
 
@@ -562,6 +563,133 @@ func TestAccDataSource_basic(t *testing.T) {
 					},
 				},
 			},
+		})
+	}
+}
+
+func TestDatasourceMigrationV0(t *testing.T) {
+	cases := []struct {
+		name     string
+		state    map[string]interface{}
+		expected map[string]interface{}
+	}{
+		{
+			name: "no json data",
+			state: map[string]interface{}{
+				"name": "test",
+				"type": "prometheus",
+			},
+			expected: map[string]interface{}{
+				"name": "test",
+				"type": "prometheus",
+			},
+		},
+		{
+			name: "no tsdb fields",
+			state: map[string]interface{}{
+				"name": "test",
+				"type": "prometheus",
+				"json_data": []map[string]interface{}{
+					{
+						"url": "http://localhost:9090",
+					},
+				},
+			},
+			expected: map[string]interface{}{
+				"name": "test",
+				"type": "prometheus",
+				"json_data": []map[string]interface{}{
+					{
+						"url": "http://localhost:9090",
+					},
+				},
+			},
+		},
+		{
+			name: "nil or empty tsdb fields",
+			state: map[string]interface{}{
+				"name": "test",
+				"type": "test",
+				"json_data": []map[string]interface{}{
+					{
+						"tsdb_version":    "",
+						"tsdb_resolution": nil,
+						"url":             "http://localhost:9090",
+					},
+				},
+			},
+			expected: map[string]interface{}{
+				"name": "test",
+				"type": "test",
+				"json_data": []map[string]interface{}{
+					{
+						"tsdb_version":    0,
+						"tsdb_resolution": 0,
+						"url":             "http://localhost:9090",
+					},
+				},
+			},
+		},
+		{
+			name: "already int",
+			state: map[string]interface{}{
+				"name": "test",
+				"type": "test",
+				"json_data": []map[string]interface{}{
+					{
+						"tsdb_version":    0,
+						"tsdb_resolution": 2,
+						"url":             "http://localhost:9090",
+					},
+				},
+			},
+			expected: map[string]interface{}{
+				"name": "test",
+				"type": "test",
+				"json_data": []map[string]interface{}{
+					{
+						"tsdb_version":    0,
+						"tsdb_resolution": 2,
+						"url":             "http://localhost:9090",
+					},
+				},
+			},
+		},
+		{
+			name: "migration",
+			state: map[string]interface{}{
+				"name": "test",
+				"type": "test",
+				"json_data": []map[string]interface{}{
+					{
+						"tsdb_version":    "0",
+						"tsdb_resolution": "2",
+						"url":             "http://localhost:9090",
+					},
+				},
+			},
+			expected: map[string]interface{}{
+				"name": "test",
+				"type": "test",
+				"json_data": []map[string]interface{}{
+					{
+						"tsdb_version":    0,
+						"tsdb_resolution": 2,
+						"url":             "http://localhost:9090",
+					},
+				},
+			},
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := resourceDataSourceV0Upgrader.Upgrade(nil, tc.expected, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("Expected %#v, got %#v", tc.expected, actual)
+			}
 		})
 	}
 }