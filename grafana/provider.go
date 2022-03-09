5c25203575953382707f610b06638e6ff903d43e
Julien Duchesne <julien.duchesne@grafana.com>
2022-03-09 09:47:39 -0500

    Support Cloud API keys (#425)
    * Support Cloud API keys
    Implements this API: https://grafana.com/docs/grafana-cloud/reference/cloud-api/#api-keys
    In our usecases, this will be used to bootstrap Synthetic Monitoring in a new stack (other PR to come)
    
    * lint
    
    * Add example and import
    
    * Fix last issues
    
    * go mod tidy

--- grafana/provider.go
+++ grafana/provider.go
@@ -164,7 +164,8 @@ func Provider(version string) func() *schema.Provider {
 				"grafana_user":                    ResourceUser(),
 
 				// Cloud
-				"grafana_cloud_stack": ResourceCloudStack(),
+				"grafana_cloud_api_key": ResourceCloudAPIKey(),
+				"grafana_cloud_stack":   ResourceCloudStack(),
 
 				// Synthetic Monitoring
 				"grafana_synthetic_monitoring_check": resourceSyntheticMonitoringCheck(),
