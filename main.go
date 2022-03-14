13b68d67f103340c3bdb5c122a5a2ce811e89c20
Julien Duchesne <julien.duchesne@grafana.com>
2022-03-14 18:43:02 -0400

    Fix debug mode

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"log"
 
 	"github.com/grafana/terraform-provider-grafana/grafana"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
@@ -23,15 +21,6 @@ func main() {
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	opts := &plugin.ServeOpts{ProviderFunc: grafana.Provider(version)}
-
-	if debugMode {
-		err := plugin.Debug(context.Background(), "registry.terraform.io/grafana/grafana", opts)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		return
-	}
-
+	opts := &plugin.ServeOpts{ProviderFunc: grafana.Provider(version), Debug: debugMode, ProviderAddr: "registry.terraform.io/grafana/grafana"}
 	plugin.Serve(opts)
 }
