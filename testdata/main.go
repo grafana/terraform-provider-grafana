cb199cd57f669444f6e5b61d178340a22a421d8d
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-05-13 17:24:37 -0300

    Reduce duplication by merging cert creation functions
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- testdata/main.go
+++ testdata/main.go
@@ -23,19 +23,17 @@ func main() {
 }
 
 func makeCerts() error {
-	now := time.Now()
-
-	ca, err := makeCA(now)
+	ca, err := makeCert(nil, "ca")
 	if err != nil {
 		return err
 	}
 
-	err = makeCert(ca, "grafana")
+	_, err = makeCert(ca, "grafana")
 	if err != nil {
 		return err
 	}
 
-	err = makeCert(ca, "client")
+	_, err = makeCert(ca, "client")
 	if err != nil {
 		return err
 	}
@@ -43,64 +41,58 @@ func makeCerts() error {
 	return nil
 }
 
-func makeCA(now time.Time) (*x509.Certificate, error) {
-	ca := &x509.Certificate{
-		BasicConstraintsValid: true,
-		Subject: pkix.Name{
-			Organization: []string{"Raintank, Inc."},
-		},
-		SerialNumber: big.NewInt(1024),
-		ExtKeyUsage: []x509.ExtKeyUsage{
-			x509.ExtKeyUsageClientAuth,
-			x509.ExtKeyUsageServerAuth,
-		},
-		KeyUsage:  x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		NotBefore: now,
-		NotAfter:  now.Add(1 * time.Hour),
-		IsCA:      true,
+func makeCert(ca *x509.Certificate, name string) (*x509.Certificate, error) {
+	var crt *x509.Certificate
+
+	if ca == nil {
+		now := time.Now()
+
+		ca = &x509.Certificate{
+			BasicConstraintsValid: true,
+			Subject: pkix.Name{
+				Organization: []string{"Raintank, Inc."},
+			},
+			SerialNumber: big.NewInt(1024),
+			ExtKeyUsage: []x509.ExtKeyUsage{
+				x509.ExtKeyUsageClientAuth,
+				x509.ExtKeyUsageServerAuth,
+			},
+			KeyUsage:  x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+			NotBefore: now,
+			NotAfter:  now.Add(1 * time.Hour),
+			IsCA:      true,
+		}
+
+		crt = ca
+	} else {
+		crt = &x509.Certificate{
+			BasicConstraintsValid: true,
+			Subject: pkix.Name{
+				Organization: []string{"Raintank, Inc."},
+			},
+			SerialNumber: big.NewInt(1024),
+			KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+			NotBefore:    ca.NotBefore,
+			NotAfter:     ca.NotAfter,
+			IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+			ExtKeyUsage: []x509.ExtKeyUsage{
+				x509.ExtKeyUsageClientAuth,
+				x509.ExtKeyUsageServerAuth,
+			},
+		}
 	}
 
 	pk, err := rsa.GenerateKey(rand.Reader, 4096)
 	if err != nil {
-		return nil, fmt.Errorf("cannot generate RSA key for CA: %w", err)
-	}
-
-	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &pk.PublicKey, pk)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create CA certificate: %w", err)
-	}
-
-	return ca, writeFiles(caBytes, pk, "ca")
-}
-
-func makeCert(ca *x509.Certificate, name string) error {
-	crt := &x509.Certificate{
-		BasicConstraintsValid: true,
-		Subject: pkix.Name{
-			Organization: []string{"Raintank, Inc."},
-		},
-		SerialNumber: big.NewInt(1024),
-		KeyUsage:     x509.KeyUsageDigitalSignature,
-		NotBefore:    ca.NotBefore,
-		NotAfter:     ca.NotAfter,
-		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
-		ExtKeyUsage: []x509.ExtKeyUsage{
-			x509.ExtKeyUsageClientAuth,
-			x509.ExtKeyUsageServerAuth,
-		},
-	}
-
-	pk, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return fmt.Errorf("cannot generate RSA key for certificate %s: %w", name, err)
+		return nil, fmt.Errorf("cannot generate RSA key for certificate %s: %w", name, err)
 	}
 
 	crtBytes, err := x509.CreateCertificate(rand.Reader, crt, ca, &pk.PublicKey, pk)
 	if err != nil {
-		return fmt.Errorf("cannot create certificate %s: %w", name, err)
+		return nil, fmt.Errorf("cannot create certificate %s: %w", name, err)
 	}
 
-	return writeFiles(crtBytes, pk, name)
+	return crt, writeFiles(crtBytes, pk, name)
 }
 
 func writeFiles(crtBytes []byte, pk *rsa.PrivateKey, name string) error {
