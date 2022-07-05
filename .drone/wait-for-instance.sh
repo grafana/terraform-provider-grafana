ef2814fc0c689361f068fdb7d09a7da5c289471a
Mike Ball <mikedball@gmail.com>
2022-07-05 06:03:29 -0400

    prevent globbing and word splitting
    Quoting variables prevents the script from breaking when input contains
    spaces, line feeds, or glob characters, and is regarded by [shellcheck](https://github.com/koalaman/shellcheck)
    as a best practice.
    
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- .drone/wait-for-instance.sh
+++ .drone/wait-for-instance.sh
@@ -3,17 +3,17 @@
 set -euxo pipefail
 
 getStatus() {
-  echo "$(curl -I -L -s -o /dev/null -w "%{http_code}" $1)"
+  echo "$(curl -I -L -s -o /dev/null -w "%{http_code}" "${1}")"
 }
 
-status=$(getStatus $1)
+status=$(getStatus "${1}")
 i=0
 while [ "${status}" != "200" ]; do
   if [ "${i}" -gt "30" ]; then
     echo "instance never became ready"
     exit 1
   fi
-  status=$(getStatus $1)
+  status=$(getStatus "${1}")
   i=$((i+1))
   sleep 2
 done
