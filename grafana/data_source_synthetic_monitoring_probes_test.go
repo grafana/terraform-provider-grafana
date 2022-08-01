a4aebba83d121306227b14c73c7badf2ba8a9e65
Julien Duchesne <julien.duchesne@grafana.com>
2022-08-01 11:24:12 -0400

    SM: Datasource with custom probe Debugging https://github.com/grafana/terraform-provider-grafana/issues/541

--- grafana/data_source_synthetic_monitoring_probes_test.go
+++ grafana/data_source_synthetic_monitoring_probes_test.go
@@ -21,6 +21,34 @@ func TestAccDataSourceSyntheticMonitoringProbes(t *testing.T) {
 				// We're not checking for deprecated probes here because there may not be any, causing tests to fail.
 				Check: resource.TestCheckResourceAttr("data.grafana_synthetic_monitoring_probes.main", "probes.Atlanta", "1"),
 			},
+			// Test with a custom probe
+			{
+				Config: testProbeDatasourceWithCustom,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.grafana_synthetic_monitoring_probes.all", "probes.Everest"),
+					resource.TestCheckResourceAttr("data.grafana_synthetic_monitoring_probe.one", "labels.type", "mountain"),
+				),
+			},
 		},
 	})
 }
+
+const testProbeDatasourceWithCustom = `
+resource "grafana_synthetic_monitoring_probe" "main" {
+	name      = "Everest"
+	latitude  = 27.98606
+	longitude = 86.92262
+	region    = "APAC"
+	labels = {
+	  type = "mountain"
+	}
+  }
+
+data "grafana_synthetic_monitoring_probes" "all" {
+	depends_on = [grafana_synthetic_monitoring_probe.main]
+}
+
+data "grafana_synthetic_monitoring_probe" "one" {
+	name = grafana_synthetic_monitoring_probe.main.name
+}
+  `
