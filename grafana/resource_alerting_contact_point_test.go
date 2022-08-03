afb7cec22e00046f49b7878459b71e556dce4628
Alex Weaver <weaver.alex.d@gmail.com>
2022-08-03 17:26:48 -0500

    Tests for correct value propagation

--- grafana/resource_alerting_contact_point_test.go
+++ grafana/resource_alerting_contact_point_test.go
@@ -22,9 +22,41 @@ func TestAccContactPoint_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccExample(t, "resources/grafana_contact_point/resource.tf"),
+				Check: resource.ComposeTestCheckFunc(
+					testContactPointCheckExists("grafana_contact_point.my_contact_point", &points),
+					resource.TestCheckResourceAttr("grafana_contact_point.my_contact_point", "name", "My Contact Point"),
+					resource.TestCheckResourceAttr("grafana_contact_point.my_contact_point", "custom.#", "1"),
+					resource.TestCheckResourceAttr("grafana_contact_point.my_contact_point", "custom.0.type", "email"),
+					resource.TestCheckResourceAttr("grafana_contact_point.my_contact_point", "custom.0.disable_resolve_message", "false"),
+					resource.TestCheckResourceAttr("grafana_contact_point.my_contact_point", "custom.0.settings.addresses", "one@company.org;two@company.org"),
+					resource.TestCheckResourceAttr("grafana_contact_point.my_contact_point", "email.#", "0"),
+				),
 			},
 		},
 	})
+}
+
+func testContactPointCheckExists(rname string, pts *[]gapi.ContactPoint) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resource, ok := s.RootModule().Resources[rname]
+		if !ok {
+			return fmt.Errorf("resource not found: %s, resources: %#v", rname, s.RootModule().Resources)
+		}
+
+		name, ok := resource.Primary.Attributes["name"]
+		if !ok {
+			return fmt.Errorf("resource name not set")
+		}
+
+		client := testAccProvider.Meta().(*client).gapi
+		points, err := client.ContactPointsByName(name)
+		if err != nil {
+			return fmt.Errorf("error getting resource: %w", err)
+		}
+
+		*pts = points
+		return nil
+	}
 }
 
 func testContactPointCheckDestroy(points []gapi.ContactPoint) resource.TestCheckFunc {
@@ -36,6 +68,7 @@ func testContactPointCheckDestroy(points []gapi.ContactPoint) resource.TestCheck
 				return fmt.Errorf("contact point still exists on the server")
 			}
 		}
+		points = []gapi.ContactPoint{}
 		return nil
 	}
 }
