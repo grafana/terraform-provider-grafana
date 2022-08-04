9734a2f3f8ffc882ba3656b5d3806e1284f206c9
Alex Weaver <weaver.alex.d@gmail.com>
2022-08-04 13:53:02 -0500

    Add abstraction for notifiers and make it easy to write new ones. Refactor main code to work against abstraction

--- grafana/resource_alerting_contact_point.go
+++ grafana/resource_alerting_contact_point.go
@@ -10,8 +10,13 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+var notifiers = []notifier{
+	customNotifier{},
+	emailNotifier{},
+}
+
 func ResourceContactPoint() *schema.Resource {
-	return &schema.Resource{
+	resource := &schema.Resource{
 		Description: `
 Manages Grafana Alerting contact points.
 
@@ -34,20 +39,19 @@ Manages Grafana Alerting contact points.
 				Required:    true,
 				Description: "The name of the contact point.",
 			},
-			"custom": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Description: "An unspecified, customizable contact point.",
-				Elem:        customContactResource(),
-			},
-			"email": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Description: "The email contact point.",
-				Elem:        emailContactResource(),
-			},
 		},
 	}
+
+	for _, n := range notifiers {
+		resource.Schema[n.meta().typeStr] = &schema.Schema{
+			Type:        schema.TypeList,
+			Optional:    true,
+			Description: n.meta().desc,
+			Elem:        n.schema(),
+		}
+	}
+
+	return resource
 }
 
 func readContactPoint(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -145,9 +149,11 @@ func deleteContactPoint(ctx context.Context, data *schema.ResourceData, meta int
 func unpackContactPoints(data *schema.ResourceData) []gapi.ContactPoint {
 	result := make([]gapi.ContactPoint, 0)
 	name := data.Get("name").(string)
-	if custom, ok := data.GetOk("custom"); ok {
-		for _, p := range custom.([]interface{}) {
-			result = append(result, unpackCustomNotifier(p, name))
+	for _, n := range notifiers {
+		if points, ok := data.GetOk(n.meta().typeStr); ok {
+			for _, p := range points.([]interface{}) {
+				result = append(result, n.unpack(p, name))
+			}
 		}
 	}
 
@@ -155,78 +161,19 @@ func unpackContactPoints(data *schema.ResourceData) []gapi.ContactPoint {
 }
 
 func packContactPoints(ps []gapi.ContactPoint, data *schema.ResourceData) {
-	points := map[string][]interface{}{}
+	pointsPerNotifier := map[notifier][]interface{}{}
 	for _, p := range ps {
 		data.Set("name", p.Name)
 
-		if p.Type == "email TODO" {
-
-		} else {
-			point := packCustomNotifier(p)
-			points["custom"] = append(points["custom"], point)
+		for _, n := range notifiers {
+			packed := n.pack(p)
+			pointsPerNotifier[n] = append(pointsPerNotifier[n], packed)
 		}
 	}
-	data.Set("custom", points["custom"])
-}
 
-func emailContactResource() *schema.Resource {
-	r := commonNotifierResource()
-	r.Schema["addresses"] = &schema.Schema{
-		Type:        schema.TypeList,
-		Required:    true,
-		Description: "The addresses to send emails to.",
-		Elem: &schema.Schema{
-			Type: schema.TypeString,
-		},
+	for n, pts := range pointsPerNotifier {
+		data.Set(n.meta().typeStr, pts)
 	}
-	r.Schema["single_email"] = &schema.Schema{
-		Type:        schema.TypeBool,
-		Optional:    true,
-		Default:     false,
-		Description: "Whether to send a single email CC'ing all addresses, rather than a separate email to each address.",
-	}
-	r.Schema["message"] = &schema.Schema{
-		Type:        schema.TypeString,
-		Optional:    true,
-		Default:     "",
-		Description: "TODO",
-	}
-	r.Schema["subject"] = &schema.Schema{
-		Type:        schema.TypeString,
-		Optional:    true,
-		Default:     "",
-		Description: "TODO",
-	}
-	return r
-}
-
-func unpackCustomNotifier(raw interface{}, name string) gapi.ContactPoint {
-	json := raw.(map[string]interface{})
-	uid, disableResolve, settings := unpackCommonNotifierFields(json)
-
-	return gapi.ContactPoint{
-		UID:                   uid,
-		Name:                  name,
-		Type:                  json["type"].(string),
-		DisableResolveMessage: disableResolve,
-		Settings:              settings,
-	}
-}
-
-func packCustomNotifier(p gapi.ContactPoint) interface{} {
-	notifier := packCommonNotifierFields(&p)
-	notifier["type"] = p.Type
-	return notifier
-}
-
-func customContactResource() *schema.Resource {
-	r := commonNotifierResource()
-	r.Schema["type"] = &schema.Schema{
-		Type:        schema.TypeString,
-		Required:    true,
-		Description: "The type of the contact point.",
-	}
-	return r
 }
 
 func unpackCommonNotifierFields(raw map[string]interface{}) (string, bool, map[string]interface{}) {
@@ -235,6 +182,7 @@ func unpackCommonNotifierFields(raw map[string]interface{}) (string, bool, map[s
 
 func packCommonNotifierFields(p *gapi.ContactPoint) map[string]interface{} {
 	settings := map[string]interface{}{}
+	// TODO: Terraform expects values to be strings. Convert here.
 	for k, v := range p.Settings {
 		settings[k] = v
 	}
@@ -289,4 +237,150 @@ func toUIDSet(uids []string) map[string]bool {
 		set[uid] = true
 	}
 	return set
+}
+
+type notifier interface {
+	meta() notifierMeta
+	schema() *schema.Resource
+	pack(p gapi.ContactPoint) interface{}
+	unpack(raw interface{}, name string) gapi.ContactPoint
+}
+
+type notifierMeta struct {
+	typeStr string
+	desc    string
+}
+
+type emailNotifier struct{}
+
+var _ notifier = (*emailNotifier)(nil)
+
+func (e emailNotifier) meta() notifierMeta {
+	return notifierMeta{
+		typeStr: "email",
+		desc:    "A contact point that sends notifications to an email address.",
+	}
+}
+
+func (e emailNotifier) schema() *schema.Resource {
+	r := commonNotifierResource()
+	r.Schema["addresses"] = &schema.Schema{
+		Type:        schema.TypeList,
+		Required:    true,
+		Description: "The addresses to send emails to.",
+		Elem: &schema.Schema{
+			Type: schema.TypeString,
+		},
+	}
+	r.Schema["single_email"] = &schema.Schema{
+		Type:        schema.TypeBool,
+		Optional:    true,
+		Default:     false,
+		Description: "Whether to send a single email CC'ing all addresses, rather than a separate email to each address.",
+	}
+	r.Schema["message"] = &schema.Schema{
+		Type:        schema.TypeString,
+		Optional:    true,
+		Default:     "",
+		Description: "The templated content of the email.",
+	}
+	r.Schema["subject"] = &schema.Schema{
+		Type:        schema.TypeString,
+		Optional:    true,
+		Default:     "",
+		Description: "The templated subject line of the email.",
+	}
+	return r
+}
+
+func (e emailNotifier) pack(p gapi.ContactPoint) interface{} {
+	notifier := packCommonNotifierFields(&p)
+	if v, ok := p.Settings["addresses"]; ok && v != nil {
+		notifier["addresses"] = packAddrs(v.(string))
+	}
+	if v, ok := p.Settings["singleEmail"]; ok && v != nil {
+		notifier["single_email"] = v.(bool)
+	}
+	if v, ok := p.Settings["message"]; ok && v != nil {
+		notifier["message"] = v.(string)
+	}
+	if v, ok := p.Settings["subject"]; ok && v != nil {
+		notifier["subject"] = v.(string)
+	}
+	return notifier
+}
+
+func (e emailNotifier) unpack(raw interface{}, name string) gapi.ContactPoint {
+	json := raw.(map[string]interface{})
+	uid, disableResolve, settings := unpackCommonNotifierFields(json)
+
+	addrs := unpackAddrs(json["addresses"].([]string))
+	settings["addresses"] = addrs
+	if v, ok := json["single_email"]; ok && v != nil {
+		settings["singleEmail"] = v.(bool)
+	}
+	if v, ok := json["message"]; ok && v != nil {
+		settings["message"] = v.(string)
+	}
+	if v, ok := json["subject"]; ok && v != nil {
+		settings["subject"] = v.(string)
+	}
+
+	return gapi.ContactPoint{
+		UID:                   uid,
+		Name:                  name,
+		Type:                  "email",
+		DisableResolveMessage: disableResolve,
+		Settings:              settings,
+	}
+}
+
+const addrSeparator = ";"
+
+func packAddrs(addrs string) []string {
+	return strings.Split(addrs, addrSeparator)
+}
+
+func unpackAddrs(addrs []string) string {
+	return strings.Join(addrs, addrSeparator)
+}
+
+type customNotifier struct{}
+
+var _ notifier = (*customNotifier)(nil)
+
+func (c customNotifier) meta() notifierMeta {
+	return notifierMeta{
+		typeStr: "custom",
+		desc:    "An unspecified, customizable contact point.",
+	}
+}
+
+func (c customNotifier) schema() *schema.Resource {
+	r := commonNotifierResource()
+	r.Schema["type"] = &schema.Schema{
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: "The type of the contact point.",
+	}
+	return r
+}
+
+func (c customNotifier) pack(p gapi.ContactPoint) interface{} {
+	notifier := packCommonNotifierFields(&p)
+	notifier["type"] = p.Type
+	return notifier
+}
+
+func (c customNotifier) unpack(raw interface{}, name string) gapi.ContactPoint {
+	json := raw.(map[string]interface{})
+	uid, disableResolve, settings := unpackCommonNotifierFields(json)
+
+	return gapi.ContactPoint{
+		UID:                   uid,
+		Name:                  name,
+		Type:                  json["type"].(string),
+		DisableResolveMessage: disableResolve,
+		Settings:              settings,
+	}
 }
