d9fe6deea70a1a98085cf618d23e3add1af42c85
Alex Weaver <weaver.alex.d@gmail.com>
2022-08-15 15:43:44 -0500

    Additional top level fields and delete hook

--- grafana/resource_alerting_notification_policy.go
+++ grafana/resource_alerting_notification_policy.go
@@ -12,7 +12,8 @@ func ResourceNotificationPolicy() *schema.Resource {
 	return &schema.Resource{
 		Description: `TODO`,
 
-		ReadContext: readNotificationPolicy,
+		ReadContext:   readNotificationPolicy,
+		DeleteContext: deleteNotificationPolicy,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
@@ -32,6 +33,21 @@ func ResourceNotificationPolicy() *schema.Resource {
 					Type: schema.TypeString,
 				},
 			},
+			"group_wait": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Time to wait to buffer alerts of the same group before sending a notification. Default is 30 seconds.",
+			},
+			"group_interval": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Minimum time interval between two notifications for the same group. Default is 5 minutes.",
+			},
+			"repeat_interval": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Minimum time interval for re-sending a notification if an alert is still firing. Default is 4 hours.",
+			},
 		},
 	}
 }
@@ -49,7 +65,19 @@ func readNotificationPolicy(ctx context.Context, data *schema.ResourceData, meta
 	return nil
 }
 
+func deleteNotificationPolicy(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*client).gapi
+
+	if err := client.ResetNotificationPolicyTree(); err != nil {
+		return diag.FromErr(err)
+	}
+	return diag.Diagnostics{}
+}
+
 func packNotifPolicy(npt gapi.NotificationPolicyTree, data *schema.ResourceData) {
 	data.Set("receiver", npt.Receiver)
 	data.Set("group_by", npt.GroupBy)
+	data.Set("group_wait", npt.GroupWait)
+	data.Set("group_interval", npt.GroupInterval)
+	data.Set("repeat_interval", npt.RepeatInterval)
 }
