a9934b5a3767c929d532a0131e0fd65a686c5890
Alex Weaver <weaver.alex.d@gmail.com>
2022-08-15 17:33:01 -0500

    Tests for create and import

--- grafana/resource_alerting_notification_policy_test.go
+++ grafana/resource_alerting_notification_policy_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	gapi "github.com/grafana/grafana-api-golang-client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -20,7 +21,21 @@ func TestAccNotificationPolicy_basic(t *testing.T) {
 			// Test creation.
 			{
 				Config: testAccExample(t, "resources/grafana_notification_policy/resource.tf"),
-				Check:  resource.ComposeTestCheckFunc(),
+				Check: resource.ComposeTestCheckFunc(
+					testNotifPolicyCheckExists("grafana_notification_policy.my_notification_policy"),
+					resource.TestCheckResourceAttr("grafana_notification_policy.my_notification_policy", "contact_point", "A Contact Point"),
+					resource.TestCheckResourceAttr("grafana_notification_policy.my_notification_policy", "group_by.#", "1"),
+					resource.TestCheckResourceAttr("grafana_notification_policy.my_notification_policy", "group_by.0", "..."),
+					resource.TestCheckResourceAttr("grafana_notification_policy.my_notification_policy", "group_wait", "45s"),
+					resource.TestCheckResourceAttr("grafana_notification_policy.my_notification_policy", "group_interval", "6m"),
+					resource.TestCheckResourceAttr("grafana_notification_policy.my_notification_policy", "repeat_interval", "3h"),
+				),
+			},
+			// Test import.
+			{
+				ResourceName:      "grafana_notification_policy.my_notification_policy",
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -34,9 +49,37 @@ func testNotifPolicyCheckDestroy() resource.TestCheckFunc {
 			return fmt.Errorf("failed to get notification policies")
 		}
 
-		if npt.Receiver != "grafana-default-email" {
+		if !notifPolicyIsDefault(npt) {
 			return fmt.Errorf("notification policy tree was not reset back to the default")
 		}
 		return nil
 	}
+}
+
+func testNotifPolicyCheckExists(rname string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resource, ok := s.RootModule().Resources[rname]
+		if !ok {
+			return fmt.Errorf("resource not found: %s, resources: %#v", rname, s.RootModule().Resources)
+		}
+
+		if resource.Primary.ID == "" {
+			return fmt.Errorf("resource id not set")
+		}
+
+		client := testAccProvider.Meta().(*client).gapi
+		npt, err := client.NotificationPolicyTree()
+		if err != nil {
+			return fmt.Errorf("failed to get notification policies")
+		}
+
+		if notifPolicyIsDefault(npt) {
+			return fmt.Errorf("policy tree on the server is still the default one")
+		}
+		return nil
+	}
+}
+
+func notifPolicyIsDefault(np gapi.NotificationPolicyTree) bool {
+	return np.Receiver == "grafana-default-email"
 }
