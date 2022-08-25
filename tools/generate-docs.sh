f32d1afbb1776aba462fdab7bf10a007f5248331
Julien Duchesne <julien.duchesne@grafana.com>
2022-08-25 15:18:51 -0400

    Add subcategories to docs Closes https://github.com/grafana/terraform-provider-grafana/issues/227 With alerting being added, resources are getting more confused. By adding a subcategory, we can split them off in an `Alerting` section in the docs Docs are still generated with `go generate` and will fail if a new resource doesn't have a category set in the `subcategories.json` file

---
+++ tools/generate-docs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Generate docs!
+go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
+# Generate subcategories
+# Follow https://github.com/hashicorp/terraform-plugin-docs/issues/156 to see if anything is merged upstream. If so, we should use that.
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+subcategories="$(cat ${SCRIPTPATH}/subcategories.json)"
+
+for f in $(find ./docs/ -name "*.md"); do
+    f=${f#"./docs/"}
+    f="${f%.*}"
+
+    subcategory="$(echo ${subcategories} | jq -r ".[\"${f}\"]")"
+    if [[ "${subcategory}" == "ignore" ]]; then
+        continue
+    elif [[ "${subcategory}" != "null" ]] && [[ -n "${subcategory}" ]]; then
+        sed -i "s/subcategory: \"\"/subcategory: \"${subcategory}\"/" ./docs/${f}.md
+    else
+        echo "No subcategory for $f. Define one in ./tools/subcategories.json" && exit 1
+    fi
+done
