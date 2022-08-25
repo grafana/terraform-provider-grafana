60805c876c8240bdf00b4bfabda42c44c2554cd4
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-08-25 17:00:09 -0300

    Fixes for macOS execution
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- tools/generate-docs.sh
+++ tools/generate-docs.sh
@@ -6,19 +6,17 @@ go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 # Generate subcategories
 # Follow https://github.com/hashicorp/terraform-plugin-docs/issues/156 to see if anything is merged upstream. If so, we should use that.
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-subcategories="$(cat ${SCRIPTPATH}/subcategories.json)"
 
 echo -e "\nAdding subcategories to docs..."
-for f in $(find ./docs/ -name "*.md"); do
-    f=${f#"./docs/"}
-    f="${f%.*}"
+for f in $(find ./docs -name "*.md" | sed -e 's|^\./docs/||' -e 's|\.md$||'); do
     echo "  - ${f}"
 
-    subcategory="$(echo ${subcategories} | jq -r ".[\"${f}\"]")"
+    subcategory=$(jq -r --arg cat "${f}" '.[$cat]' "${SCRIPTPATH}/subcategories.json")
     if [[ "${subcategory}" == "ignore" ]]; then
         continue
     elif [[ "${subcategory}" != "null" ]] && [[ -n "${subcategory}" ]]; then
-        sed -i "s/subcategory: \"\"/subcategory: \"${subcategory}\"/" ./docs/${f}.md
+        sed -i.bak "s/subcategory: \"\"/subcategory: \"${subcategory}\"/" ./docs/${f}.md &&
+            rm ./docs/${f}.md.bak
     else
         echo "No subcategory for $f. Define one in ./tools/subcategories.json" && exit 1
     fi
