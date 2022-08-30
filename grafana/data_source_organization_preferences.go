eda12c614a35e187118b88c866d9f6a57d5254b3
Mike Ball <mikedball@gmail.com>
2022-08-30 06:57:28 -0400

    set ID on organization_preferences data source
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- grafana/data_source_organization_preferences.go
+++ grafana/data_source_organization_preferences.go
@@ -2,6 +2,9 @@ package grafana
 
 import (
 	"context"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -63,6 +66,26 @@ func dataSourceOrganizationPreferencesRead(ctx context.Context, d *schema.Resour
 	d.Set("timezone", prefs.Timezone)
 	d.Set("week_start", prefs.WeekStart)
 	d.Set("locale", prefs.Locale)
+
+	// TODO: is it problematic that every organization_preference data source will have the same ID?
+	//
+	// According to @julienduchesne (https://github.com/grafana/terraform-provider-grafana/pull/583/files/b261189cf70ae4c076d9319d83abda2a959e5112#r944357467) ...
+	// "The ID should be declarative because it needs to be unique.
+	// For a datasource like this, it's usually the combination of all entry parameters because you
+	// will typically not have the same datasource twice with the same parameters"
+	//
+	// However, in this instance, the data source does not accept any parameters; they are all computed.
+	// So, what would be a reasonable way to calculate its ID?
+	sha := sha256.Sum256([]byte(strings.Join([]string{
+		"theme",
+		"home_dashboard_id",
+		"home_dashboard_uid",
+		"timezone",
+		"week_start",
+		"locale",
+	}, "-")))
+
+	d.SetId(fmt.Sprintf("%x", sha))
 
 	return nil
 }
