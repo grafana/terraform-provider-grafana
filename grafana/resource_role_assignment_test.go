d59abcc8711a4bb87e35538acd522ba03929fcf9
IevaVasiljeva <ieva.vasiljeva@grafana.com>
2022-09-23 10:48:18 +0100

    improve the test

--- grafana/resource_role_assignment_test.go
+++ grafana/resource_role_assignment_test.go
@@ -2,7 +2,6 @@ package grafana
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -13,12 +12,6 @@ import (
 
 func TestRoleAssignments(t *testing.T) {
 	CheckEnterpriseTestsEnabled(t)
-	defer removeResources()
-
-	if err := prepareResources(); err != nil {
-		t.Errorf("could not prepare resources for the test: %s", err)
-		return
-	}
 	var roleAssignment gapi.RoleAssignments
 
 	resource.Test(t, resource.TestCase{
@@ -26,7 +19,7 @@ func TestRoleAssignments(t *testing.T) {
 		CheckDestroy:      testRoleAssignmentCheckDestroy(&roleAssignment),
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(roleAssignmentConfig, roleUID, user1ID, user2ID, teamID),
+				Config: fmt.Sprintf(roleAssignmentConfig, roleUID),
 				Check: resource.ComposeTestCheckFunc(
 					testRoleAssignmentCheckExists("grafana_role_assignment.test", &roleAssignment),
 					resource.TestCheckResourceAttr(
@@ -36,31 +29,21 @@ func TestRoleAssignments(t *testing.T) {
 						"grafana_role_assignment.test", "users.#", "2",
 					),
 					resource.TestCheckResourceAttr(
-						"grafana_role_assignment.test", "users.0", strconv.FormatInt(user1ID, 10),
-					),
-					resource.TestCheckResourceAttr(
-						"grafana_role_assignment.test", "users.1", strconv.FormatInt(user2ID, 10),
-					),
-					resource.TestCheckResourceAttr(
 						"grafana_role_assignment.test", "service_accounts.#", "0",
 					),
 					resource.TestCheckResourceAttr(
 						"grafana_role_assignment.test", "teams.#", "1",
 					),
-					resource.TestCheckResourceAttr(
-						"grafana_role_assignment.test", "teams.0", strconv.FormatInt(teamID, 10),
-					),
 				),
 			},
 			{
-				Config:  fmt.Sprintf(roleAssignmentConfig, roleUID, user1ID, user2ID, teamID),
+				Config:  fmt.Sprintf(roleAssignmentConfig, roleUID),
 				Destroy: true,
 			},
 		},
 	})
 }
 
-// TODO
 func testRoleAssignmentCheckExists(rn string, ra *gapi.RoleAssignments) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[rn]
@@ -96,74 +79,28 @@ func testRoleAssignmentCheckDestroy(ra *gapi.RoleAssignments) resource.TestCheck
 	}
 }
 
-func prepareResources() error {
-	client := testAccProvider.Meta().(*client).gapi
-	r := gapi.Role{
-		UID:  roleUID,
-		Name: "terraform_test_role",
-		Permissions: []gapi.Permission{
-			{
-				Action: "reports:read",
-				Scope:  "reports:*",
-			},
-		},
-	}
-	if _, err := client.NewRole(r); err != nil {
-		return fmt.Errorf("error creating role: %w", err)
-	}
-
-	var err error
-	if teamID, err = client.AddTeam("terraform_test_team", "terraform_test@team"); err != nil {
-		return fmt.Errorf("error creating team: %w", err)
-	}
-
-	user := gapi.User{
-		Email:    "terraform_test_user@grafana.com",
-		Login:    "terraform_test_user",
-		Name:     "terraform_test_user",
-		Password: "123456",
-	}
-	if user1ID, err = client.CreateUser(user); err != nil {
-		return fmt.Errorf("error creating user: %w", err)
-	}
-
-	user2 := gapi.User{
-		Email:    "terraform_test_user2@grafana.com",
-		Login:    "terraform_test_user2",
-		Name:     "terraform_test_user2",
-		Password: "123456",
-	}
-	if user2ID, err = client.CreateUser(user2); err != nil {
-		return fmt.Errorf("error creating user: %w", err)
-	}
-
-	return nil
-}
-
-func removeResources() {
-	client := testAccProvider.Meta().(*client).gapi
-
-	if err := client.DeleteRole(roleUID, false); err != nil {
-		fmt.Printf("failed to remove role with UID %s\n", roleUID)
-	}
-	if err := client.DeleteTeam(teamID); err != nil {
-		fmt.Printf("failed to remove team with ID %d\n", teamID)
-	}
-	if err := client.DeleteUser(user1ID); err != nil {
-		fmt.Printf("failed to remove user with ID %d\n", user1ID)
-	}
-	if err := client.DeleteUser(user2ID); err != nil {
-		fmt.Printf("failed to remove user with ID %d\n", user2ID)
-	}
-}
-
-var user1ID, user2ID, teamID int64
 var roleUID = "terraform_test_role"
 
 var roleAssignmentConfig = `
+resource "grafana_team" "test_team" {
+	name = "terraform_test_team"
+}
+
+resource "grafana_user" "test_user" {
+	email = "terraform_user@test.com"
+	login    = "terraform_user@test.com"
+	password = "12345"
+}
+
+resource "grafana_user" "test_user2" {
+	email = "terraform_user2@test.com"
+	login    = "terraform_user2@test.com"
+	password = "12345"
+}
+
 resource "grafana_role_assignment" "test" {
   role_uid = "%s"
-  users = [%d,%d]
-  teams = [%d]
+  users = [grafana_user.test_user.id, grafana_user.test_user2.id]
+  teams = [grafana_team.test_team.id]
 }
 `
