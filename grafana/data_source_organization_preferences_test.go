f0ddb0fac375e529d78e58d600ba9250a37526d2
Mike Ball <mikedball@gmail.com>
2022-10-16 07:06:08 -0400

    add initial organization_preferences datasource test
    Signed-off-by: Mike Ball <mikedball@gmail.com>

---
+++ grafana/data_source_organization_preferences_test.go
@@ -0,0 +1,30 @@
+package grafana
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDatasourceOrganizationPreferences(t *testing.T) {
+	CheckOSSTestsEnabled(t)
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"data.grafana_organization_preferences.test", "theme", "",
+		),
+		resource.TestCheckResourceAttr(
+			"data.grafana_organization_preferences.test", "timezone", "",
+		),
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccExample(t, "data-sources/grafana_organization_preferences/data-source.tf"),
+				Check:  resource.ComposeTestCheckFunc(checks...),
+			},
+		},
+	})
+}
