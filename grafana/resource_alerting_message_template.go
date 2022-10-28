89a5c0d3e10b1a58a4ae2209bf2f1bf09e7c3eeb
Julien Duchesne <julien.duchesne@grafana.com>
2022-10-28 09:42:51 -0400

    Fix broken alerting links (#703)
    `next` -> `latest` in the URLs

--- grafana/resource_alerting_message_template.go
+++ grafana/resource_alerting_message_template.go
@@ -14,7 +14,7 @@ func ResourceMessageTemplate() *schema.Resource {
 		Description: `
 Manages Grafana Alerting message templates.
 
-* [Official documentation](https://grafana.com/docs/grafana/next/alerting/contact-points/message-templating/)
+* [Official documentation](https://grafana.com/docs/grafana/latest/alerting/contact-points/message-templating/)
 * [HTTP API](https://grafana.com/docs/grafana/next/developers/http_api/alerting_provisioning/#templates)
 
 This resource requires Grafana 9.1.0 or later.
