bf78e71120cb8abfe0b77d1ab44177b1c9b9dd86
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2023-03-01 13:59:10 +0800

    docs

--- examples/resources/grafana_oncall_integration/resource.tf
+++ examples/resources/grafana_oncall_integration/resource.tf
@@ -5,3 +5,24 @@ resource "grafana_oncall_integration" "test-acc-integration" {
   default_route {
   }
 }
+
+# Also it's possible to manage integration templates.
+# Check docs to see all available templates.
+resource "grafana_oncall_integration" "integration_with_templates" {
+  provider = grafana.oncall
+  name     = "integration_with_templates"
+  type     = "webhook"
+  default_route {
+  }
+  templates {
+    grouping_key = "{{ payload.group_id }}"
+    slack {
+        title = "Slack title"
+        message = <<-EOT
+          This is example of multiline template
+          {{ payload.message }}
+        EOT
+        image_url = "{{ payload.image_url }}"
+    }
+  }
+}
