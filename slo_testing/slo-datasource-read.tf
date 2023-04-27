d6817011129f97c4bb80dac614818f95b3348369
Elaine Vuong <elaine.yt.vuong@gmail.com>
2023-04-27 11:26:29 -0400

    Add Instructions for Testing SLO Resources

---
+++ slo_testing/slo-datasource-read.tf
@@ -0,0 +1,18 @@
+# terraform {
+#   required_providers {
+#     grafana = {
+#       source  = "registry.terraform.io/grafana/grafana"
+#     }
+#   }
+# }
+
+# provider "grafana" {
+#   url = "https://elainetest.grafana.net/"
+# }
+
+# data "grafana_slo" "test1" {
+# }
+
+# output "test1" {
+#   value = data.grafana_slo.test1
+# }
---
+++ slo_testing/slo-resource-import.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    grafana = {
+      source = "registry.terraform.io/grafana/grafana"
+    }
+  }
+}
+
+provider "grafana" {
+  url = "https://elainetest.grafana.net/"
+}
+
+resource "grafana_slo" "sample" {
+  name        = "Terraform - Import Test Name"
+  description = "Terraform - Import Test Description"
+  query       = "sum(rate(apiserver_request_total{code!=\"500\"}[$__rate_interval])) / sum(rate(apiserver_request_total[$__rate_interval]))"
+  objectives {
+    objective_value  = 0.995
+    objective_window = "30d"
+  }
+}
+
+output "sample_slo" {
+  value = grafana_slo.sample
+}
