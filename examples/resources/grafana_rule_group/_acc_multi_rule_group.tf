grafana/terraform-provider-grafana

commit 9e6d1f5ec453edcca7989151d5d05d73c753331b

Alerting Rule Group: Suppress duration equivalent diffs (#941)
Closes https://github.com/grafana/terraform-provider-grafana/issues/861
When setting 60m, the API returns 1h. Also added a duration validation

examples/resources/grafana_rule_group/_acc_multi_rule_group.tf
@@ -9,7 +9,7 @@ resource "grafana_rule_group" "my_multi_alert_group" {
   org_id           = 1
   rule {
     name           = "My Alert Rule 1"
-    for            = "2m"
+    for            = "60m"
     condition      = "B"
     no_data_state  = "NoData"
     exec_err_state = "Alerting"

examples/resources/grafana_rule_group/_acc_multi_rule_group_added.tf
@@ -9,7 +9,7 @@ resource "grafana_rule_group" "my_multi_alert_group" {
   org_id           = 1
   rule {
     name           = "My Alert Rule 1"
-    for            = "2m"
+    for            = "1h"
     condition      = "B"
     no_data_state  = "NoData"
     exec_err_state = "Alerting"

examples/resources/grafana_rule_group/_acc_multi_rule_group_subtracted.tf
@@ -9,7 +9,7 @@ resource "grafana_rule_group" "my_multi_alert_group" {
   org_id           = 1
   rule {
     name           = "My Alert Rule 1"
-    for            = "2m"
+    for            = "1h"
     condition      = "B"
     no_data_state  = "NoData"
     exec_err_state = "Alerting"

internal/common/schema.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -49,4 +52,13 @@ func CloneResourceSchemaForDatasource(r *schema.Resource, updates map[string]*sc
 
 func AllowedValuesDescription(description string, allowedValues []string) string {
 	return fmt.Sprintf("%s. Allowed values: `%s`.", description, strings.Join(allowedValues, "`, `"))
+}
+
+func ValidateDuration(i interface{}, p cty.Path) diag.Diagnostics {
+	v := i.(string)
+	_, err := time.ParseDuration(v)
+	if err != nil {
+		return diag.Errorf("%q is not a valid duration: %s", v, err)
+	}
+	return nil
 }

internal/resources/grafana/resource_alerting_rule_group.go
@@ -76,10 +76,16 @@ This resource requires Grafana 9.1.0 or later.
 							Description: "The name of the alert rule.",
 						},
 						"for": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     0,
-							Description: "The amount of time for which the rule must be breached for the rule to be considered to be Firing. Before this time has elapsed, the rule is only considered to be Pending.",
+							Type:             schema.TypeString,
+							Optional:         true,
+							Default:          0,
+							Description:      "The amount of time for which the rule must be breached for the rule to be considered to be Firing. Before this time has elapsed, the rule is only considered to be Pending.",
+							ValidateDiagFunc: common.ValidateDuration,
+							DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
+								oldDuration, _ := time.ParseDuration(oldValue)
+								newDuration, _ := time.ParseDuration(newValue)
+								return oldDuration == newDuration
+							},
 						},
 						"no_data_state": {
 							Type:        schema.TypeString,

internal/resources/cloud/resource_cloud_stack.go
@@ -14,7 +14,6 @@ import (
 
 	gapi "github.com/grafana/grafana-api-golang-client"
 	"github.com/grafana/terraform-provider-grafana/internal/common"
-	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -86,17 +85,10 @@ available at “https://<stack_slug>.grafana.net".`,
 				DiffSuppressFunc: func(_, _, newValue string, _ *schema.ResourceData) bool { return newValue == "false" },
 			},
 			"wait_for_readiness_timeout": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  defaultReadinessTimeout.String(),
-				ValidateDiagFunc: func(i interface{}, p cty.Path) diag.Diagnostics {
-					v := i.(string)
-					_, err := time.ParseDuration(v)
-					if err != nil {
-						return diag.Errorf("%q is not a valid duration: %s", v, err)
-					}
-					return nil
-				},
+				Type:             schema.TypeString,
+				Optional:         true,
+				Default:          defaultReadinessTimeout.String(),
+				ValidateDiagFunc: common.ValidateDuration,
 				// Only used when wait_for_readiness is true
 				DiffSuppressFunc: func(_, _, newValue string, d *schema.ResourceData) bool {
 					return newValue == defaultReadinessTimeout.String()

commit 3cff499bf9c6deb82ac3d6936db74b56ee3e31fb

Alerting Rule Group: Allow days in validation Bug introduced in https://github.com/grafana/terraform-provider-grafana/pull/941 Closes https://github.com/grafana/terraform-provider-grafana/issues/955
Turns out the duration lib used in alerting supports days and weeks. Let's use the same lib here

examples/resources/grafana_rule_group/_acc_multi_rule_group.tf
@@ -85,7 +85,7 @@ EOT
 
   rule {
     name           = "My Alert Rule 2"
-    for            = "4m"
+    for            = "6d"
     condition      = "B"
     no_data_state  = "NoData"
     exec_err_state = "Alerting"

examples/resources/grafana_rule_group/_acc_multi_rule_group_added.tf
@@ -85,7 +85,7 @@ EOT
 
   rule {
     name           = "My Alert Rule 2"
-    for            = "4m"
+    for            = "144h" // 6 days
     condition      = "B"
     no_data_state  = "NoData"
     exec_err_state = "Alerting"

examples/resources/grafana_rule_group/_acc_multi_rule_group_subtracted.tf
@@ -85,7 +85,7 @@ EOT
 
   rule {
     name           = "My Alert Rule 2"
-    for            = "4m"
+    for            = "8640m" // 6 days
     condition      = "B"
     no_data_state  = "NoData"
     exec_err_state = "Alerting"

go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320
 	github.com/hashicorp/terraform-plugin-docs v0.15.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.26.1
+	github.com/prometheus/common v0.44.0
 	golang.org/x/text v0.10.0
 )
 

go.sum
@@ -171,7 +171,9 @@ github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZN
 github.com/posener/complete v1.1.1/go.mod h1:em0nMJCgc9GFtwrmVmEMR/ZL6WyhyjMBndrE9hABlRI=
 github.com/posener/complete v1.2.3 h1:NP0eAhjcjImqslEwo/1hq7gpajME0fTLTezBKDqfXqo=
 github.com/posener/complete v1.2.3/go.mod h1:WZIdtGGp+qx0sLrYKtIRAruyNpv6hFCicSgv7Sy7s/s=
-github.com/rogpeppe/go-internal v1.6.1 h1:/FiVV8dS/e+YqF2JvO3yXRFbBLTIuSDkuC7aBOAvL+k=
+github.com/prometheus/common v0.44.0 h1:+5BrQJwiBB9xsMygAB3TNvpQKOwlkc25LbISbrdOOfY=
+github.com/prometheus/common v0.44.0/go.mod h1:ofAIvZbQ1e/nugmZGz4/qCb9Ap1VoSTIO7x0VV9VvuY=
+github.com/rogpeppe/go-internal v1.10.0 h1:TMyTOH3F/DB16zRVcYyreMH6GnZZrwQVAoYjRBZyWFQ=
 github.com/russross/blackfriday v1.6.0 h1:KqfZb0pUVN2lYqZUYRddxF4OR8ZMURnJIG5Y3VRLtww=
 github.com/russross/blackfriday v1.6.0/go.mod h1:ti0ldHuxg49ri4ksnFxlkCfN+hvslNlmVHqNRXXJNAY=
 github.com/sergi/go-diff v1.2.0 h1:XU+rvMAioB0UC3q1MFrIQy4Vo5/4VsRDQQXHsEya6xQ=
@@ -280,8 +282,8 @@ google.golang.org/protobuf v1.26.0/go.mod h1:9q0QmTI4eRPtz6boOQmLYwt+qCgq0jsYwAQ
 google.golang.org/protobuf v1.30.0 h1:kPPoIgf3TsEvrm0PFe15JQ+570QVxYzEvvHqChK+cng=
 google.golang.org/protobuf v1.30.0/go.mod h1:HV8QOd/L58Z+nl8r43ehVNZIU/HEI6OcFqwMG9pJV4I=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=
-gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 h1:qIbj1fsPNlZgppZ+VLlY7N33q108Sa+fhmuc+sWQYwY=
 gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=
+gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c h1:Hei/4ADfdWqJk1ZMxUNpqntNwaWcugrBjAiHlqqRiVk=
 gopkg.in/warnings.v0 v0.1.2 h1:wFXVbFY8DY5/xOe1ECiWdKCzZlxgshcYVNkBHstARME=
 gopkg.in/yaml.v2 v2.2.2/go.mod h1:hI93XBmqTisBFMUTm0b8Fm+jr3Dg1NNxqwp+5A1VGuI=
 gopkg.in/yaml.v2 v2.3.0/go.mod h1:hI93XBmqTisBFMUTm0b8Fm+jr3Dg1NNxqwp+5A1VGuI=

internal/common/schema.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	promModel "github.com/prometheus/common/model"
 )
 
 // SchemaDiffFloat32 is a SchemaDiffSuppressFunc for diffing float32 values.
@@ -57,6 +58,15 @@ func AllowedValuesDescription(description string, allowedValues []string) string
 func ValidateDuration(i interface{}, p cty.Path) diag.Diagnostics {
 	v := i.(string)
 	_, err := time.ParseDuration(v)
+	if err != nil {
+		return diag.Errorf("%q is not a valid duration: %s", v, err)
+	}
+	return nil
+}
+
+func ValidateDurationWithDays(i interface{}, p cty.Path) diag.Diagnostics {
+	v := i.(string)
+	_, err := promModel.ParseDuration(v)
 	if err != nil {
 		return diag.Errorf("%q is not a valid duration: %s", v, err)
 	}

internal/resources/grafana/resource_alerting_rule_group.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	promModel "github.com/prometheus/common/model"
 )
 
 func ResourceRuleGroup() *schema.Resource {
@@ -81,10 +82,10 @@ This resource requires Grafana 9.1.0 or later.
 							Optional:         true,
 							Default:          0,
 							Description:      "The amount of time for which the rule must be breached for the rule to be considered to be Firing. Before this time has elapsed, the rule is only considered to be Pending.",
-							ValidateDiagFunc: common.ValidateDuration,
+							ValidateDiagFunc: common.ValidateDurationWithDays,
 							DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
-								oldDuration, _ := time.ParseDuration(oldValue)
-								newDuration, _ := time.ParseDuration(newValue)
+								oldDuration, _ := promModel.ParseDuration(oldValue)
+								newDuration, _ := promModel.ParseDuration(newValue)
 								return oldDuration == newDuration
 							},
 						},