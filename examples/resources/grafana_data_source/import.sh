6e0d09b08afdac059e6fbdc65dd33ba87c63242a
Jérôme Tama <j.tama@groupeonepoint.com>
2023-09-07 13:59:41 +0200

    Specify allowed id format for datasource import

--- examples/resources/grafana_data_source/import.sh
+++ examples/resources/grafana_data_source/import.sh
@@ -1,2 +1,5 @@
-terraform import grafana_data_source.by_integer_id {{datasource id}}
-terraform import grafana_data_source.by_uid {{datasource uid}}
+terraform import grafana_data_source.by_integer_id {{datasource_id}} # To use the default provider org
+terraform import grafana_data_source.by_uid {{datasource_uid}} # To use the default provider org
+
+terraform import grafana_data_source.by_integer_id {{org_id}}:{{datasource_id}} # When "org_id" is set on the resource
+terraform import grafana_data_source.by_uid {{org_id}}:{{datasource_uid}} # When "org_id" is set on the resource
