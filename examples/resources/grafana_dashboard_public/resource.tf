a237b4260e5533cec977bca0c0244c014b27f753
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2023-09-08 16:11:38 -0300

    fix lint errors

--- examples/resources/grafana_dashboard_public/resource.tf
+++ examples/resources/grafana_dashboard_public/resource.tf
@@ -7,11 +7,11 @@ resource "grafana_dashboard" "my_dashboard" {
 resource "grafana_dashboard_public" "my_public_dashboard" {
   dashboard_uid = grafana_dashboard.my_dashboard.uid
 
-  uid = "my-custom-public-uid"
+  uid          = "my-custom-public-uid"
   access_token = "e99e4275da6f410d83760eefa934d8d2"
 
   time_selection_enabled = true
-  is_enabled = true
-  annotations_enabled = true
-  share = "public"
+  is_enabled             = true
+  annotations_enabled    = true
+  share                  = "public"
 }
