ad7ee1fd4d4d6a37fea1aca4366bd041b40196fd
Ezequiel Victorero <ezequiel.victorero@grafana.com>
2023-09-18 10:30:54 -0300

    fix lint

--- examples/resources/grafana_dashboard_public/resource.tf
+++ examples/resources/grafana_dashboard_public/resource.tf
@@ -19,16 +19,16 @@ resource "grafana_dashboard" "test_dash" {
 }
 
 resource "grafana_dashboard_public" "my_public_dashboard" {
-  org_id = grafana_organization.my_org.org_id
+  org_id        = grafana_organization.my_org.org_id
   dashboard_uid = grafana_dashboard.test_dash.uid
 
   uid          = "my-custom-public-uid"
   access_token = "e99e4275da6f410d83760eefa934d8d2"
 
   time_selection_enabled = true
-  is_enabled = true
-  annotations_enabled = true
-  share = "public"
+  is_enabled             = true
+  annotations_enabled    = true
+  share                  = "public"
 }
 
 // Optional (On-premise, not supported in Grafana Cloud): Create an organization
@@ -45,7 +45,7 @@ resource "grafana_dashboard" "test_dash2" {
 }
 
 resource "grafana_dashboard_public" "my_public_dashboard2" {
-  org_id = grafana_organization.my_org2.org_id
+  org_id        = grafana_organization.my_org2.org_id
   dashboard_uid = grafana_dashboard.test_dash2.uid
 
   share = "public"
