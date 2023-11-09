80415a032620f77673d840a34a62dcdcc8277bcb
Julien Duchesne <julien.duchesne@grafana.com>
2023-11-09 10:22:53 -0500

    Library Panel Tests: Use OpenAPI client (#1138)
    Gets rid of the manually written checkers
    Also, I added a quick sanity check in the helpers. When `exists` is called, `destroyed` should fail at that moment. This ensures that the destroyed check doesn't just always pass

--- internal/resources/grafana/data_source_library_panel_test.go
+++ internal/resources/grafana/data_source_library_panel_test.go
@@ -3,7 +3,7 @@ package grafana_test
 import (
 	"testing"
 
-	gapi "github.com/grafana/grafana-api-golang-client"
+	"github.com/grafana/grafana-openapi-client-go/models"
 	"github.com/grafana/terraform-provider-grafana/internal/common"
 	"github.com/grafana/terraform-provider-grafana/internal/testutils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -12,10 +12,10 @@ import (
 func TestAccDatasourceLibraryPanel_basic(t *testing.T) {
 	testutils.CheckOSSTestsEnabled(t, ">=8.0.0")
 
-	var panel gapi.LibraryPanel
+	var panel models.LibraryElementResponse
 	// var dashboard gapi.Dashboard
 	checks := []resource.TestCheckFunc{
-		testAccLibraryPanelCheckExists("grafana_library_panel.test", &panel),
+		libraryPanelCheckExists.exists("grafana_library_panel.test", &panel),
 		resource.TestCheckResourceAttr(
 			"data.grafana_library_panel.from_name", "name", "test name",
 		),
@@ -33,7 +33,7 @@ func TestAccDatasourceLibraryPanel_basic(t *testing.T) {
 	// TODO: Make parallelizable
 	resource.Test(t, resource.TestCase{
 		ProviderFactories: testutils.ProviderFactories,
-		CheckDestroy:      testAccLibraryPanelCheckDestroy(&panel),
+		CheckDestroy:      libraryPanelCheckExists.destroyed(&panel, nil),
 		Steps: []resource.TestStep{
 			{
 				Config: testutils.TestAccExample(t, "data-sources/grafana_library_panel/data-source.tf"),
