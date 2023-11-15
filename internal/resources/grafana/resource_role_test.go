f76a4650d3aa6b32c06372f69dfe0a20455f09d3
Julien Duchesne <julien.duchesne@grafana.com>
2023-11-15 08:33:24 -0500

    Role: Add test for non-default org creation (#1158)
    Failing to reproduce https://github.com/grafana/terraform-provider-grafana/issues/1157

--- internal/resources/grafana/resource_role_test.go
+++ internal/resources/grafana/resource_role_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
+	gapi "github.com/grafana/grafana-api-golang-client"
 	"github.com/grafana/grafana-openapi-client-go/models"
 	"github.com/grafana/terraform-provider-grafana/internal/testutils"
 )
@@ -120,6 +121,73 @@ func TestAccRoleVersioning(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccRole_inOrg(t *testing.T) {
+	testutils.CheckEnterpriseTestsEnabled(t)
+
+	var role models.RoleDTO
+	var org gapi.Org
+	name := acctest.RandomWithPrefix("role-")
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProviderFactories: testutils.ProviderFactories,
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			roleCheckExists.destroyed(&role, &org),
+			testAccOrganizationCheckDestroy(&org),
+		),
+		Steps: []resource.TestStep{
+			{
+				Config: roleInOrg(name, true),
+				Check: resource.ComposeTestCheckFunc(
+					roleCheckExists.exists("grafana_role.test", &role),
+					testAccOrganizationCheckExists("grafana_organization.test", &org),
+					checkResourceIsInOrg("grafana_role.test", "grafana_organization.test"),
+					resource.TestMatchResourceAttr("grafana_role.test", "id", nonDefaultOrgIDRegexp),
+					resource.TestCheckResourceAttr("grafana_role.test", "name", name),
+					resource.TestCheckResourceAttr("grafana_role.test", "description", name+" desc"),
+					resource.TestCheckResourceAttr("grafana_role.test", "display_name", name),
+					resource.TestCheckResourceAttr("grafana_role.test", "group", name),
+					resource.TestCheckResourceAttr("grafana_role.test", "version", "1"),
+					resource.TestCheckResourceAttr("grafana_role.test", "uid", name),
+					resource.TestCheckResourceAttr("grafana_role.test", "global", "false"),
+					resource.TestCheckResourceAttr("grafana_role.test", "hidden", "false"),
+				),
+			},
+			// Test destroying role within org
+			{
+				Config: roleInOrg(name, false),
+				Check: resource.ComposeTestCheckFunc(
+					roleCheckExists.destroyed(&role, &org),
+					testAccOrganizationCheckExists("grafana_organization.test", &org),
+				),
+			},
+		},
+	})
+}
+
+func roleInOrg(name string, roleExists bool) string {
+	def := fmt.Sprintf(`
+resource "grafana_organization" "test" {
+	name = "%s"
+}`, name)
+
+	if roleExists {
+		def += fmt.Sprintf(`
+resource "grafana_role" "test" {
+	org_id = grafana_organization.test.id
+	name  = "%[1]s"
+	description = "%[1]s desc"
+	uid = "%[1]s"
+	global = false
+	group = "%[1]s"
+	display_name = "%[1]s"
+	hidden = false
+	auto_increment_version = true
+}`, name)
+	}
+
+	return def
 }
 
 const roleConfigBasic = `
