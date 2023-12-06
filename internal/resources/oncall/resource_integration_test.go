grafana/terraform-provider-grafana

commit 48332319625a39c3bd1c6069d817160055beb73f

Nil check on oncall integration templates (#1185)
* Nil check on oncall integration templates
If a user defines an empty templates block, it currently panics
An empty block should be functionally equivalent to not defining the block at all

* Set an empty templates in the tests

* oops

* Add test for all three cases
- no templates block
- empty templates block
- non-empty templates block

+ fix all cases

internal/resources/oncall/resource_integration.go
@@ -202,7 +202,28 @@ func ResourceIntegration() *schema.Resource {
 					},
 				},
 				MaxItems:    1,
-				Description: "Jinja2 templates for Alert payload.",
+				Description: "Jinja2 templates for Alert payload. An empty templates block will be ignored.",
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					oldTemplate, newTemplate := d.GetChange("templates")
+
+					getTemplatesOrEmpty := func(template interface{}) map[string]interface{} {
+						list := template.([]interface{})
+						if len(list) > 0 && list[0] != nil {
+							return list[0].(map[string]interface{})
+						}
+						return map[string]interface{}{}
+					}
+					oldTemplateMap, newTemplateMap := getTemplatesOrEmpty(oldTemplate), getTemplatesOrEmpty(newTemplate)
+					if len(oldTemplateMap) != len(newTemplateMap) {
+						return false
+					}
+					for k, v := range oldTemplateMap {
+						if newTemplateMap[k] != v {
+							return false
+						}
+					}
+					return true
+				},
 			},
 		},
 	}
@@ -575,6 +596,10 @@ func expandTemplates(input []interface{}) *onCallAPI.Templates {
 	templates := onCallAPI.Templates{}
 
 	for _, r := range input {
+		if r == nil {
+			continue
+		}
+
 		inputMap := r.(map[string]interface{})
 		if inputMap["grouping_key"] != "" {
 			gk := inputMap["grouping_key"].(string)

internal/resources/oncall/resource_integration_test.go
@@ -23,12 +23,33 @@ func TestAccOnCallIntegration_basic(t *testing.T) {
 		CheckDestroy:      testAccCheckOnCallIntegrationResourceDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOnCallIntegrationConfig(rName, rType),
+				Config: testAccOnCallIntegrationConfig(rName, rType, ``),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckOnCallIntegrationResourceExists("grafana_oncall_integration.test-acc-integration"),
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "name", rName),
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "type", rType),
 					resource.TestCheckResourceAttrSet("grafana_oncall_integration.test-acc-integration", "link"),
+				),
+			},
+			{
+				Config: testAccOnCallIntegrationConfig(rName, rType, `templates {}`),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOnCallIntegrationResourceExists("grafana_oncall_integration.test-acc-integration"),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "name", rName),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "type", rType),
+					resource.TestCheckResourceAttrSet("grafana_oncall_integration.test-acc-integration", "link"),
+				),
+			},
+			{
+				Config: testAccOnCallIntegrationConfig(rName, rType, `templates {
+					grouping_key = "test"
+				}`),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOnCallIntegrationResourceExists("grafana_oncall_integration.test-acc-integration"),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "name", rName),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "type", rType),
+					resource.TestCheckResourceAttrSet("grafana_oncall_integration.test-acc-integration", "link"),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "templates.0.grouping_key", "test"),
 				),
 			},
 		},
@@ -49,7 +70,7 @@ func testAccCheckOnCallIntegrationResourceDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccOnCallIntegrationConfig(rName, rType string) string {
+func testAccOnCallIntegrationConfig(rName, rType, additionalConfigs string) string {
 	return fmt.Sprintf(`
 resource "grafana_oncall_integration" "test-acc-integration" {
 	name = "%s"
@@ -62,8 +83,10 @@ resource "grafana_oncall_integration" "test-acc-integration" {
 	        enabled = false
 	    }
 	}
+
+	%s
 }
-`, rName, rType)
+`, rName, rType, additionalConfigs)
 }
 
 func testAccCheckOnCallIntegrationResourceExists(name string) resource.TestCheckFunc {

commit e43886d5b72de8d5b5e825e90f448c74b9b3bd7d

Oncall Integration: Fix removing templates Closes https://github.com/grafana/terraform-provider-grafana/issues/1199 This was broken in https://github.com/grafana/terraform-provider-grafana/pull/1185 I added a test so that it doesn't occur again

internal/resources/oncall/resource_integration.go
@@ -204,6 +204,10 @@ func ResourceIntegration() *schema.Resource {
 				MaxItems:    1,
 				Description: "Jinja2 templates for Alert payload. An empty templates block will be ignored.",
 				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					if old != "" && new == "" || old == "" && new != "" {
+						return false
+					}
+
 					oldTemplate, newTemplate := d.GetChange("templates")
 
 					getTemplatesOrEmpty := func(template interface{}) map[string]interface{} {
@@ -218,7 +222,7 @@ func ResourceIntegration() *schema.Resource {
 						return false
 					}
 					for k, v := range oldTemplateMap {
-						if newTemplateMap[k] != v {
+						if fmt.Sprintf("%v", newTemplateMap[k]) != fmt.Sprintf("%v", v) {
 							return false
 						}
 					}

internal/resources/oncall/resource_integration_test.go
@@ -29,6 +29,7 @@ func TestAccOnCallIntegration_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "name", rName),
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "type", rType),
 					resource.TestCheckResourceAttrSet("grafana_oncall_integration.test-acc-integration", "link"),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "templates.#", "0"),
 				),
 			},
 			{
@@ -38,6 +39,7 @@ func TestAccOnCallIntegration_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "name", rName),
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "type", rType),
 					resource.TestCheckResourceAttrSet("grafana_oncall_integration.test-acc-integration", "link"),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "templates.#", "0"),
 				),
 			},
 			{
@@ -49,7 +51,19 @@ func TestAccOnCallIntegration_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "name", rName),
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "type", rType),
 					resource.TestCheckResourceAttrSet("grafana_oncall_integration.test-acc-integration", "link"),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "templates.#", "1"),
 					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "templates.0.grouping_key", "test"),
+				),
+			},
+			// Remove templates
+			{
+				Config: testAccOnCallIntegrationConfig(rName, rType, ``),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOnCallIntegrationResourceExists("grafana_oncall_integration.test-acc-integration"),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "name", rName),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "type", rType),
+					resource.TestCheckResourceAttrSet("grafana_oncall_integration.test-acc-integration", "link"),
+					resource.TestCheckResourceAttr("grafana_oncall_integration.test-acc-integration", "templates.#", "0"),
 				),
 			},
 		},