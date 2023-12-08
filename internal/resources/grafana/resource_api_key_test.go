4df33d832ada448a13ec97cbc7aebe16dd8cb28b
Julien Duchesne <julien.duchesne@grafana.com>
2023-12-08 08:57:39 -0500

    API Keys: Use OpenAPI for tests

--- internal/resources/grafana/resource_api_key_test.go
+++ internal/resources/grafana/resource_api_key_test.go
@@ -1,33 +1,29 @@
 package grafana_test
 
 import (
-	"errors"
 	"fmt"
-	"strconv"
 	"testing"
 
 	"github.com/grafana/grafana-openapi-client-go/models"
-	"github.com/grafana/terraform-provider-grafana/internal/common"
-	"github.com/grafana/terraform-provider-grafana/internal/resources/grafana"
 	"github.com/grafana/terraform-provider-grafana/internal/testutils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccGrafanaAuthKey_basic(t *testing.T) {
 	testutils.CheckOSSTestsEnabled(t)
 
+	var apiKey models.APIKeyDTO
 	testName := acctest.RandString(10)
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProviderFactories: testutils.ProviderFactories,
-		CheckDestroy:      testAccGrafanaAuthKeyCheckDestroy,
+		CheckDestroy:      apiKeyCheckExists.destroyed(&apiKey, nil),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccGrafanaAuthKeyConfig(testName, "Admin", 0, false),
 				Check: resource.ComposeTestCheckFunc(
-					testAccGrafanaAuthKeyCheckExists,
+					apiKeyCheckExists.exists("grafana_api_key.foo", &apiKey),
 					resource.TestMatchResourceAttr("grafana_api_key.foo", "id", defaultOrgIDRegexp),
 					resource.TestCheckResourceAttr("grafana_api_key.foo", "org_id", "1"),
 					resource.TestCheckResourceAttrSet("grafana_api_key.foo", "key"),
@@ -39,7 +35,7 @@ func TestAccGrafanaAuthKey_basic(t *testing.T) {
 			{
 				Config: testAccGrafanaAuthKeyConfig(testName+"-modified", "Viewer", 300, false),
 				Check: resource.ComposeTestCheckFunc(
-					testAccGrafanaAuthKeyCheckExists,
+					apiKeyCheckExists.exists("grafana_api_key.foo", &apiKey),
 					resource.TestMatchResourceAttr("grafana_api_key.foo", "id", defaultOrgIDRegexp),
 					resource.TestCheckResourceAttrSet("grafana_api_key.foo", "key"),
 					resource.TestCheckResourceAttr("grafana_api_key.foo", "name", testName+"-modified"),
@@ -55,16 +51,17 @@ func TestAccGrafanaAuthKey_inOrg(t *testing.T) {
 	testutils.CheckOSSTestsEnabled(t)
 
 	var org models.OrgDetailsDTO
+	var apiKey models.APIKeyDTO
 	testName := acctest.RandString(10)
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProviderFactories: testutils.ProviderFactories,
-		CheckDestroy:      testAccGrafanaAuthKeyCheckDestroy,
+		CheckDestroy:      orgCheckExists.destroyed(&org, nil),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccGrafanaAuthKeyConfig(testName, "Admin", 0, true),
 				Check: resource.ComposeTestCheckFunc(
-					testAccGrafanaAuthKeyCheckExists,
+					apiKeyCheckExists.exists("grafana_api_key.foo", &apiKey),
 					resource.TestCheckResourceAttrSet("grafana_api_key.foo", "key"),
 					resource.TestCheckResourceAttr("grafana_api_key.foo", "name", testName),
 					resource.TestCheckResourceAttr("grafana_api_key.foo", "role", "Admin"),
@@ -76,69 +73,16 @@ func TestAccGrafanaAuthKey_inOrg(t *testing.T) {
 					checkResourceIsInOrg("grafana_api_key.foo", "grafana_organization.test"),
 				),
 			},
+			// Check API key deletion within an organization
+			{
+				Config: testutils.WithoutResource(t, testAccGrafanaAuthKeyConfig(testName, "Admin", 0, true), "grafana_api_key.foo"),
+				Check: resource.ComposeTestCheckFunc(
+					orgCheckExists.exists("grafana_organization.test", &org),
+					apiKeyCheckExists.destroyed(&apiKey, &org),
+				),
+			},
 		},
 	})
-}
-
-func testAccGrafanaAuthKeyCheckExists(s *terraform.State) error {
-	return testAccGrafanaAuthKeyCheckExistsBool(s, true)
-}
-
-func testAccGrafanaAuthKeyCheckDestroy(s *terraform.State) error {
-	return testAccGrafanaAuthKeyCheckExistsBool(s, false)
-}
-
-func testAccGrafanaAuthKeyCheckExistsBool(s *terraform.State, shouldExist bool) error {
-	c := testutils.Provider.Meta().(*common.Client).GrafanaAPI
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "grafana_api_key" {
-			continue
-		}
-
-		orgID, idStr := grafana.SplitOrgResourceID(rs.Primary.ID)
-		id, err := strconv.ParseInt(idStr, 10, 32)
-		if err != nil {
-			return err
-		}
-
-		// If orgID > 1, always check that they key doesn't exist in the default org
-		if orgID > 1 {
-			keys, err := c.GetAPIKeys(false)
-			if err != nil {
-				return err
-			}
-
-			for _, key := range keys {
-				if key.ID == id {
-					return errors.New("API key exists in the default org")
-				}
-			}
-
-			c = c.WithOrgID(orgID)
-		}
-
-		keys, err := c.GetAPIKeys(false)
-		if err != nil {
-			return err
-		}
-
-		for _, key := range keys {
-			if key.ID == id {
-				if shouldExist {
-					return nil
-				} else {
-					return errors.New("API key still exists")
-				}
-			}
-		}
-
-		if shouldExist {
-			return errors.New("API key was not found")
-		}
-	}
-
-	return nil
 }
 
 func testAccGrafanaAuthKeyConfig(name, role string, secondsToLive int, inOrg bool) string {
