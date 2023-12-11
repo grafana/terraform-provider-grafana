7a1dbbcfbff3cf2366d442992fc3fb5af3a21282
Julien Duchesne <julien.duchesne@grafana.com>
2023-12-11 08:16:03 -0500

    Alerting Message Template: Use OpenAPI for tests (#1213)

--- internal/resources/grafana/resource_alerting_message_template_test.go
+++ internal/resources/grafana/resource_alerting_message_template_test.go
@@ -1,31 +1,28 @@
 package grafana_test
 
 import (
-	"fmt"
 	"testing"
 
-	gapi "github.com/grafana/grafana-api-golang-client"
-	"github.com/grafana/terraform-provider-grafana/internal/common"
+	"github.com/grafana/grafana-openapi-client-go/models"
 	"github.com/grafana/terraform-provider-grafana/internal/testutils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccMessageTemplate_basic(t *testing.T) {
 	testutils.CheckOSSTestsEnabled(t, ">=9.0.0")
 
-	var tmpl gapi.AlertingMessageTemplate
+	var tmpl models.NotificationTemplate
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProviderFactories: testutils.ProviderFactories,
 		// Implicitly tests deletion.
-		CheckDestroy: testMessageTemplateCheckDestroy(&tmpl),
+		CheckDestroy: alertingMessageTemplateCheckExists.destroyed(&tmpl, nil),
 		Steps: []resource.TestStep{
 			// Test creation.
 			{
 				Config: testutils.TestAccExample(t, "resources/grafana_message_template/resource.tf"),
 				Check: resource.ComposeTestCheckFunc(
-					testMessageTemplateCheckExists("grafana_message_template.my_template", &tmpl),
+					alertingMessageTemplateCheckExists.exists("grafana_message_template.my_template", &tmpl),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "name", "My Reusable Template"),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "template", "{{define \"My Reusable Template\" }}\n template content\n{{ end }}"),
 				),
@@ -46,7 +43,7 @@ func TestAccMessageTemplate_basic(t *testing.T) {
 EOT`,
 				}),
 				Check: resource.ComposeTestCheckFunc(
-					testMessageTemplateCheckExists("grafana_message_template.my_template", &tmpl),
+					alertingMessageTemplateCheckExists.exists("grafana_message_template.my_template", &tmpl),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "name", "My Reusable Template"),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "template", "{{define \"My Reusable Template\" }}\n template content\n{{ end }}"),
 				),
@@ -57,7 +54,7 @@ EOT`,
 					"template content": "different content",
 				}),
 				Check: resource.ComposeTestCheckFunc(
-					testMessageTemplateCheckExists("grafana_message_template.my_template", &tmpl),
+					alertingMessageTemplateCheckExists.exists("grafana_message_template.my_template", &tmpl),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "name", "My Reusable Template"),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "template", "{{define \"My Reusable Template\" }}\n different content\n{{ end }}"),
 				),
@@ -68,45 +65,12 @@ EOT`,
 					"My Reusable Template": "A Different Template",
 				}),
 				Check: resource.ComposeTestCheckFunc(
-					testMessageTemplateCheckExists("grafana_message_template.my_template", &tmpl),
+					alertingMessageTemplateCheckExists.exists("grafana_message_template.my_template", &tmpl),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "name", "A Different Template"),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "template", "{{define \"A Different Template\" }}\n template content\n{{ end }}"),
-					testMessageTemplateCheckDestroy(&gapi.AlertingMessageTemplate{Name: "My Reusable Template"}),
+					alertingMessageTemplateCheckExists.destroyed(&models.NotificationTemplate{Name: "My Reusable Template"}, nil),
 				),
 			},
 		},
 	})
-}
-
-func testMessageTemplateCheckExists(rname string, mt *gapi.AlertingMessageTemplate) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		resource, ok := s.RootModule().Resources[rname]
-		if !ok {
-			return fmt.Errorf("resource not found: %s, resources: %#v", rname, s.RootModule().Resources)
-		}
-
-		if resource.Primary.ID == "" {
-			return fmt.Errorf("resource id not set")
-		}
-
-		client := testutils.Provider.Meta().(*common.Client).GrafanaAPI
-		tmpl, err := client.MessageTemplate(resource.Primary.ID)
-		if err != nil {
-			return fmt.Errorf("error getting resource: %s", err)
-		}
-
-		*mt = *tmpl
-		return nil
-	}
-}
-
-func testMessageTemplateCheckDestroy(mt *gapi.AlertingMessageTemplate) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		client := testutils.Provider.Meta().(*common.Client).GrafanaAPI
-		tmpl, err := client.MessageTemplate(mt.Name)
-		if err == nil && tmpl != nil {
-			return fmt.Errorf("message template still exists on the server")
-		}
-		return nil
-	}
 }
