dd59a147a26e5f6099752b8c564bf34afb220af5
Julien Duchesne <julien.duchesne@grafana.com>
2024-01-22 11:26:55 -0500

    Add SM URL to tests (#1294)
    * Add SM URL to tests
    Useful when changing the test instance
    
    * Remove probe IDs
    
    * Remove more IDs

--- internal/resources/syntheticmonitoring/data_source_probes_test.go
+++ internal/resources/syntheticmonitoring/data_source_probes_test.go
@@ -16,12 +16,12 @@ func TestAccDataSourceProbes(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testutils.TestAccExample(t, "data-sources/grafana_synthetic_monitoring_probes/data-source.tf"),
-				Check:  resource.TestCheckResourceAttr("data.grafana_synthetic_monitoring_probes.main", "probes.Atlanta", "1"),
+				Check:  resource.TestCheckResourceAttrSet("data.grafana_synthetic_monitoring_probes.main", "probes.Atlanta"),
 			},
 			{
 				Config: testutils.TestAccExample(t, "data-sources/grafana_synthetic_monitoring_probes/with-deprecated.tf"),
 				// We're not checking for deprecated probes here because there may not be any, causing tests to fail.
-				Check: resource.TestCheckResourceAttr("data.grafana_synthetic_monitoring_probes.main", "probes.Atlanta", "1"),
+				Check: resource.TestCheckResourceAttrSet("data.grafana_synthetic_monitoring_probes.main", "probes.Atlanta"),
 			},
 		},
 	})
