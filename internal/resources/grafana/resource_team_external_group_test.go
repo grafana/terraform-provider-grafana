489708743a6c8f532cac02df9e1b1d0474bf4b3d
Julien Duchesne <julien.duchesne@grafana.com>
2024-02-19 14:51:02 -0500

    Add version restrictions

--- internal/resources/grafana/resource_team_external_group_test.go
+++ internal/resources/grafana/resource_team_external_group_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestAccTeamExternalGroup_basic(t *testing.T) {
-	testutils.CheckEnterpriseTestsEnabled(t)
+	testutils.CheckEnterpriseTestsEnabled(t, ">=9.0.0")
 
 	name := acctest.RandString(10)
 	var team models.TeamDTO
