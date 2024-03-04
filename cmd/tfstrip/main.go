2ee802c6a47a58eb79347cea45e74a987f1346f3
Duologic <jeroen@simplistic.be>
2024-03-04 23:02:05 +0100

    wip: read TF file

---
+++ cmd/tfstrip/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+)
+
+type Config struct {
+	LogLevel string `hcl:"log_level"`
+}
+
+func main() {
+	src, err := os.ReadFile("/home/duologic/git/grafana/terraform-provider-grafana/out/stack-terraformprovidergrafana2-resources.tf")
+	if err != nil {
+		panic(err)
+	}
+
+	file, diags := hclwrite.ParseConfig(src, "decode.tf", hcl.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		err := errors.New("an error occurred")
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, v := range file.Body().Blocks() {
+		log.Printf("%+v", v.Body().Attributes())
+	}
+
+}
