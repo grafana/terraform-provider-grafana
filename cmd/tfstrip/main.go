06fb2d7f633d80d507bf10b9d7bc0aa4dc015115
Duologic <jeroen@simplistic.be>
2024-03-05 15:28:20 +0100

    wip: traverse out path

--- cmd/tfstrip/main.go
+++ cmd/tfstrip/main.go
@@ -4,36 +4,67 @@ import (
 	"errors"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 )
 
-type Config struct {
-	LogLevel string `hcl:"log_level"`
-}
-
 func main() {
-	src, err := os.ReadFile("/home/duologic/git/grafana/terraform-provider-grafana/out/stack-terraformprovidergrafana2-resources.tf")
+	path := os.Getenv("TFGEN_OUT_PATH")
+	if path == "" {
+		log.Fatal("TFGEN_OUT_PATH environment variable must be set")
+	}
+	path, err := filepath.Abs(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	items, err := os.ReadDir(path)
 	if err != nil {
 		panic(err)
 	}
 
-	file, diags := hclwrite.ParseConfig(src, "decode.tf", hcl.Pos{Line: 1, Column: 1})
-	if diags.HasErrors() {
-		err := errors.New("an error occurred")
+	for _, item := range items {
+		if item.IsDir() {
+			continue
+		}
+
+		if !strings.HasSuffix(item.Name(), ".tf") {
+			continue
+		}
+
+		fpath := filepath.Join(path, item.Name())
+		src, err := os.ReadFile(fpath)
 		if err != nil {
 			panic(err)
 		}
-	}
-	for _, block := range file.Body().Blocks() {
-		for name, attribute := range block.Body().Attributes() {
-			log.Printf("%+v", name)
-			if string(attribute.Expr().BuildTokens(nil).Bytes()) == " null" {
-				block.Body().RemoveAttribute(name)
+
+		file, diags := hclwrite.ParseConfig(src, item.Name(), hcl.Pos{Line: 1, Column: 1})
+		if diags.HasErrors() {
+			err := errors.New("an error occurred")
+			if err != nil {
+				panic(err)
+			}
+		}
+		hasChanges := false
+		for _, block := range file.Body().Blocks() {
+			for name, attribute := range block.Body().Attributes() {
+				if string(attribute.Expr().BuildTokens(nil).Bytes()) == " null" {
+					hasChanges = true
+					block.Body().RemoveAttribute(name)
+				}
+				if string(attribute.Expr().BuildTokens(nil).Bytes()) == " {}" {
+					hasChanges = true
+					block.Body().RemoveAttribute(name)
+				}
+			}
+		}
+		if hasChanges {
+			log.Printf("Updating file: %s\n", item.Name())
+			if err := os.WriteFile(fpath, file.Bytes(), 0644); err != nil {
+				panic(err)
 			}
 		}
 	}
-
-	log.Print(string(file.Bytes()))
 }
