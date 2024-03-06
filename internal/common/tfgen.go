69ce4e990139b1154736923c771efcc2be92695c
Julien Duchesne <julien.duchesne@grafana.com>
2024-03-06 10:34:39 -0500

    Post-processing wrap dashboards + jsonencode

--- internal/common/tfgen.go
+++ internal/common/tfgen.go
@@ -11,22 +11,15 @@ import (
 	"strings"
 
 	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/zclconf/go-cty/cty"
 )
 
 func StripDefaults(fpath string, extraFieldsToRemove map[string]string) error {
-	src, err := os.ReadFile(fpath)
+	file, err := readHCLFile(fpath)
 	if err != nil {
-		panic(err)
-	}
-
-	file, diags := hclwrite.ParseConfig(src, fpath, hcl.Pos{Line: 1, Column: 1})
-	if diags.HasErrors() {
-		err := errors.New("an error occurred")
-		if err != nil {
-			return err
-		}
+		return err
 	}
 
 	hasChanges := false
@@ -40,6 +33,148 @@ func StripDefaults(fpath string, extraFieldsToRemove map[string]string) error {
 		return os.WriteFile(fpath, file.Bytes(), 0644)
 	}
 	return nil
+}
+
+func WrapJSONFieldsInFunction(fpath string) error {
+	file, err := readHCLFile(fpath)
+	if err != nil {
+		return err
+	}
+
+	hasChanges := false
+	// Find json attributes and use jsonencode
+	for _, block := range file.Body().Blocks() {
+		for key, attr := range block.Body().Attributes() {
+			asMap, err := attributeToMap(attr)
+			if err != nil || asMap == nil {
+				continue
+			}
+			tokens := hclwrite.TokensForValue(HCL2ValueFromConfigValue(asMap))
+			block.Body().SetAttributeRaw(key, hclwrite.TokensForFunctionCall("jsonencode", tokens))
+			hasChanges = true
+		}
+	}
+
+	if hasChanges {
+		log.Printf("Updating file: %s\n", fpath)
+		return os.WriteFile(fpath, file.Bytes(), 0644)
+	}
+	return nil
+}
+
+func AbstractDashboards(fpath string) error {
+	fDir := filepath.Dir(fpath)
+	outPath := filepath.Join(fDir, "files")
+
+	file, err := readHCLFile(fpath)
+	if err != nil {
+		return err
+	}
+
+	hasChanges := false
+	dashboardJsons := map[string][]byte{}
+	for _, block := range file.Body().Blocks() {
+		labels := block.Labels()
+		if len(labels) == 0 || labels[0] != "grafana_dashboard" {
+			continue
+		}
+
+		dashboard, err := attributeToJson(block.Body().GetAttribute("config_json"))
+		if err != nil {
+			return err
+		}
+
+		if dashboard == nil {
+			continue
+		}
+
+		writeTo := filepath.Join(outPath, fmt.Sprintf("%s.json", block.Labels()[1]))
+
+		// Replace $${ with ${ in the json. No need to escape in the json file
+		dashboard = []byte(strings.ReplaceAll(string(dashboard), "$${", "${"))
+		dashboardJsons[writeTo] = dashboard
+
+		// Hacky relative path with interpolation
+		relativePath := strings.ReplaceAll(writeTo, fDir, "")
+		pathWithInterpolation := hclwrite.Tokens{
+			{Type: hclsyntax.TokenOQuote, Bytes: []byte(`"`)},
+			{Type: hclsyntax.TokenTemplateInterp, Bytes: []byte(`${`)},
+			{Type: hclsyntax.TokenIdent, Bytes: []byte(`path.module`)},
+			{Type: hclsyntax.TokenTemplateSeqEnd, Bytes: []byte(`}`)},
+			{Type: hclsyntax.TokenQuotedLit, Bytes: []byte(relativePath)},
+			{Type: hclsyntax.TokenCQuote, Bytes: []byte(`"`)},
+		}
+
+		block.Body().SetAttributeRaw(
+			"config_json",
+			hclwrite.TokensForFunctionCall("file", pathWithInterpolation),
+		)
+
+		hasChanges = true
+	}
+	if hasChanges {
+		log.Printf("Updating file: %s\n", fpath)
+		os.Mkdir(outPath, 0755)
+		for writeTo, dashboard := range dashboardJsons {
+			err := os.WriteFile(writeTo, dashboard, 0644)
+			if err != nil {
+				panic(err)
+			}
+		}
+		return os.WriteFile(fpath, file.Bytes(), 0644)
+	}
+	return nil
+}
+
+func attributeToMap(attr *hclwrite.Attribute) (map[string]interface{}, error) {
+	s := string(attr.Expr().BuildTokens(nil).Bytes())
+	s = strings.TrimPrefix(s, " ")
+	if !strings.HasPrefix(s, "\"") {
+		// if expr is not a string, assume it's already converted, return (idempotency
+		return nil, nil
+	}
+	s, err := strconv.Unquote(s)
+	if err != nil {
+		return nil, err
+	}
+	s = strings.ReplaceAll(s, "$${", "${") // These are escaped interpolations
+
+	var jsonMap map[string]interface{}
+	err = json.Unmarshal([]byte(s), &jsonMap)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonMap, nil
+}
+
+func attributeToJson(attr *hclwrite.Attribute) ([]byte, error) {
+	jsonMap, err := attributeToMap(attr)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonMarshalled, err := json.MarshalIndent(jsonMap, "", "\t")
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonMarshalled, nil
+
+}
+
+func readHCLFile(fpath string) (*hclwrite.File, error) {
+	src, err := os.ReadFile(fpath)
+	if err != nil {
+		return nil, err
+	}
+
+	file, diags := hclwrite.ParseConfig(src, fpath, hcl.Pos{Line: 1, Column: 1})
+	if diags.HasErrors() {
+		return nil, errors.New(diags.Error())
+	}
+
+	return file, nil
 }
 
 func stripDefaultsFromBlock(block *hclwrite.Block, extraFieldsToRemove map[string]string) bool {
@@ -81,88 +216,49 @@ func stripDefaultsFromBlock(block *hclwrite.Block, extraFieldsToRemove map[strin
 	return hasChanges
 }
 
-func AbstractDashboards(fpath string) error {
-	path := filepath.Dir(fpath)
-	outPath := filepath.Join(path, "files")
+// BELOW IS FROM https://github.com/hashicorp/terraform/blob/main/internal/configs/hcl2shim/values.go
 
-	src, err := os.ReadFile(fpath)
-	if err != nil {
-		panic(err)
+// UnknownVariableValue is a sentinel value that can be used
+// to denote that the value of a variable is unknown at this time.
+// RawConfig uses this information to build up data about
+// unknown keys.
+const UnknownVariableValue = "74D93920-ED26-11E3-AC10-0800200C9A66"
+
+// HCL2ValueFromConfigValue is the opposite of configValueFromHCL2: it takes
+// a value as would be returned from the old interpolator and turns it into
+// a cty.Value so it can be used within, for example, an HCL2 EvalContext.
+func HCL2ValueFromConfigValue(v interface{}) cty.Value {
+	if v == nil {
+		return cty.NullVal(cty.DynamicPseudoType)
+	}
+	if v == UnknownVariableValue {
+		return cty.DynamicVal
 	}
 
-	file, diags := hclwrite.ParseConfig(src, fpath, hcl.Pos{Line: 1, Column: 1})
-	if diags.HasErrors() {
-		err := errors.New("an error occurred")
-		if err != nil {
-			return err
+	switch tv := v.(type) {
+	case bool:
+		return cty.BoolVal(tv)
+	case string:
+		return cty.StringVal(tv)
+	case int:
+		return cty.NumberIntVal(int64(tv))
+	case float64:
+		return cty.NumberFloatVal(tv)
+	case []interface{}:
+		vals := make([]cty.Value, len(tv))
+		for i, ev := range tv {
+			vals[i] = HCL2ValueFromConfigValue(ev)
 		}
-	}
-
-	hasChanges := false
-	dashboardJsons := map[string][]byte{}
-	for _, block := range file.Body().Blocks() {
-		labels := block.Labels()
-		if len(labels) == 0 || labels[0] != "grafana_dashboard" {
-			continue
+		return cty.TupleVal(vals)
+	case map[string]interface{}:
+		vals := map[string]cty.Value{}
+		for k, ev := range tv {
+			vals[k] = HCL2ValueFromConfigValue(ev)
 		}
-
-		dashboard, err := dashboardToJson(block)
-		if err != nil {
-			return err
-		}
-
-		if dashboard == nil {
-			continue
-		}
-
-		writeTo := filepath.Join(outPath, fmt.Sprintf("%s.json", block.Labels()[1]))
-
-		dashboardJsons[writeTo] = dashboard
-
-		block.Body().SetAttributeRaw(
-			"config_json",
-			hclwrite.TokensForFunctionCall("file",
-				hclwrite.TokensForValue(cty.StringVal(writeTo))))
-
-		hasChanges = true
+		return cty.ObjectVal(vals)
+	default:
+		// HCL/HIL should never generate anything that isn't caught by
+		// the above, so if we get here something has gone very wrong.
+		panic(fmt.Errorf("can't convert %#v to cty.Value", v))
 	}
-	if hasChanges {
-		log.Printf("Updating file: %s\n", fpath)
-		os.Mkdir(outPath, 0755)
-		for writeTo, dashboard := range dashboardJsons {
-			err := os.WriteFile(writeTo, dashboard, 0644)
-			if err != nil {
-				panic(err)
-			}
-		}
-		return os.WriteFile(fpath, file.Bytes(), 0644)
-	}
-	return nil
-}
-
-func dashboardToJson(block *hclwrite.Block) ([]byte, error) {
-	s := string(block.Body().GetAttribute("config_json").Expr().BuildTokens(nil).Bytes())
-	s = strings.TrimPrefix(s, " ")
-	if !strings.HasPrefix(s, "\"") {
-		// if expr is not a string, assume it's already converted, return (idempotency
-		return nil, nil
-	}
-	s, err := strconv.Unquote(s)
-	if err != nil {
-		return nil, err
-	}
-
-	var jsonMap map[string]interface{}
-	err = json.Unmarshal([]byte(s), &jsonMap)
-	if err != nil {
-		return nil, err
-	}
-
-	jsonMarshalled, err := json.MarshalIndent(jsonMap, "", "\t")
-	if err != nil {
-		return nil, err
-	}
-
-	return jsonMarshalled, nil
-
 }
