6826dd42753d098e6b9acc8261355df71e17600f
Russ <8377044+rdubrock@users.noreply.github.com>
2024-03-29 13:57:31 -0800

    SM: add support for scripted checks
    This adds support for Synthetic Monitoring scripted checks.
    
    The provided example nudges the user towards using a local_file
    datasource, instead of simply specifying the script as a string inline.

--- internal/resources/syntheticmonitoring/resource_check.go
+++ internal/resources/syntheticmonitoring/resource_check.go
@@ -119,6 +119,23 @@ var (
 				MaxItems:    1,
 				Elem:        syntheticMonitoringCheckSettingsMultiHTTP,
 			},
+			"scripted": {
+				Description: "Settings for scripted check. See https://grafana.com/docs/grafana-cloud/testing/synthetic-monitoring/create-checks/checks/k6/.",
+				Type:        schema.TypeSet,
+				Optional:    true,
+				MaxItems:    1,
+				Elem:        syntheticMonitoringCheckSettingsScripted,
+			},
+		},
+	}
+
+	syntheticMonitoringCheckSettingsScripted = &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"script": {
+				Type:     schema.TypeString,
+				Optional: false,
+				Required: true,
+			},
 		},
 	}
 
@@ -629,7 +646,6 @@ var (
 
 func resourceCheck() *common.Resource {
 	schema := &schema.Resource{
-
 		Description: `
 Synthetic Monitoring checks are tests that run on selected probes at defined
 intervals and report metrics and logs back to your Grafana Cloud account. The
@@ -687,7 +703,8 @@ multiple checks for a single endpoint to check different capabilities.
 				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
 					// Suppress diff if it's a multihttp check with a timeout of 5000 (default timeout for those)
 					// and it's being changed to 3000 (default timeout set here).
-					if d.Get("settings.0.multihttp.0") != nil &&
+					doSuppress := d.Get("settings.0.multihttp.0") != nil || d.Get("settings.0.scripted") != nil
+					if doSuppress &&
 						old == strconv.Itoa(checkMultiHTTPDefaultTimeout) &&
 						new == strconv.Itoa(checkDefaultTimeout) {
 						return true
@@ -1079,6 +1096,17 @@ func resourceCheckRead(ctx context.Context, d *schema.ResourceData, c *smapi.Cli
 		settings.Add(map[string]any{
 			"multihttp": multiHTTP,
 		})
+	case chk.Settings.Scripted != nil:
+		scripted := schema.NewSet(
+			schema.HashResource(syntheticMonitoringCheckSettingsScripted),
+			[]any{},
+		)
+		scripted.Add(map[string]any{
+			"script": string(chk.Settings.Scripted.Script),
+		})
+		settings.Add(map[string]any{
+			"scripted": scripted,
+		})
 	}
 
 	d.Set("settings", settings)
@@ -1139,7 +1167,7 @@ func makeCheck(d *schema.ResourceData) (*sm.Check, error) {
 	}
 
 	timeout := int64(d.Get("timeout").(int))
-	if timeout == checkDefaultTimeout && settings.Multihttp != nil {
+	if timeout == checkDefaultTimeout && (settings.Multihttp != nil || settings.Scripted != nil) {
 		timeout = checkMultiHTTPDefaultTimeout
 	}
 
@@ -1496,6 +1524,14 @@ func makeCheckSettings(settings map[string]interface{}) (sm.CheckSettings, error
 		err := makeMultiHTTPSettings(m, &cs)
 		if err != nil {
 			return cs, fmt.Errorf("invalid MultiHTTP settings: %w", err)
+		}
+	}
+
+	scripted := settings["scripted"].(*schema.Set).List()
+	if len(scripted) > 0 {
+		s := scripted[0].(map[string]interface{})
+		cs.Scripted = &sm.ScriptedSettings{
+			Script: []byte(s["script"].(string)),
 		}
 	}
 
