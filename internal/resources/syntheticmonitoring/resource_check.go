4229e66bb1a251de8988e73d80c8e7cb14bce45e
tomaszgawliksa <111283611+tomaszgawliksa@users.noreply.github.com>
2024-05-13 17:08:47 +0100

    Update resource_check.go

--- internal/resources/syntheticmonitoring/resource_check.go
+++ internal/resources/syntheticmonitoring/resource_check.go
@@ -672,7 +672,7 @@ multiple checks for a single endpoint to check different capabilities.
 			},
 			"frequency": {
 				Description: "How often the check runs in milliseconds (the value is not truly a \"frequency\" but a \"period\"). " +
-					"The minimum acceptable value is 10 second (10000 ms), and the maximum is 1 hour (3600000 ms).",
+					"The minimum acceptable value is 1 second (1000 ms), and the maximum is 1 hour (3600000 ms).",
 				Type:         schema.TypeInt,
 				Optional:     true,
 				Default:      60000,
