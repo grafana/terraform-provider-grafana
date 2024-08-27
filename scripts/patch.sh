f2233731df534078782aa188f7b03fe78d2c23c0
Pablo Balbi <pablo.l.balbi@gmail.com>
2024-08-27 13:44:19 -0300

    added script

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -2,11 +2,12 @@
 
 PATCH_FILE="0001-testing-commit.patch"
 
-if git apply --check ${PATCH_FILE}; then
+git apply --check ${PATCH_FILE} &> /dev/null
+if [ $? -eq 0 ]; then
 	echo "applying patch for running against local k3d..."
 	git am < ${PATCH_FILE}
 else
 	echo "there's an error when trying to add the patch"
-	echo "Run: git apply --check ${PATCH_FILE}" for details"
+	echo "Run: \"git apply --check ${PATCH_FILE}\" for details"
 	exit 1
 fi
