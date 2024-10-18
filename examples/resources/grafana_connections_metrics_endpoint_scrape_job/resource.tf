9f1a456f4bf47ca7d77798039da8ce1a960383ad
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2024-10-18 11:49:52 +0200

    Put back enabled field into metrics endpoint resources

--- examples/resources/grafana_connections_metrics_endpoint_scrape_job/resource.tf
+++ examples/resources/grafana_connections_metrics_endpoint_scrape_job/resource.tf
@@ -1,6 +1,7 @@
 resource "grafana_connections_metrics_endpoint_scrape_job" "test" {
   stack_id                      = "1"
   name                          = "scrape-job-name"
+  enabled                       = true
   authentication_method         = "basic"
   authentication_basic_username = "my-username"
   authentication_basic_password = "my-password"
