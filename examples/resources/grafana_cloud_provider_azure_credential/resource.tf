6df460141d0b6c35636a68b65dc6397c09e0417d
andriikushch <andrii.kushch@grafana.com>
2024-11-14 15:55:11 +0100

    Add docs

--- examples/resources/grafana_cloud_provider_azure_credential/resource.tf
+++ examples/resources/grafana_cloud_provider_azure_credential/resource.tf
@@ -1,7 +1,7 @@
 resource "grafana_cloud_provider_azure_credential" "test" {
-  stack_id = "1"
-  name = "test-name"
-  client_id = "my-client-id"
+  stack_id      = "1"
+  name          = "test-name"
+  client_id     = "my-client-id"
   client_secret = "my-client-secret"
-  tenant_id = "my-tenant-id"
+  tenant_id     = "my-tenant-id"
 }
