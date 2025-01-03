b5231bdcca702903a2b1f0072b4b13fe7e8c98b3
andriikushch <andrii.kushch@gmail.com>
2025-01-03 09:59:11 +0100

    Add support for the Azure credentials resource (#1907)
    * Add support for the Azure credentials resource

---
+++ examples/data-sources/grafana_cloud_provider_azure_credential/data-source.tf
@@ -0,0 +1,22 @@
+resource "grafana_cloud_provider_azure_credential" "test" {
+  stack_id      = "1"
+  name          = "test-name"
+  client_id     = "my-client-id"
+  client_secret = "my-client-secret"
+  tenant_id     = "my-tenant-id"
+
+  resource_discovery_tag_filter {
+    key   = "key-1"
+    value = "value-1"
+  }
+  resource_discovery_tag_filter {
+    key   = "key-2"
+    value = "value-2"
+  }
+}
+
+
+data "grafana_cloud_provider_azure_credential" "test" {
+  stack_id    = grafana_cloud_provider_azure_credential.test.stack_id
+  resource_id = grafana_cloud_provider_azure_credential.test.resource_id
+}
---
+++ examples/resources/grafana_cloud_provider_azure_credential/import.sh
@@ -0,0 +1,1 @@
+terraform import grafana_cloud_provider_azure_credential.name "{{ stack_id }}:{{ resource_id }}"
