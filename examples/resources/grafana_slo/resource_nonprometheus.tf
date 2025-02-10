e60b74177d71f939708cd98d1160a6ef2c71387c
Leo DiCara <leo.dicara@grafana.com>
2025-02-10 15:00:42 -0800

    chore(bigTent slo): WIP update documentation

---
+++ examples/resources/grafana_slo/resource_nonprometheus.tf
@@ -0,0 +1,44 @@
+resource "grafana_slo" "test" {
+  name        = "Terraform Testing"
+  description = "Terraform Description"
+  query {
+    freeform {
+      query = "sum(rate(apiserver_request_total{code!=\"500\"}[$__rate_interval])) / sum(rate(apiserver_request_total[$__rate_interval]))"
+    }
+    type = "freeform"
+  }
+  objectives {
+    value  = 0.995
+    window = "30d"
+  }
+  destination_datasource {
+    uid = "grafanacloud-prom"
+  }
+  label {
+    key   = "slo"
+    value = "terraform"
+  }
+  alerting {
+    fastburn {
+      annotation {
+        key   = "name"
+        value = "SLO Burn Rate Very High"
+      }
+      annotation {
+        key   = "description"
+        value = "Error budget is burning too fast"
+      }
+    }
+
+    slowburn {
+      annotation {
+        key   = "name"
+        value = "SLO Burn Rate High"
+      }
+      annotation {
+        key   = "description"
+        value = "Error budget is burning too fast"
+      }
+    }
+  }
+}
