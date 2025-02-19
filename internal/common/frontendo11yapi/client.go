659ded78c11df791535081b613fb2418df02f575
Kostas Pelelis <13784424+kpelelis@users.noreply.github.com>
2025-02-19 14:49:34 +0200

    feat: Add Frontend Observability provider (#2001)
    This commit adds the groundwork to support Frontend Observability as
    code. More specificailly, the change contains
    
    1. A Go HTTP client that consumes the Apps API (Create/Read/Update/Delete)
    2. Data Source and Resource version of the Frontend Observability App
       entity.

---
+++ internal/common/frontendo11yapi/client.go
@@ -0,0 +1,234 @@
+package frontendo11yapi
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
+)
+
+var _ json.Unmarshaler = &App{}
+
+type Client struct {
+	authToken      string
+	client         *http.Client
+	cloudAPIHost   string
+	userAgent      string
+	defaultHeaders map[string]string
+}
+
+const (
+	defaultRetries = 3
+	defaultTimeout = 90 * time.Second
+	pathPrefix     = "/api/v1"
+)
+
+func NewClient(cloudAPIHost string, authToken string, client *http.Client, userAgent string, defaultHeaders map[string]string) (*Client, error) {
+	if client == nil {
+		retryClient := retryablehttp.NewClient()
+		retryClient.RetryMax = defaultRetries
+		client = retryClient.StandardClient()
+		client.Timeout = defaultTimeout
+	}
+
+	return &Client{
+		authToken:      authToken,
+		client:         client,
+		cloudAPIHost:   cloudAPIHost,
+		userAgent:      userAgent,
+		defaultHeaders: defaultHeaders,
+	}, nil
+}
+
+// LogLabels ...
+type LogLabel struct {
+	ID    int64  `json:"id,omitempty"`
+	Label string `json:"label"`
+	Value string `json:"value"`
+}
+
+// AllowedOrigin ...
+type AllowedOrigin struct {
+	ID  int64  `json:"id,omitempty"`
+	URL string `json:"url"`
+}
+
+type AppSetting struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+// App model
+type App struct {
+	ID                 int64             `json:"id,omitempty"`
+	Name               string            `json:"name,omitempty"`
+	Key                string            `json:"appKey,omitempty"`
+	ExtraLogLabels     []LogLabel        `json:"extraLogLabels,omitempty"`
+	CORSAllowedOrigins []AllowedOrigin   `json:"corsOrigins,omitempty"`
+	AllowedRate        uint64            `json:"allowedRate,omitempty"`
+	Settings           map[string]string `json:"settings,omitempty"`
+
+	CreatedAt *time.Time `json:"createdAt,omitempty"`
+	UpdatedAt *time.Time `json:"updatedAt,omitempty"`
+	DeletedAt *time.Time `json:"deletedAt,omitempty"`
+}
+
+func (c *Client) Host() string {
+	return c.cloudAPIHost
+}
+
+func (c *Client) CreateApp(ctx context.Context, baseURL string, stackID int64, appData App) (App, error) {
+	path := fmt.Sprintf("%s/app", pathPrefix)
+	var app App
+	err := c.doAPIRequest(ctx, baseURL, stackID, http.MethodPost, path, &appData, &app)
+	if err != nil {
+		return App{}, fmt.Errorf("failed to create faro app %q: %w", appData.Name, err)
+	}
+	return app, nil
+}
+
+func (c *Client) GetApps(ctx context.Context, baseURL string, stackID int64) ([]App, error) {
+	path := fmt.Sprintf("%s/app", pathPrefix)
+	var apps []App
+	err := c.doAPIRequest(ctx, baseURL, stackID, http.MethodGet, path, nil, &apps)
+	if err != nil {
+		return []App{}, fmt.Errorf("failed to get faro apps: %w", err)
+	}
+	return apps, nil
+}
+
+func (c *Client) GetApp(ctx context.Context, baseURL string, stackID int64, appID int64) (App, error) {
+	path := fmt.Sprintf("%s/app/%d", pathPrefix, appID)
+	var app App
+	err := c.doAPIRequest(ctx, baseURL, stackID, http.MethodGet, path, nil, &app)
+	if err != nil {
+		return App{}, fmt.Errorf("failed to get faro app: %w", err)
+	}
+	return app, nil
+}
+
+func (c *Client) UpdateApp(ctx context.Context, baseURL string, stackID int64, appID int64, appData App) (App, error) {
+	path := fmt.Sprintf("%s/app/%d", pathPrefix, appID)
+	var app App
+	err := c.doAPIRequest(ctx, baseURL, stackID, http.MethodPut, path, &appData, &app)
+	if err != nil {
+		return App{}, fmt.Errorf("failed to update faro app %q: %w", appData.Name, err)
+	}
+	return app, nil
+}
+
+func (c *Client) DeleteApp(ctx context.Context, baseURL string, stackID int64, appID int64) error {
+	path := fmt.Sprintf("%s/app/%d", pathPrefix, appID)
+	err := c.doAPIRequest(ctx, baseURL, stackID, http.MethodDelete, path, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to delete faro app id=%d: %w", appID, err)
+	}
+	return nil
+}
+
+var (
+	ErrNotFound     = fmt.Errorf("not found")
+	ErrUnauthorized = fmt.Errorf("request not authorized for stack")
+)
+
+func (c *Client) doAPIRequest(ctx context.Context, rawURL string, stackID int64, method string, path string, body any, responseData any) error {
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return fmt.Errorf("failed to parse frontend o11y API url: %w", err)
+	}
+
+	var reqBodyBytes io.Reader
+	if body != nil {
+		bs, err := json.Marshal(body)
+		if err != nil {
+			return fmt.Errorf("failed to marshal request body: %w", err)
+		}
+		reqBodyBytes = bytes.NewReader(bs)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, parsedURL.String()+path, reqBodyBytes)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	for k, v := range c.defaultHeaders {
+		req.Header.Add(k, v)
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %d:%s", stackID, c.authToken))
+	req.Header.Add("X-Scope-OrgID", fmt.Sprintf("%d", stackID))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("User-Agent", c.userAgent)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to do request: %w", err)
+	}
+
+	bodyContents, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	switch {
+	case resp.StatusCode == http.StatusNotFound:
+		return ErrNotFound
+	case resp.StatusCode == http.StatusUnauthorized:
+		return ErrUnauthorized
+	case resp.StatusCode >= 400:
+		return fmt.Errorf("status: %d", resp.StatusCode)
+	case responseData == nil || resp.StatusCode == http.StatusNoContent:
+		return nil
+	}
+
+	err = json.Unmarshal(bodyContents, &responseData)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal response body: %w", err)
+	}
+	return nil
+}
+
+func (a *App) UnmarshalJSON(input []byte) error {
+	// We use a type alias here to avoid
+	// 1. a stack overflow from recursively calling App.UnmarshalJSON() (see: https://groups.google.com/g/golang-nuts/c/o22NS2yTe2A)
+	// 2. needing to pull individual fields out of a map[string]interface{}
+	type RawApp App
+
+	raw := &RawApp{}
+
+	err := json.Unmarshal(input, raw)
+	if err != nil {
+		return err
+	}
+
+	corsSet := make(map[string]struct{})
+	for _, corsOrigin := range raw.CORSAllowedOrigins {
+		normalizedURL := strings.ToLower(corsOrigin.URL)
+		if _, ok := corsSet[normalizedURL]; ok {
+			continue
+		}
+
+		a.CORSAllowedOrigins = append(a.CORSAllowedOrigins, corsOrigin)
+		corsSet[corsOrigin.URL] = struct{}{}
+	}
+
+	a.ID = raw.ID
+	a.Name = raw.Name
+	a.Key = raw.Key
+	a.ExtraLogLabels = raw.ExtraLogLabels
+	a.Settings = raw.Settings
+	a.AllowedRate = raw.AllowedRate
+	a.CreatedAt = raw.CreatedAt
+	a.UpdatedAt = raw.UpdatedAt
+	a.DeletedAt = raw.DeletedAt
+
+	return nil
+}
---
+++ internal/common/frontendo11yapi/client_test.go
@@ -0,0 +1,560 @@
+package frontendo11yapi_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/terraform-provider-grafana/v3/internal/common/frontendo11yapi"
+)
+
+func Test_NewClient(t *testing.T) {
+	defaultHeaders := map[string]string{}
+
+	t.Run("successfully creates a new client", func(t *testing.T) {
+		client, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", &http.Client{}, "some-user-agent", defaultHeaders)
+
+		assert.NotNil(t, client)
+		assert.NoError(t, err)
+	})
+}
+
+func TestClient_CreateApp(t *testing.T) {
+	defaultHeaders := map[string]string{"Grafana-Terraform-Provider": "True"}
+	t.Run("successfully sends request and receives response", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPost, r.Method)
+			assert.Equal(t, "True", r.Header.Get("Grafana-Terraform-Provider"))
+			assert.Equal(t, "/api/v1/app", r.URL.Path)
+			requestBody, err := io.ReadAll(r.Body)
+			require.NoError(t, err)
+			assert.JSONEq(t, `
+			{
+				"name": "Test App",
+    			"corsOrigins": [{
+    			    "url": "*"
+    			}],
+				"settings": {
+					"geolcation.enabled": "1"
+				}
+			}`, string(requestBody))
+
+			w.WriteHeader(http.StatusCreated)
+			_, _ = w.Write([]byte(`
+			{
+				"id": 1,
+				"name": "Test App",
+    			"appKey": "foobar",
+    			"corsOrigins": [{
+    			    "id": 1,
+    			    "url": "*"
+    			}],
+				"settings": {
+					"geolcation.enabled": "1"
+				},
+				"allowedRate": 0
+			}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		acutalApp, err := c.CreateApp(context.Background(), svr.URL, 1, frontendo11yapi.App{
+			Name: "Test App",
+			CORSAllowedOrigins: []frontendo11yapi.AllowedOrigin{
+				{
+					URL: "*",
+				},
+			},
+			Settings: map[string]string{
+				"geolcation.enabled": "1",
+			},
+		})
+		assert.NoError(t, err)
+
+		assert.Equal(t, frontendo11yapi.App{
+			ID:   1,
+			Name: "Test App",
+			Key:  "foobar",
+			CORSAllowedOrigins: []frontendo11yapi.AllowedOrigin{
+				{
+					ID:  1,
+					URL: "*",
+				},
+			},
+			Settings: map[string]string{
+				"geolcation.enabled": "1",
+			},
+			AllowedRate: 0,
+		}, acutalApp)
+	})
+
+	t.Run("sets auth token, content type, user agent", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "Bearer 1:some token", r.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+			assert.Equal(t, "some-user-agent", r.Header.Get("User-Agent"))
+			_, _ = fmt.Fprintf(w, `{}`)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.CreateApp(context.Background(), svr.URL, 1, frontendo11yapi.App{})
+		require.NoError(t, err)
+	})
+
+	t.Run("returns error when API responds 500", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(500)
+			_, _ = w.Write([]byte(`{"some error"}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.CreateApp(context.Background(), svr.URL, 1, frontendo11yapi.App{Name: "test app"})
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to create faro app "test app": status: 500`, err.Error())
+	})
+
+	t.Run("returns ErrUnauthorized when API responds 401", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(401)
+			_, _ = w.Write([]byte(`{"some error"}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.CreateApp(context.Background(), svr.URL, 1, frontendo11yapi.App{Name: "test app"})
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to create faro app "test app": request not authorized for stack`, err.Error())
+		assert.True(t, errors.Is(err, frontendo11yapi.ErrUnauthorized))
+	})
+}
+
+func TestClient_GetApp(t *testing.T) {
+	defaultHeaders := map[string]string{"Grafana-Terraform-Provider": "True"}
+	t.Run("successfully sends request and receives response", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodGet, r.Method)
+			assert.Equal(t, "True", r.Header.Get("Grafana-Terraform-Provider"))
+			assert.Equal(t, "/api/v1/app/1", r.URL.Path)
+
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte(`
+			{
+				"id": 1,
+				"name": "Test App",
+    			"appKey": "foobar",
+    			"corsOrigins": [{
+    			    "id": 1,
+    			    "url": "*"
+    			}],
+				"allowedRate": 0
+			}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		acutalApp, err := c.GetApp(context.Background(), svr.URL, 1, 1)
+		assert.NoError(t, err)
+
+		assert.Equal(t, frontendo11yapi.App{
+			ID:   1,
+			Name: "Test App",
+			Key:  "foobar",
+			CORSAllowedOrigins: []frontendo11yapi.AllowedOrigin{
+				{
+					ID:  1,
+					URL: "*",
+				},
+			},
+			AllowedRate: 0,
+		}, acutalApp)
+	})
+
+	t.Run("sets auth token, content type, user agent", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "Bearer 1:some token", r.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+			assert.Equal(t, "some-user-agent", r.Header.Get("User-Agent"))
+			_, _ = fmt.Fprintf(w, `{}`)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.GetApp(context.Background(), svr.URL, 1, 1)
+		require.NoError(t, err)
+	})
+
+	t.Run("returns ErrUnauthorized when frontend o11y API responds 401", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(401)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.GetApp(context.Background(), svr.URL, 1, 1)
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to get faro app: request not authorized for stack`, err.Error())
+		assert.True(t, errors.Is(err, frontendo11yapi.ErrUnauthorized))
+	})
+
+	t.Run("returns error when frontend o11y API responds 500", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(500)
+			_, _ = w.Write([]byte(`{"some error"}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.GetApp(context.Background(), svr.URL, 1, 1)
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to get faro app: status: 500`, err.Error())
+	})
+}
+
+func TestClient_GetApps(t *testing.T) {
+	defaultHeaders := map[string]string{"Grafana-Terraform-Provider": "True"}
+	t.Run("successfully sends request and receives response", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodGet, r.Method)
+			assert.Equal(t, "True", r.Header.Get("Grafana-Terraform-Provider"))
+			assert.Equal(t, "/api/v1/app", r.URL.Path)
+
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte(`
+			[
+				{
+					"id": 1,
+					"name": "Test App",
+    				"appKey": "foobar",
+    				"corsOrigins": [{
+    				    "id": 1,
+    				    "url": "*"
+    				}],
+					"allowedRate": 0
+				},
+				{
+					"id": 2,
+					"name": "Test App 2",
+    				"appKey": "foobar2",
+    				"corsOrigins": [{
+    				    "id": 1,
+    				    "url": "*"
+    				}],
+					"allowedRate": 0
+				}
+			]`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		acutalApps, err := c.GetApps(context.Background(), svr.URL, 1)
+		assert.NoError(t, err)
+
+		assert.Equal(t, []frontendo11yapi.App{
+			{
+				ID:   1,
+				Name: "Test App",
+				Key:  "foobar",
+				CORSAllowedOrigins: []frontendo11yapi.AllowedOrigin{
+					{
+						ID:  1,
+						URL: "*",
+					},
+				},
+				AllowedRate: 0,
+			},
+			{
+				ID:   2,
+				Name: "Test App 2",
+				Key:  "foobar2",
+				CORSAllowedOrigins: []frontendo11yapi.AllowedOrigin{
+					{
+						ID:  1,
+						URL: "*",
+					},
+				},
+				AllowedRate: 0,
+			},
+		}, acutalApps)
+	})
+
+	t.Run("sets auth token, content type, user agent", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "Bearer 1:some token", r.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+			assert.Equal(t, "some-user-agent", r.Header.Get("User-Agent"))
+			_, _ = fmt.Fprintf(w, `[]`)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.GetApps(context.Background(), svr.URL, 1)
+		require.NoError(t, err)
+	})
+
+	t.Run("returns ErrUnauthorized when frontend o11y API responds 401", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(401)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.GetApps(context.Background(), svr.URL, 1)
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to get faro apps: request not authorized for stack`, err.Error())
+		assert.True(t, errors.Is(err, frontendo11yapi.ErrUnauthorized))
+	})
+
+	t.Run("returns error when frontend o11y API responds 500", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(500)
+			_, _ = w.Write([]byte(`{"some error"}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.GetApps(context.Background(), svr.URL, 1)
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to get faro apps: status: 500`, err.Error())
+	})
+}
+
+func TestClient_UpdateApp(t *testing.T) {
+	defaultHeaders := map[string]string{"Grafana-Terraform-Provider": "True"}
+	t.Run("successfully sends request and receives response", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPut, r.Method)
+			assert.Equal(t, "True", r.Header.Get("Grafana-Terraform-Provider"))
+			assert.Equal(t, "/api/v1/app/1", r.URL.Path)
+			requestBody, err := io.ReadAll(r.Body)
+			require.NoError(t, err)
+			assert.JSONEq(t, `
+			{
+				"name": "New Name",
+    			"corsOrigins": [{
+					"url": "https://grafana.com"
+    			}],
+				"settings": {
+					"geolcation.enabled": "0"
+				}
+			}`, string(requestBody))
+
+			w.WriteHeader(http.StatusCreated)
+			_, _ = w.Write([]byte(`
+			{
+				"id": 1,
+				"name": "New Name",
+    			"appKey": "foobar",
+    			"corsOrigins": [{
+    			    "id": 2,
+					"url": "https://grafana.com"
+    			}],
+				"settings": {
+					"geolcation.enabled": "0"
+				},
+				"allowedRate": 0
+			}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		acutalApp, err := c.UpdateApp(context.Background(), svr.URL, 1, 1, frontendo11yapi.App{
+			Name: "New Name",
+			CORSAllowedOrigins: []frontendo11yapi.AllowedOrigin{
+				{
+					URL: "https://grafana.com",
+				},
+			},
+			Settings: map[string]string{
+				"geolcation.enabled": "0",
+			},
+		})
+		assert.NoError(t, err)
+
+		assert.Equal(t, frontendo11yapi.App{
+			ID:   1,
+			Name: "New Name",
+			Key:  "foobar",
+			CORSAllowedOrigins: []frontendo11yapi.AllowedOrigin{
+				{
+					ID:  2,
+					URL: "https://grafana.com",
+				},
+			},
+			AllowedRate: 0,
+			Settings: map[string]string{
+				"geolcation.enabled": "0",
+			},
+		}, acutalApp)
+	})
+
+	t.Run("sets auth token, content type, user agent", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "Bearer 1:some token", r.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+			assert.Equal(t, "some-user-agent", r.Header.Get("User-Agent"))
+			_, _ = fmt.Fprintf(w, `{}`)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.UpdateApp(context.Background(), svr.URL, 1, 1, frontendo11yapi.App{})
+		require.NoError(t, err)
+	})
+
+	t.Run("returns ErrorNotFound when frontend o11y API responds 404", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(404)
+			_, _ = w.Write([]byte(`{"some error"}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.UpdateApp(context.Background(), svr.URL, 1, 1, frontendo11yapi.App{Name: "Test App"})
+
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, frontendo11yapi.ErrNotFound)
+		assert.Equal(t, `failed to update faro app "Test App": not found`, err.Error())
+	})
+
+	t.Run("returns Unauthorized when frontend o11y API responds 401", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(401)
+			_, _ = w.Write([]byte(`{"some error"}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.UpdateApp(context.Background(), svr.URL, 1, 1, frontendo11yapi.App{Name: "Test App"})
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to update faro app "Test App": request not authorized for stack`, err.Error())
+		assert.True(t, errors.Is(err, frontendo11yapi.ErrUnauthorized))
+	})
+
+	t.Run("returns error when frontend o11y API responds 500", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(500)
+			_, _ = w.Write([]byte(`{"some error"}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		_, err = c.UpdateApp(context.Background(), svr.URL, 1, 1, frontendo11yapi.App{Name: "Test App"})
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to update faro app "Test App": status: 500`, err.Error())
+	})
+}
+
+func TestClient_DeleteApp(t *testing.T) {
+	defaultHeaders := map[string]string{"Grafana-Terraform-Provider": "True"}
+	t.Run("successfully sends request and receives response", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodDelete, r.Method)
+			assert.Equal(t, "/api/v1/app/1", r.URL.Path)
+
+			assert.Equal(t, "True", r.Header.Get("Grafana-Terraform-Provider"))
+
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		err = c.DeleteApp(context.Background(), svr.URL, 1, 1)
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("sets auth token, content type, user agent", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "Bearer 1:some token", r.Header.Get("Authorization"))
+			assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+			assert.Equal(t, "some-user-agent", r.Header.Get("User-Agent"))
+			_, _ = fmt.Fprintf(w, `{}`)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		err = c.DeleteApp(context.Background(), svr.URL, 1, 1)
+		require.NoError(t, err)
+	})
+
+	t.Run("returns ErrorNotFound when frontend o11y API responds 404", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(404)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		err = c.DeleteApp(context.Background(), svr.URL, 1, 1)
+
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, frontendo11yapi.ErrNotFound)
+		assert.Equal(t, `failed to delete faro app id=1: not found`, err.Error())
+	})
+
+	t.Run("returns ErrUnauthorized when frontend o11y API responds 401", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(401)
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		err = c.DeleteApp(context.Background(), svr.URL, 1, 1)
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to delete faro app id=1: request not authorized for stack`, err.Error())
+		assert.True(t, errors.Is(err, frontendo11yapi.ErrUnauthorized))
+	})
+
+	t.Run("returns error when frontend o11y API responds 500", func(t *testing.T) {
+		svr := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(500)
+			_, _ = w.Write([]byte(`{"some error"}`))
+		}))
+		defer svr.Close()
+
+		c, err := frontendo11yapi.NewClient("grafana-dev.com", "some token", svr.Client(), "some-user-agent", defaultHeaders)
+		require.NoError(t, err)
+		err = c.DeleteApp(context.Background(), svr.URL, 1, 1)
+
+		assert.Error(t, err)
+		assert.Equal(t, `failed to delete faro app id=1: status: 500`, err.Error())
+	})
+}
