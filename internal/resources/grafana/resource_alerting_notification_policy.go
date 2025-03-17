8ad54acda49d26863489a1e38b5c5513bd77e62c
Pepe Cano <825430+ppcano@users.noreply.github.com>
2025-03-17 11:29:36 +0100

    docs(alerting): clarify behavior on the notification policy tree (#2068)

--- internal/resources/grafana/resource_alerting_notification_policy.go
+++ internal/resources/grafana/resource_alerting_notification_policy.go
@@ -22,7 +22,7 @@ func resourceNotificationPolicy() *common.Resource {
 		Description: `
 Sets the global notification policy for Grafana.
 
-!> This resource manages the entire notification policy tree, and will overwrite any existing policies.
+!> This resource manages the entire notification policy tree and overwrites its policies. However, it does not overwrite internal policies created when alert rules directly set a contact point for notifications.
 
 * [Official documentation](https://grafana.com/docs/grafana/latest/alerting/set-up/provision-alerting-resources/terraform-provisioning/)
 * [HTTP API](https://grafana.com/docs/grafana/latest/developers/http_api/alerting_provisioning/#notification-policies)
