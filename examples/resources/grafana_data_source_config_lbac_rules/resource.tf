6411f12fbb15058f95a569bde94cbb4604b6e4cc
spinillos <selenepinillos@gmail.com>
2025-03-18 16:36:12 +0100

    Set rules as map

--- examples/resources/grafana_data_source_config_lbac_rules/resource.tf
+++ examples/resources/grafana_data_source_config_lbac_rules/resource.tf
@@ -17,12 +17,12 @@ resource "grafana_data_source" "test" {
 
 resource "grafana_data_source_config_lbac_rules" "test_rule" {
   datasource_uid = grafana_data_source.test.uid
-  rules = jsonencode({
-    "${grafana_team.team.team_uid}" = [
-      "{ cluster = \"dev-us-central-0\", namespace = \"hosted-grafana\" }",
-      "{ foo = \"qux\" }"
+  rules = {
+    (grafana_team.team.team_uid) = [
+      { cluster = "dev-us-central-0", namespace = "hosted-grafana" },
+      { foo = "qux" }
     ]
-  })
+  }
 
   depends_on = [
     grafana_team.team,
