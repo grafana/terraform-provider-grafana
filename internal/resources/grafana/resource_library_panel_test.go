9451a9c0a7c2e4ebe404f79f051249fc294eb6fe
Alexander Akhmetov <me@alx.cx>
2025-04-11 15:43:09 +0200

    grafana_library_panel: fix tests on Grafana >11.0.0

--- internal/resources/grafana/resource_library_panel_test.go
+++ internal/resources/grafana/resource_library_panel_test.go
@@ -12,49 +12,67 @@ import (
 )
 
 func TestAccLibraryPanel_basic(t *testing.T) {
-	testutils.CheckOSSTestsEnabled(t, ">=8.0.0")
-
-	name := acctest.RandString(10)
-	var panel models.LibraryElementResponse
-
-	resource.ParallelTest(t, resource.TestCase{
-		ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
-		CheckDestroy:             libraryPanelCheckExists.destroyed(&panel, nil),
-		Steps: []resource.TestStep{
-			{
-				// Test resource creation.
-				Config: testAccLibraryPanelBasic(name),
-				Check: resource.ComposeTestCheckFunc(
-					libraryPanelCheckExists.exists("grafana_library_panel.test", &panel),
-					resource.TestCheckResourceAttrSet("grafana_library_panel.test", "uid"),
-					resource.TestMatchResourceAttr("grafana_library_panel.test", "id", defaultOrgIDRegexp),
-					resource.TestCheckResourceAttr("grafana_library_panel.test", "org_id", "1"),
-					resource.TestCheckResourceAttr("grafana_library_panel.test", "name", name),
-					resource.TestCheckResourceAttr("grafana_library_panel.test", "version", "1"),
-					resource.TestCheckResourceAttr("grafana_library_panel.test", "model_json", fmt.Sprintf(`{"description":"","title":"%s","type":""}`, name)),
-					testutils.CheckLister("grafana_library_panel.test"),
-				),
-			},
-			{
-				// Updates title.
-				Config: testAccLibraryPanelBasic("updated " + name),
-				Check: resource.ComposeTestCheckFunc(
-					libraryPanelCheckExists.exists("grafana_library_panel.test", &panel),
-					resource.TestCheckResourceAttrSet("grafana_library_panel.test", "uid"),
-					resource.TestMatchResourceAttr("grafana_library_panel.test", "id", defaultOrgIDRegexp),
-					resource.TestCheckResourceAttr("grafana_library_panel.test", "name", "updated "+name),
-					resource.TestCheckResourceAttr("grafana_library_panel.test", "version", "2"),
-					resource.TestCheckResourceAttr("grafana_library_panel.test", "model_json", fmt.Sprintf(`{"description":"","title":"updated %s","type":""}`, name)),
-				),
-			},
-			{
-				// Importing matches the state of the previous step.
-				ResourceName:      "grafana_library_panel.test",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
+	testCases := []struct {
+		versionConstraint string
+		folder            string
+	}{
+		{
+			versionConstraint: ">=8.0.0,<=11.0.0",
+			folder:            "",
 		},
-	})
+		{
+			versionConstraint: ">11.0.0",
+			folder:            "general",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.versionConstraint, func(t *testing.T) {
+			testutils.CheckOSSTestsEnabled(t, tc.versionConstraint)
+
+			name := acctest.RandString(10)
+			var panel models.LibraryElementResponse
+
+			resource.ParallelTest(t, resource.TestCase{
+				ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
+				CheckDestroy:             libraryPanelCheckExists.destroyed(&panel, nil),
+				Steps: []resource.TestStep{
+					{
+						// Test resource creation.
+						Config: testAccLibraryPanelBasic(name, tc.folder),
+						Check: resource.ComposeTestCheckFunc(
+							libraryPanelCheckExists.exists("grafana_library_panel.test", &panel),
+							resource.TestCheckResourceAttrSet("grafana_library_panel.test", "uid"),
+							resource.TestMatchResourceAttr("grafana_library_panel.test", "id", defaultOrgIDRegexp),
+							resource.TestCheckResourceAttr("grafana_library_panel.test", "org_id", "1"),
+							resource.TestCheckResourceAttr("grafana_library_panel.test", "name", name),
+							resource.TestCheckResourceAttr("grafana_library_panel.test", "version", "1"),
+							resource.TestCheckResourceAttr("grafana_library_panel.test", "model_json", fmt.Sprintf(`{"description":"","title":"%s","type":""}`, name)),
+							testutils.CheckLister("grafana_library_panel.test"),
+						),
+					},
+					{
+						// Updates title.
+						Config: testAccLibraryPanelBasic("updated "+name, tc.folder),
+						Check: resource.ComposeTestCheckFunc(
+							libraryPanelCheckExists.exists("grafana_library_panel.test", &panel),
+							resource.TestCheckResourceAttrSet("grafana_library_panel.test", "uid"),
+							resource.TestMatchResourceAttr("grafana_library_panel.test", "id", defaultOrgIDRegexp),
+							resource.TestCheckResourceAttr("grafana_library_panel.test", "name", "updated "+name),
+							resource.TestCheckResourceAttr("grafana_library_panel.test", "version", "2"),
+							resource.TestCheckResourceAttr("grafana_library_panel.test", "model_json", fmt.Sprintf(`{"description":"","title":"updated %s","type":""}`, name)),
+						),
+					},
+					{
+						// Importing matches the state of the previous step.
+						ResourceName:      "grafana_library_panel.test",
+						ImportState:       true,
+						ImportStateVerify: true,
+					},
+				},
+			})
+		})
+	}
 }
 
 func TestAccLibraryPanel_folder(t *testing.T) {
@@ -91,59 +109,108 @@ func TestAccLibraryPanel_folder(t *testing.T) {
 }
 
 func TestAccLibraryPanel_dashboard(t *testing.T) {
-	testutils.CheckOSSTestsEnabled(t, ">=8.0.0")
-
-	var panel models.LibraryElementResponse
-	var dashboard models.DashboardFullWithMeta
-
-	resource.ParallelTest(t, resource.TestCase{
-		ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
-		CheckDestroy:             libraryPanelCheckExists.destroyed(&panel, nil),
-		Steps: []resource.TestStep{
-			{
-				// Test library panel is connected to dashboard
-				Config: testutils.TestAccExample(t, "data-sources/grafana_library_panel/data-source.tf"),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestMatchResourceAttr("grafana_library_panel.dashboard", "id", defaultOrgIDRegexp),
-					libraryPanelCheckExists.exists("grafana_library_panel.dashboard", &panel),
-					dashboardCheckExists.exists("grafana_dashboard.with_library_panel", &dashboard),
-				),
+	testCases := []struct {
+		versionConstraint string
+		replacements      map[string]string
+	}{
+		{
+			versionConstraint: ">=8.0.0,<=11.0.0",
+			replacements: map[string]string{
+				`"general"`: "null",
 			},
 		},
-	})
+		{
+			versionConstraint: ">11.0.0",
+			replacements:      map[string]string{},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.versionConstraint, func(t *testing.T) {
+			testutils.CheckOSSTestsEnabled(t, tc.versionConstraint)
+
+			var panel models.LibraryElementResponse
+			var dashboard models.DashboardFullWithMeta
+
+			resource.ParallelTest(t, resource.TestCase{
+				ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
+				CheckDestroy:             libraryPanelCheckExists.destroyed(&panel, nil),
+				Steps: []resource.TestStep{
+					{
+						// Test library panel is connected to dashboard
+						Config: testutils.TestAccExampleWithReplace(t,
+							"data-sources/grafana_library_panel/data-source.tf",
+							tc.replacements,
+						),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestMatchResourceAttr("grafana_library_panel.dashboard", "id", defaultOrgIDRegexp),
+							libraryPanelCheckExists.exists("grafana_library_panel.dashboard", &panel),
+							dashboardCheckExists.exists("grafana_dashboard.with_library_panel", &dashboard),
+						),
+					},
+				},
+			})
+		})
+	}
 }
 
 func TestAccLibraryPanel_inOrg(t *testing.T) {
-	testutils.CheckOSSTestsEnabled(t, ">=8.0.0")
-
-	var panel models.LibraryElementResponse
-	orgName := acctest.RandString(10)
-
-	resource.ParallelTest(t, resource.TestCase{
-		ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
-		CheckDestroy:             libraryPanelCheckExists.destroyed(&panel, nil),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccLibraryPanelInOrganization(orgName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestMatchResourceAttr("grafana_library_panel.test", "id", nonDefaultOrgIDRegexp),
-					libraryPanelCheckExists.exists("grafana_library_panel.test", &panel),
-					checkResourceIsInOrg("grafana_library_panel.test", "grafana_organization.test"),
-				),
-			},
+	testCases := []struct {
+		versionConstraint string
+		folder            string
+	}{
+		{
+			versionConstraint: ">=8.0.0,<=11.0.0",
+			folder:            "",
 		},
-	})
+		{
+			versionConstraint: ">11.0.0",
+			folder:            "general",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.versionConstraint, func(t *testing.T) {
+			testutils.CheckOSSTestsEnabled(t, tc.versionConstraint)
+
+			var panel models.LibraryElementResponse
+			orgName := acctest.RandString(10)
+
+			resource.ParallelTest(t, resource.TestCase{
+				ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
+				CheckDestroy:             libraryPanelCheckExists.destroyed(&panel, nil),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccLibraryPanelInOrganization(orgName, tc.folder),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestMatchResourceAttr("grafana_library_panel.test", "id", nonDefaultOrgIDRegexp),
+							libraryPanelCheckExists.exists("grafana_library_panel.test", &panel),
+							checkResourceIsInOrg("grafana_library_panel.test", "grafana_organization.test"),
+						),
+					},
+				},
+			})
+		})
+	}
 }
 
-func testAccLibraryPanelBasic(name string) string {
+func testAccLibraryPanelBasic(name, folder string) string {
+	var fuid string
+	if folder == "" {
+		fuid = "null"
+	} else {
+		fuid = fmt.Sprintf(`"%s"`, folder)
+	}
+
 	return fmt.Sprintf(`
 resource "grafana_library_panel" "test" {
-	name      = "%[1]s"
+	name       = "%[1]s"
+	folder_uid = %[2]s
 	model_json = jsonencode({
 		title   = "%[1]s",
 	})
 }
-`, name)
+`, name, fuid)
 }
 
 func testAccLibraryPanelInFolder(name string) string {
@@ -163,17 +230,25 @@ resource "grafana_library_panel" "test_folder" {
 }`, name)
 }
 
-func testAccLibraryPanelInOrganization(orgName string) string {
+func testAccLibraryPanelInOrganization(orgName, folder string) string {
+	var fuid string
+	if folder == "" {
+		fuid = "null"
+	} else {
+		fuid = fmt.Sprintf(`"%s"`, folder)
+	}
+
 	return fmt.Sprintf(`
 resource "grafana_organization" "test" {
 	name = "%[1]s"
 }
 
 resource "grafana_library_panel" "test" {
-	org_id    = grafana_organization.test.id
-	name      = "%[1]s"
+	org_id     = grafana_organization.test.id
+	name       = "%[1]s"
+    folder_uid = %[2]s
 	model_json = jsonencode({
 	  title   = "%[1]s",
 	})
-  }`, orgName)
+  }`, orgName, fuid)
 }
