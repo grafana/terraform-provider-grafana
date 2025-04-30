b2e8fef78034cd45458c3f5d213142602331d635
Tito Lins <tito.linsesilva@grafana.com>
2025-04-30 10:06:00 +0200

    fix formatting

--- examples/resources/grafana_contact_point/_acc_receiver_types.tf
+++ examples/resources/grafana_contact_point/_acc_receiver_types.tf
@@ -169,7 +169,7 @@ resource "grafana_contact_point" "receiver_types" {
     max_alerts          = 100
     message             = "Custom message"
     title               = "Custom title"
-    tls_config          = {
+    tls_config = {
       insecure_skip_verify = true
       ca_certificate       = "ca.crt"
       client_certificate   = "client.crt"
