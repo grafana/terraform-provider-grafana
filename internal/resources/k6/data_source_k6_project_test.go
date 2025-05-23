d426ca07672bcbdf496c721fb947ef958a4c760a
Oleksandr Kotov <alxkot16@gmail.com>
2025-05-23 10:39:07 +0100

    Fix tests

--- internal/resources/k6/data_source_k6_project_test.go
+++ internal/resources/k6/data_source_k6_project_test.go
@@ -3,6 +3,7 @@ package k6_test
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
 	"github.com/grafana/terraform-provider-grafana/v3/internal/testutils"
@@ -11,11 +12,15 @@ import (
 func TestAccDataSourceK6Project_basic(t *testing.T) {
 	testutils.CheckCloudInstanceTestsEnabled(t)
 
+	projectName := "Terraform Test Project " + acctest.RandString(8)
+
 	resource.ParallelTest(t, resource.TestCase{
 		ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testutils.TestAccExample(t, "data-sources/grafana_k6_project/data-source.tf"),
+				Config: testutils.TestAccExampleWithReplace(t, "data-sources/grafana_k6_project/data-source.tf", map[string]string{
+					"Terraform Test Project": projectName,
+				}),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("data.grafana_k6_project.from_id", "id"),
 					resource.TestCheckResourceAttr("data.grafana_k6_project.from_id", "name", "Terraform Test Project"),
