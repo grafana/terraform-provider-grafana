352db44242ce5f46abbce5e42264d3f2db23549a
Oleksandr Kotov <alxkot16@gmail.com>
2025-05-23 09:59:34 +0100

    Fix missing imports

--- internal/resources/k6/resource_loadtest_test.go
+++ internal/resources/k6/resource_loadtest_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
--- internal/resources/k6/resource_project_limits_test.go
+++ internal/resources/k6/resource_project_limits_test.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
