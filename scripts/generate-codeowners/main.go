ab4e7c50374f67004067d07ae6eb6f2aad72c814
nikimanoledaki <niki.manoledaki@grafana.com>
2025-06-06 16:25:55 +0200

    Read ownership file

--- scripts/generate-codeowners/main.go
+++ scripts/generate-codeowners/main.go
@@ -13,13 +13,13 @@ const (
 )
 
 func main() {
-	repoRoot, err := filepath.Abs("../../.github")
+	repoRoot, err := filepath.Abs("../../")
 	if err != nil {
 		log.Fatalf("Failed to get absolute path: %v", err)
 	}
 
 	// Open the CODEOWNERS file for writing
-	codeownersFilePath := filepath.Join(repoRoot, "CODEOWNERS")
+	codeownersFilePath := filepath.Join(repoRoot, ".github", "CODEOWNERS")
 	codeownersFile, err := os.OpenFile(codeownersFilePath, os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("Failed to open CODEOWNERS file: %v", err)
@@ -35,7 +35,18 @@ func main() {
 		filepath.Join("internal", "resources"),
 	}
 
-	generator := generator.New(repoRoot, codeownersFile)
+	ownershipFilePath := filepath.Join(repoRoot, "ownership.json")
+	ownershipFile, err := os.OpenFile(ownershipFilePath, os.O_RDONLY, 0644)
+	if err != nil {
+		log.Fatalf("Failed to open ownership file: %v", err)
+	}
+	defer func() {
+		if err := ownershipFile.Close(); err != nil {
+			log.Fatalf("Failed to close ownership file: %v", err)
+		}
+	}()
+
+	generator := generator.New(repoRoot, codeownersFile, ownershipFile)
 	if err := generator.Generate(pathsToCheck); err != nil {
 		log.Fatal(err)
 	}
