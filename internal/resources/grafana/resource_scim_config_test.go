335ec54174a6a2af2bbeae518ab6c1a4588ff7fc
colin-stuart <colindonstuart@gmail.com>
2025-07-15 12:29:12 -0500

    SCIM: Only run tests on Grafana >=12.0.0 (#2258)
    * SCIM: Only run tests on grafana >=12.0.0
    
    * update test file
    
    * Update schema
    
    * skip SCIM tests if grafana <12.0.0
    
    ---------
    
    Co-authored-by: spinillos <selenepinillos@gmail.com>

--- internal/resources/grafana/resource_scim_config_test.go
+++ internal/resources/grafana/resource_scim_config_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestAccResourceSCIMConfig_basic(t *testing.T) {
-	testutils.CheckEnterpriseTestsEnabled(t, ">=11.0.0")
+	testutils.CheckEnterpriseTestsEnabled(t, ">=12.0.0")
 
 	resourceName := "grafana_scim_config.test"
 
@@ -53,7 +53,7 @@ func TestAccResourceSCIMConfig_basic(t *testing.T) {
 }
 
 func TestAccResourceSCIMConfig_import(t *testing.T) {
-	testutils.CheckEnterpriseTestsEnabled(t, ">=11.0.0")
+	testutils.CheckEnterpriseTestsEnabled(t, ">=12.0.0")
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
