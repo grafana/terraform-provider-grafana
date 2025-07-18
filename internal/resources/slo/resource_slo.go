2e161201d857e0d0d66b93673cc7278b7fe38e26
Sima Arfania <60867913+simaarfania@users.noreply.github.com>
2025-07-18 13:01:34 -0400

    fix(update-schema): use Github Token to generate PR containing schema changes (#2260)
    * fix(update-schema): add app configuration
    
    * fix: remove temporary fix
    
    * fix: add hash
    
    * test: use GITHUB_TOKEN
    
    * test: change provider schema
    
    * test: fix workflow
    
    * chore: update provider schema and issue templates
    
    Auto-generated by schema consistency workflow
    
    * fix: run workflow on specific path filters
    
    * fix: make action generate a new PR for schema update
    
    * test: test run workflow
    
    * fix: run on PR
    
    * fix: run on PR
    
    * fix: revert testing changes
    
    * fix: docs
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- internal/resources/slo/resource_slo.go
+++ internal/resources/slo/resource_slo.go
@@ -28,7 +28,7 @@ var resourceSloID = common.NewResourceID(common.StringIDField("uuid"))
 func resourceSlo() *common.Resource {
 	schema := &schema.Resource{
 		Description: `
-Resource manages Grafana SLOs. 
+Resource manages Grafana SLOs.
 
 * [Official documentation](https://grafana.com/docs/grafana-cloud/alerting-and-irm/slo/)
 * [API documentation](https://grafana.com/docs/grafana-cloud/alerting-and-irm/slo/api/)
--- scripts/generate_schema.sh
+++ scripts/generate_schema.sh
@@ -4,28 +4,6 @@ CURRENTDIR=$(realpath $(dirname $0))
 REPODIR="${CURRENTDIR}"/..
 WORKDIR=$(mktemp -d)
 
-TERRAFORM_VERSION="1.12.2"
-INSTALL_DIR="${REPODIR}"/testdata/terraform_"${TERRAFORM_VERSION}"
-
-OS="$(uname | tr '[:upper:]' '[:lower:]')"
-case "$OS" in
-  linux*)   OS="linux" ;;
-  darwin*)  OS="darwin" ;;
-  msys*|cygwin*|mingw*) OS="windows" ;;
-  *) echo "Unknown OS: $OS"; exit 1 ;;
-esac
-
-ARCH="$(uname -m)"
-case "$ARCH" in
-  x86_64) ARCH="amd64" ;;
-  i386|i686) ARCH="386" ;;
-  armv6l|armv7l) ARCH="arm" ;;
-  aarch64|arm64) ARCH="arm64" ;;
-  *) echo "Unknown Arch: $ARCH"; exit 1 ;;
-esac
-
-FILENAME=terraform_"${TERRAFORM_VERSION}"_"${OS}"_"${ARCH}".zip
-
 function finish {
     rm -rf "${WORKDIR}"
 }
@@ -52,11 +30,4 @@ echo '{"terraform":[{"required_providers":[{"provider":{"source":"grafana/grafan
 
 export TF_CLI_CONFIG_FILE=terraform.rc
 
-if [ ! -d "${INSTALL_DIR}" ]; then
-  mkdir -p "${INSTALL_DIR}"
-  
-  curl -o "${FILENAME}" https://releases.hashicorp.com/terraform/"${TERRAFORM_VERSION}"/"${FILENAME}"
-  unzip -qq -o "${FILENAME}" -d "${INSTALL_DIR}"
-fi
-
-"${INSTALL_DIR}/terraform" providers schema -json=true
+terraform providers schema -json=true
