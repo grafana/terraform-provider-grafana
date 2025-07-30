ab7f70246bfec5e26ee84041acb7db34868fe47c
Patrick Easters <patrick.easters@grafana.com>
2025-07-30 16:20:11 -0400

    Use correct attribute to dllow/deny non-provisioned users

--- internal/resources/grafana/resource_scim_config.go
+++ internal/resources/grafana/resource_scim_config.go
@@ -30,9 +30,9 @@ type SCIMConfigMetadata struct {
 
 // SCIMConfigSpec represents the SCIM configuration specification
 type SCIMConfigSpec struct {
-	EnableUserSync           bool `json:"enableUserSync"`
-	EnableGroupSync          bool `json:"enableGroupSync"`
-	AllowNonProvisionedUsers bool `json:"allowNonProvisionedUsers"`
+	EnableUserSync            bool `json:"enableUserSync"`
+	EnableGroupSync           bool `json:"enableGroupSync"`
+	RejectNonProvisionedUsers bool `json:"rejectNonProvisionedUsers"`
 }
 
 func resourceSCIMConfig() *common.Resource {
@@ -109,9 +109,9 @@ func CreateOrUpdateSCIMConfig(ctx context.Context, d *schema.ResourceData, meta 
 			Namespace: namespace,
 		},
 		Spec: SCIMConfigSpec{
-			EnableUserSync:           d.Get("enable_user_sync").(bool),
-			EnableGroupSync:          d.Get("enable_group_sync").(bool),
-			AllowNonProvisionedUsers: d.Get("allow_non_provisioned_users").(bool),
+			EnableUserSync:            d.Get("enable_user_sync").(bool),
+			EnableGroupSync:           d.Get("enable_group_sync").(bool),
+			RejectNonProvisionedUsers: !d.Get("allow_non_provisioned_users").(bool),
 		},
 	}
 
@@ -236,7 +236,7 @@ func ReadSCIMConfig(ctx context.Context, d *schema.ResourceData, meta interface{
 	if err != nil {
 		return diag.FromErr(err)
 	}
-	err = d.Set("allow_non_provisioned_users", scimConfig.Spec.AllowNonProvisionedUsers)
+	err = d.Set("allow_non_provisioned_users", !scimConfig.Spec.RejectNonProvisionedUsers)
 	if err != nil {
 		return diag.FromErr(err)
 	}
