c5566935c282fa276a1fb38a337fdb6c1004fdc1
Paweł Dobrowolski <pawel.dobrowolski@grafana.com>
2025-08-04 13:30:12 +0200

    fix allowed_load_zones example

--- examples/resources/grafana_k6_project_allowed_load_zones/resource.tf
+++ examples/resources/grafana_k6_project_allowed_load_zones/resource.tf
@@ -2,7 +2,7 @@ resource "grafana_k6_project" "test_project_allowed_load_zones" {
   name = "Terraform Project Test Allowed Load Zones"
 }
 
-resource "grafana_k6_project_limits" "test_limits" {
-  project_id              = grafana_k6_project.test_project_allowed_load_zones.id
-  allowed_load_zones      = ["my-load-zone-1", "other-load-zone"]
+resource "grafana_k6_project_allowed_load_zones" "test_allowed_zones" {
+  project_id         = grafana_k6_project.test_project_allowed_load_zones.id
+  allowed_load_zones = ["my-load-zone-1", "other-load-zone"]
 }
