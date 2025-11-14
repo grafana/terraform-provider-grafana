0599ca93e7d6e8d612bc5dfd63939a7104c0af72
Sima Arfania <60867913+simaarfania@users.noreply.github.com>
2025-11-14 13:18:12 -0500

    test: alter SLO acronym in resource description (#2429)
    * test: alter SLO acronym in resource description
    
    * fix: regenerate docs

--- internal/resources/slo/resource_slo.go
+++ internal/resources/slo/resource_slo.go
@@ -69,7 +69,7 @@ func createAssertsSLOClient(baseClient *slo.APIClient) *slo.APIClient {
 func resourceSlo() *common.Resource {
 	schema := &schema.Resource{
 		Description: `
-Resource manages Grafana SLOs.
+Resource manages Grafana SLOs (Service Level Objectives).
 
 * [Official documentation](https://grafana.com/docs/grafana-cloud/alerting-and-irm/slo/)
 * [API documentation](https://grafana.com/docs/grafana-cloud/alerting-and-irm/slo/api/)
