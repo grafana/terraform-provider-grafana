18056b092f20f78ff57db90c328171be1e8f3edc
Andrew Hackmann <ajhack93@gmail.com>
2025-11-14 14:52:22 -0600

    tests use require

--- internal/resources/grafana/resource_data_source_deprecated_auth_test.go
+++ internal/resources/grafana/resource_data_source_deprecated_auth_test.go
@@ -3,7 +3,9 @@ package grafana
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCheckDeprecatedPrometheusAuth_SigV4(t *testing.T) {
@@ -11,7 +13,7 @@ func TestCheckDeprecatedPrometheusAuth_SigV4(t *testing.T) {
 		"name":              "test-prometheus",
 		"type":              "prometheus",
 		"url":               "http://localhost:9090",
-		"json_data_encoded": `{"sigV4Auth":true,"sigV4Region":"us-east-1"}`,
+		"json_data_encoded": `{"sigV4Auth":true}`,
 	})
 
 	diags := checkDeprecatedPrometheusAuth(d)
@@ -20,14 +22,8 @@ func TestCheckDeprecatedPrometheusAuth_SigV4(t *testing.T) {
 		t.Errorf("Expected 1 diagnostic, got %d", len(diags))
 	}
 
-	if len(diags) > 0 {
-		if diags[0].Severity != 1 { // Warning
-			t.Errorf("Expected warning severity, got %d", diags[0].Severity)
-		}
-		if diags[0].Summary != "Deprecated authentication method" {
-			t.Errorf("Unexpected summary: %s", diags[0].Summary)
-		}
-	}
+	require.Equal(t, len(diags), 1)
+	require.Equal(t, diags[0].Severity, diag.Warning)
 }
 
 func TestCheckDeprecatedPrometheusAuth_Azure(t *testing.T) {
@@ -35,7 +31,7 @@ func TestCheckDeprecatedPrometheusAuth_Azure(t *testing.T) {
 		"name":              "test-prometheus",
 		"type":              "prometheus",
 		"url":               "http://localhost:9090",
-		"json_data_encoded": `{"azureAuth":true}`,
+		"json_data_encoded": `{"azureCredentials":{}}`,
 	})
 
 	diags := checkDeprecatedPrometheusAuth(d)
@@ -44,47 +40,11 @@ func TestCheckDeprecatedPrometheusAuth_Azure(t *testing.T) {
 		t.Errorf("Expected 1 diagnostic, got %d", len(diags))
 	}
 
-	if len(diags) > 0 {
-		if diags[0].Severity != 1 { // Warning
-			t.Errorf("Expected warning severity, got %d", diags[0].Severity)
-		}
-		if diags[0].Summary != "Deprecated authentication method" {
-			t.Errorf("Unexpected summary: %s", diags[0].Summary)
-		}
-	}
+	require.Equal(t, len(diags), 1)
+	require.Equal(t, diags[0].Severity, diag.Warning)
 }
 
 func TestCheckDeprecatedPrometheusAuth_NoDeprecatedAuth(t *testing.T) {
-	d := schema.TestResourceDataRaw(t, resourceDataSource().Schema.Schema, map[string]interface{}{
-		"name":              "test-prometheus",
-		"type":              "prometheus",
-		"url":               "http://localhost:9090",
-		"json_data_encoded": `{"httpMethod":"POST"}`,
-	})
-
-	diags := checkDeprecatedPrometheusAuth(d)
-
-	if len(diags) != 0 {
-		t.Errorf("Expected 0 diagnostics, got %d", len(diags))
-	}
-}
-
-func TestCheckDeprecatedPrometheusAuth_NonPrometheusDataSource(t *testing.T) {
-	d := schema.TestResourceDataRaw(t, resourceDataSource().Schema.Schema, map[string]interface{}{
-		"name":              "test-loki",
-		"type":              "loki",
-		"url":               "http://localhost:3100",
-		"json_data_encoded": `{"sigV4Auth":true}`,
-	})
-
-	diags := checkDeprecatedPrometheusAuth(d)
-
-	if len(diags) != 0 {
-		t.Errorf("Expected 0 diagnostics for non-Prometheus datasource, got %d", len(diags))
-	}
-}
-
-func TestCheckDeprecatedPrometheusAuth_EmptyJSONData(t *testing.T) {
 	d := schema.TestResourceDataRaw(t, resourceDataSource().Schema.Schema, map[string]interface{}{
 		"name": "test-prometheus",
 		"type": "prometheus",
@@ -93,23 +53,18 @@ func TestCheckDeprecatedPrometheusAuth_EmptyJSONData(t *testing.T) {
 
 	diags := checkDeprecatedPrometheusAuth(d)
 
-	if len(diags) != 0 {
-		t.Errorf("Expected 0 diagnostics for empty JSON data, got %d", len(diags))
-	}
+	require.Equal(t, len(diags), 0)
 }
 
-func TestCheckDeprecatedPrometheusAuth_BothAuthMethods(t *testing.T) {
+func TestCheckDeprecatedPrometheusAuth_NonPrometheusDataSource(t *testing.T) {
 	d := schema.TestResourceDataRaw(t, resourceDataSource().Schema.Schema, map[string]interface{}{
-		"name":              "test-prometheus",
-		"type":              "prometheus",
+		"name":              "test-loki",
+		"type":              "loki",
 		"url":               "http://localhost:9090",
-		"json_data_encoded": `{"sigV4Auth":true,"azureAuth":true}`,
+		"json_data_encoded": `{"sigV4Auth":true}`,
 	})
 
 	diags := checkDeprecatedPrometheusAuth(d)
 
-	// Should get warnings for both auth methods
-	if len(diags) != 2 {
-		t.Errorf("Expected 2 diagnostics, got %d", len(diags))
-	}
+	require.Equal(t, len(diags), 0)
 }
