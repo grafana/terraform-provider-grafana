9e8327d501d7f09a8e6e2904dd0baafa564e6c30
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2026-01-15 17:11:13 -0300

    examples_test: require v12.2.0 for secrets

--- internal/resources/examples_test.go
+++ internal/resources/examples_test.go
@@ -36,6 +36,8 @@ func TestAccExamples(t *testing.T) {
 					testutils.CheckEnterpriseTestsEnabled(t, ">=12.2.0")
 				case strings.Contains(filename, "grafana_apps_rules"):
 					t.Skip() // TODO: Enable once the API is no longer behind a feature toggle.
+				case strings.Contains(filename, "grafana_apps_secret_"):
+					testutils.CheckEnterpriseTestsEnabled(t, ">=12.2.0")
 				default:
 					testutils.CheckOSSTestsEnabled(t, ">=11.0.0") // Only run on latest OSS version. The examples should be updated to reflect their latest working config.
 				}
