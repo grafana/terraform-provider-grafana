3a88ef8cceb2079bb54b6550d05778f95c51fcfb
Emily Rager <emily.rager@grafana.com>
2026-03-27 11:45:12 +0100

    Fmt terraform files

--- examples/resources/grafana_cloud_integration/resource.tf
+++ examples/resources/grafana_cloud_integration/resource.tf
@@ -29,17 +29,17 @@ resource "grafana_cloud_integration" "kafka" {
 # Output info
 output "linux_node_integration" {
   value = {
-    name             = grafana_cloud_integration.linux-node.name
-    latest_version = grafana_cloud_integration.linux-node.latest_version
+    name              = grafana_cloud_integration.linux-node.name
+    latest_version    = grafana_cloud_integration.linux-node.latest_version
     installed_version = grafana_cloud_integration.linux-node.installed_version
-    dashboard_folder = grafana_cloud_integration.linux-node.dashboard_folder
+    dashboard_folder  = grafana_cloud_integration.linux-node.dashboard_folder
   }
 }
 output "kafka_integration" {
   value = {
-    name             = grafana_cloud_integration.kafka.name
-    latest_version = grafana_cloud_integration.kafka.latest_version
+    name              = grafana_cloud_integration.kafka.name
+    latest_version    = grafana_cloud_integration.kafka.latest_version
     installed_version = grafana_cloud_integration.kafka.installed_version
-    dashboard_folder = grafana_cloud_integration.kafka.dashboard_folder
+    dashboard_folder  = grafana_cloud_integration.kafka.dashboard_folder
   }
 }
