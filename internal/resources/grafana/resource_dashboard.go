ac2828f9d9c30c3355c4c99b8b28db44ff08d24a
spinillos <selenepinillos@gmail.com>
2026-03-30 17:28:47 +0200

    Update documentation links with new and legacy api

--- internal/resources/grafana/resource_dashboard.go
+++ internal/resources/grafana/resource_dashboard.go
@@ -33,7 +33,8 @@ func resourceDashboard() *common.Resource {
 Manages Grafana dashboards.
 
 * [Official documentation](https://grafana.com/docs/grafana/latest/dashboards/)
-* [HTTP API](https://grafana.com/docs/grafana/latest/developers/http_api/dashboard/)
+* [HTTP API (legacy API, recommended for Grafana 12 or earlier)](https://grafana.com/docs/grafana/v11.6/developers/http_api/dashboard/)
+* [HTTP API (new Kubernetes-style API, recommended for Grafana 13 and later)](https://grafana.com/docs/grafana/latest/developers/http_api/dashboard/)
 `,
 
 		CreateContext: common.WithDashboardMutex[schema.CreateContextFunc](CreateDashboard),
