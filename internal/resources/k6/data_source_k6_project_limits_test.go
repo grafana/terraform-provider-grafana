a65a3304b003c58b14826e86f45fb5e265dbfa83
belén <rooneyshuman@gmail.com>
2026-04-01 12:46:18 -0700

    Compare datasource attributes against what grafana_k6_project_limits reported after apply

--- internal/resources/k6/data_source_k6_project_limits_test.go
+++ internal/resources/k6/data_source_k6_project_limits_test.go
@@ -22,11 +22,11 @@ func TestAccDataSourceK6ProjectLimits_basic(t *testing.T) {
 					"Terraform Project Test Limits": projectName,
 				}),
 				Check: resource.ComposeTestCheckFunc(
-					// project_id
-					resource.TestCheckResourceAttr("data.grafana_k6_project_limits.from_project_id", "vuh_max_per_month", "10000"),
-					resource.TestCheckResourceAttr("data.grafana_k6_project_limits.from_project_id", "vu_max_per_test", "10000"),
-					resource.TestCheckResourceAttr("data.grafana_k6_project_limits.from_project_id", "vu_browser_max_per_test", "1000"),
-					resource.TestCheckResourceAttr("data.grafana_k6_project_limits.from_project_id", "duration_max_per_test", "3600"),
+					// Verify datasource values match what was set by the resource.
+					resource.TestCheckResourceAttrPair("data.grafana_k6_project_limits.from_project_id", "vuh_max_per_month", "grafana_k6_project_limits.test_limits", "vuh_max_per_month"),
+					resource.TestCheckResourceAttrPair("data.grafana_k6_project_limits.from_project_id", "vu_max_per_test", "grafana_k6_project_limits.test_limits", "vu_max_per_test"),
+					resource.TestCheckResourceAttrPair("data.grafana_k6_project_limits.from_project_id", "vu_browser_max_per_test", "grafana_k6_project_limits.test_limits", "vu_browser_max_per_test"),
+					resource.TestCheckResourceAttrPair("data.grafana_k6_project_limits.from_project_id", "duration_max_per_test", "grafana_k6_project_limits.test_limits", "duration_max_per_test"),
 				),
 			},
 		},
