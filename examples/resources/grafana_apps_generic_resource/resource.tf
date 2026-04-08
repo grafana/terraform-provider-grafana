1bcc878dae17e8608e13a55697c85389983f1410
Artur Wierzbicki <artur@arturwierzbicki.com>
2026-04-08 14:17:46 +0100

    App Platform: generic resource (#2657)
    * add app ptf generic resource
    
    * add app ptf generic resource
    
    * lint
    
    * tests
    
    * tests
    
    * remove overrides
    
    * remove overrides
    
    * fix test
    
    * fix test
    
    * fix secure
    
    * docs
    
    * nested fields drift fix
    
    * default allow_ui_updates to false
    
    * docs fix
    
    * move client config to configure_clients.go
    
    * review comments
    
    * review comments
    
    * add readme
    
    * add readme
    
    * add readme
    
    * update docs

---
+++ examples/resources/grafana_apps_generic_resource/resource.tf
@@ -0,0 +1,62 @@
+# Folder from a YAML manifest file.
+resource "grafana_apps_generic_resource" "folder_from_yaml" {
+  manifest = yamldecode(file("${path.module}/folder.yaml"))
+}
+
+# Folder with an inline manifest.
+resource "grafana_apps_generic_resource" "folder_inline" {
+  manifest = {
+    apiVersion = "folder.grafana.app/v1"
+    kind       = "Folder"
+    metadata = {
+      name = "my-inline-folder"
+    }
+    spec = {
+      title = "My Inline Folder"
+    }
+  }
+}
+
+# Dashboard with annotations and labels.
+resource "grafana_apps_generic_resource" "dashboard" {
+  manifest = {
+    apiVersion = "dashboard.grafana.app/v2"
+    kind       = "Dashboard"
+    metadata = {
+      name = "my-dashboard"
+      annotations = {
+        "grafana.app/folder" = "my-inline-folder"
+      }
+      labels = {
+        "team" = "platform"
+      }
+    }
+    spec = {
+      title      = "My Dashboard"
+      cursorSync = "Off"
+      preload    = false
+      elements   = {}
+      layout     = { kind = "GridLayout", spec = { items = [] } }
+    }
+  }
+
+  depends_on = [grafana_apps_generic_resource.folder_inline]
+}
+
+variable "folder_title" {
+  type    = string
+  default = "My Dynamic Folder"
+}
+
+# Inject Terraform variables into a static manifest using merge().
+resource "grafana_apps_generic_resource" "folder_with_variable" {
+  manifest = merge(yamldecode(file("${path.module}/folder.yaml")), {
+    metadata = merge(yamldecode(file("${path.module}/folder.yaml")).metadata, {
+      name = "my-dynamic-folder"
+    })
+    spec = {
+      title       = var.folder_title
+      description = "A folder managed by Terraform"
+    }
+  })
+}
