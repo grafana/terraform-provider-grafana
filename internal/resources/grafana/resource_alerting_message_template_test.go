grafana/terraform-provider-grafana

commit f185dc2cf0073be15e992529c12c3d321f680afe

Migrate grafana_alerting_message_template to use Framework SDK (#2567)
* Migrate grafana_alerting_message_template to framework sdk
* Set disable_provenance as computed when using default
* Update generate testdata for message_template

internal/resources/grafana/resource_alerting_message_template.go
@@ -11,14 +11,55 @@ import (
 	"github.com/grafana/grafana-openapi-client-go/client/provisioning"
 	"github.com/grafana/grafana-openapi-client-go/models"
 	"github.com/grafana/terraform-provider-grafana/v4/internal/common"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+var (
+	_ resource.Resource                = &messageTemplateResource{}
+	_ resource.ResourceWithConfigure   = &messageTemplateResource{}
+	_ resource.ResourceWithImportState = &messageTemplateResource{}
+	_ resource.ResourceWithModifyPlan  = &messageTemplateResource{}
+
+	resourceMessageTemplateName = "grafana_message_template"
+	resourceMessageTemplateID   = orgResourceIDString("name")
 )
 
 func resourceMessageTemplate() *common.Resource {
-	schema := &schema.Resource{
-		Description: `
+	return common.NewResource(
+		common.CategoryAlerting,
+		resourceMessageTemplateName,
+		resourceMessageTemplateID,
+		&messageTemplateResource{},
+	).WithLister(listerFunctionOrgResource(listMessageTemplate))
+}
+
+type messageTemplateModel struct {
+	ID                types.String `tfsdk:"id"`
+	OrgID             types.String `tfsdk:"org_id"`
+	Name              types.String `tfsdk:"name"`
+	Template          types.String `tfsdk:"template"`
+	DisableProvenance types.Bool   `tfsdk:"disable_provenance"`
+}
+
+type messageTemplateResource struct {
+	basePluginFrameworkResource
+}
+
+func (r *messageTemplateResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = resourceMessageTemplateName
+}
+
+func (r *messageTemplateResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: `
 Manages Grafana Alerting notification template groups, including notification templates.
 
 * [Official documentation](https://grafana.com/docs/grafana/latest/alerting/set-up/provision-alerting-resources/terraform-provisioning/)
@@ -26,47 +67,268 @@ Manages Grafana Alerting notification template groups, including notification te
 
 This resource requires Grafana 9.1.0 or later.
 `,
-		CreateContext: common.WithAlertingMutex[schema.CreateContextFunc](putMessageTemplate),
-		ReadContext:   readMessageTemplate,
-		UpdateContext: common.WithAlertingMutex[schema.UpdateContextFunc](putMessageTemplate),
-		DeleteContext: common.WithAlertingMutex[schema.DeleteContextFunc](deleteMessageTemplate),
-		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
-		},
-
-		SchemaVersion: 0,
-		Schema: map[string]*schema.Schema{
-			"org_id": orgIDAttribute(),
-			"name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				ForceNew:    true,
-				Description: "The name of the notification template group.",
-			},
-			"template": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "The content of the notification template group.",
-				StateFunc: func(v any) string {
-					return strings.TrimSpace(v.(string))
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:    true,
+				Description: "The ID of this resource.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
 				},
 			},
-			"disable_provenance": {
-				Type:        schema.TypeBool,
+			"org_id": schema.StringAttribute{
 				Optional:    true,
-				Default:     false,
-				ForceNew:    true, // TODO: The API doesn't return provenance, so we have to force new for now.
-				Description: "Allow modifying the message template from other sources than Terraform or the Grafana API.",
+				Computed:    true,
+				Description: "The Organization ID. If not set, the Org ID defined in the provider block will be used.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+					&orgIDAttributePlanModifier{},
+				},
+			},
+			"name": schema.StringAttribute{
+				Required:    true,
+				Description: "The name of the notification template group.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"template": schema.StringAttribute{
+				Required:    true,
+				Description: "The content of the notification template group.",
+			},
+			"disable_provenance": schema.BoolAttribute{
+				Optional:    true,
+				Computed:    true,
+				Description: "Allow modifying the message template from other sources than Terraform or the Grafana API. Defaults to `false`.",
+				Default:     booldefault.StaticBool(false),
+				PlanModifiers: []planmodifier.Bool{
+					boolplanmodifier.RequiresReplace(),
+				},
 			},
 		},
 	}
+}
 
-	return common.NewLegacySDKResource(
-		common.CategoryAlerting,
-		"grafana_message_template",
-		orgResourceIDString("name"),
-		schema,
-	).WithLister(listerFunctionOrgResource(listMessageTemplate))
+func (r *messageTemplateResource) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
+	// Normalize template to trimmed value so plan matches state when only whitespace differs
+	if req.Plan.Raw.IsNull() {
+		return
+	}
+	var plan messageTemplateModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	if plan.Template.IsNull() || plan.Template.IsUnknown() {
+		return
+	}
+	trimmed := strings.TrimSpace(plan.Template.ValueString())
+	if trimmed != plan.Template.ValueString() {
+		plan.Template = types.StringValue(trimmed)
+		resp.Diagnostics.Append(resp.Plan.Set(ctx, plan)...)
+	}
+}
+
+func (r *messageTemplateResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	readData, diags := r.read(ctx, req.ID)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	if readData == nil {
+		resp.Diagnostics.AddError("Resource not found", "Message template not found")
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, readData)...)
+}
+
+func (r *messageTemplateResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan messageTemplateModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	client, orgID, err := r.clientFromNewOrgResource(plan.OrgID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to get client", err.Error())
+		return
+	}
+
+	name := plan.Name.ValueString()
+	content := strings.TrimSpace(plan.Template.ValueString())
+	disableProvenance := plan.DisableProvenance.ValueBool()
+
+	var putErr error
+	r.commonClient.WithAlertingLock(func() {
+		putErr = retry.RetryContext(ctx, 2*time.Minute, func() *retry.RetryError {
+			params := provisioning.NewPutTemplateParams().
+				WithName(name).
+				WithBody(&models.NotificationTemplateContent{
+					Template: content,
+				})
+			if disableProvenance {
+				params.SetXDisableProvenance(&provenanceDisabled)
+			}
+			if _, err := client.Provisioning.PutTemplate(params); err != nil {
+				if err.(runtime.ClientResponseStatus).IsCode(500) {
+					return retry.RetryableError(err)
+				}
+				return retry.NonRetryableError(err)
+			}
+			return nil
+		})
+	})
+	if putErr != nil {
+		resp.Diagnostics.AddError("Failed to create message template", putErr.Error())
+		return
+	}
+
+	plan.ID = types.StringValue(MakeOrgResourceID(orgID, name))
+	readData, diags := r.read(ctx, plan.ID.ValueString())
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, readData)...)
+}
+
+func (r *messageTemplateResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state messageTemplateModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	readData, diags := r.read(ctx, state.ID.ValueString())
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	if readData == nil {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, readData)...)
+}
+
+func (r *messageTemplateResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan messageTemplateModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	client, orgID, split, err := r.clientFromExistingOrgResource(resourceMessageTemplateID, plan.ID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to parse resource ID", err.Error())
+		return
+	}
+	if len(split) == 0 {
+		resp.Diagnostics.AddError("Invalid resource ID", "Resource ID has no name part")
+		return
+	}
+	name := split[0].(string)
+	content := strings.TrimSpace(plan.Template.ValueString())
+	disableProvenance := plan.DisableProvenance.ValueBool()
+
+	var putErr error
+	r.commonClient.WithAlertingLock(func() {
+		putErr = retry.RetryContext(ctx, 2*time.Minute, func() *retry.RetryError {
+			params := provisioning.NewPutTemplateParams().
+				WithName(name).
+				WithBody(&models.NotificationTemplateContent{
+					Template: content,
+				})
+			if disableProvenance {
+				params.SetXDisableProvenance(&provenanceDisabled)
+			}
+			if _, err := client.Provisioning.PutTemplate(params); err != nil {
+				if err.(runtime.ClientResponseStatus).IsCode(500) {
+					return retry.RetryableError(err)
+				}
+				return retry.NonRetryableError(err)
+			}
+			return nil
+		})
+	})
+	if putErr != nil {
+		resp.Diagnostics.AddError("Failed to update message template", putErr.Error())
+		return
+	}
+
+	readData, diags := r.read(ctx, MakeOrgResourceID(orgID, name))
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	resp.Diagnostics.Append(resp.State.Set(ctx, readData)...)
+}
+
+func (r *messageTemplateResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state messageTemplateModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	client, _, split, err := r.clientFromExistingOrgResource(resourceMessageTemplateID, state.ID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError("Failed to parse resource ID", err.Error())
+		return
+	}
+	if len(split) == 0 {
+		resp.Diagnostics.AddError("Invalid resource ID", "Resource ID has no name part")
+		return
+	}
+	name := split[0].(string)
+
+	var deleteErr error
+	r.commonClient.WithAlertingLock(func() {
+		params := provisioning.NewDeleteTemplateParams().WithName(name)
+		_, deleteErr = client.Provisioning.DeleteTemplate(params)
+	})
+	if deleteErr != nil && !common.IsNotFoundError(deleteErr) {
+		resp.Diagnostics.AddError("Failed to delete message template", deleteErr.Error())
+	}
+}
+
+func (r *messageTemplateResource) read(ctx context.Context, id string) (*messageTemplateModel, diag.Diagnostics) {
+	var diags diag.Diagnostics
+	client, orgID, split, err := r.clientFromExistingOrgResource(resourceMessageTemplateID, id)
+	if err != nil {
+		diags.AddError("Failed to parse resource ID", err.Error())
+		return nil, diags
+	}
+	if len(split) == 0 {
+		diags.AddError("Invalid resource ID", "Resource ID has no name part")
+		return nil, diags
+	}
+	name := split[0].(string)
+
+	var tmpl *models.NotificationTemplate
+	r.commonClient.WithAlertingLock(func() {
+		resp, getErr := client.Provisioning.GetTemplate(name)
+		if getErr != nil {
+			if common.IsNotFoundError(getErr) {
+				return
+			}
+			diags.AddError("Failed to read message template", getErr.Error())
+			return
+		}
+		tmpl = resp.Payload
+	})
+	if diags.HasError() || tmpl == nil {
+		return nil, diags
+	}
+
+	return &messageTemplateModel{
+		ID:                types.StringValue(MakeOrgResourceID(orgID, tmpl.Name)),
+		OrgID:             types.StringValue(strconv.FormatInt(orgID, 10)),
+		Name:              types.StringValue(tmpl.Name),
+		Template:          types.StringValue(strings.TrimSpace(tmpl.Template)),
+		DisableProvenance: types.BoolValue(false), // API does not return provenance
+	}, diags
 }
 
 func listMessageTemplate(ctx context.Context, client *goapi.GrafanaHTTPAPI, orgID int64) ([]string, error) {
@@ -91,62 +353,4 @@ func listMessageTemplate(ctx context.Context, client *goapi.GrafanaHTTPAPI, orgI
 	}
 
 	return ids, nil
-}
-
-func readMessageTemplate(ctx context.Context, data *schema.ResourceData, meta any) diag.Diagnostics {
-	client, orgID, name := OAPIClientFromExistingOrgResource(meta, data.Id())
-
-	resp, err := client.Provisioning.GetTemplate(name)
-	if err, shouldReturn := common.CheckReadError("message template", data, err); shouldReturn {
-		return err
-	}
-	tmpl := resp.Payload
-
-	data.Set("org_id", strconv.FormatInt(orgID, 10))
-	data.Set("name", tmpl.Name)
-	data.Set("template", tmpl.Template)
-
-	return nil
-}
-
-func putMessageTemplate(ctx context.Context, data *schema.ResourceData, meta any) diag.Diagnostics {
-	client, orgID := OAPIClientFromNewOrgResource(meta, data)
-
-	name := data.Get("name").(string)
-	content := data.Get("template").(string)
-
-	// Retry if the API returns 500 because it may be that the alertmanager is not ready in the org yet.
-	// The alertmanager is provisioned asynchronously when the org is created.
-	err := retry.RetryContext(ctx, 2*time.Minute, func() *retry.RetryError {
-		params := provisioning.NewPutTemplateParams().
-			WithName(name).
-			WithBody(&models.NotificationTemplateContent{
-				Template: content,
-			})
-		if v, ok := data.GetOk("disable_provenance"); ok && v.(bool) {
-			params.SetXDisableProvenance(&provenanceDisabled)
-		}
-		if _, err := client.Provisioning.PutTemplate(params); err != nil {
-			if err.(runtime.ClientResponseStatus).IsCode(500) {
-				return retry.RetryableError(err)
-			}
-			return retry.NonRetryableError(err)
-		}
-		return nil
-	})
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	data.SetId(MakeOrgResourceID(orgID, name))
-	return readMessageTemplate(ctx, data, meta)
-}
-
-func deleteMessageTemplate(ctx context.Context, data *schema.ResourceData, meta any) diag.Diagnostics {
-	client, _, name := OAPIClientFromExistingOrgResource(meta, data.Id())
-
-	params := provisioning.NewDeleteTemplateParams().WithName(name)
-	_, err := client.Provisioning.DeleteTemplate(params)
-	diag, _ := common.CheckReadError("message template", data, err)
-	return diag
 }

internal/common/client.go
@@ -65,6 +65,13 @@ func WithAlertingMutex[T schema.CreateContextFunc | schema.ReadContextFunc | sch
 	}
 }
 
+// WithAlertingLock runs f while holding the alerting mutex. Used by Plugin Framework resources that need to serialize alerting API calls.
+func (c *Client) WithAlertingLock(f func()) {
+	c.alertingMutex.Lock()
+	defer c.alertingMutex.Unlock()
+	f()
+}
+
 // WithFolderMutex is a helper function that wraps a CRUD Terraform function with a mutex.
 func WithFolderMutex[T schema.CreateContextFunc | schema.ReadContextFunc | schema.UpdateContextFunc | schema.DeleteContextFunc](f T) T {
 	return func(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {

internal/resources/grafana/common_plugin_framework.go
@@ -69,8 +69,9 @@ func (r *basePluginFrameworkDataSource) clientFromNewOrgResource(orgIDStr string
 }
 
 type basePluginFrameworkResource struct {
-	client *goapi.GrafanaHTTPAPI
-	config *goapi.TransportConfig
+	client       *goapi.GrafanaHTTPAPI
+	config       *goapi.TransportConfig
+	commonClient *common.Client
 }
 
 func (r *basePluginFrameworkResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
@@ -101,6 +102,7 @@ func (r *basePluginFrameworkResource) Configure(ctx context.Context, req resourc
 
 	r.client = client.GrafanaAPI
 	r.config = client.GrafanaAPIConfig
+	r.commonClient = client
 }
 
 // clientFromExistingOrgResource creates a client from the ID of an org-scoped resource

commit 29b26f15e94a654dbab7915f3eff9b4ce848303d

fix(alerting): invalid plan on creating template with whitespace (#2771)
Fixes bug introduced in
https://github.com/grafana/terraform-provider-grafana/pull/2567 where
templates with leading/trailing whitespace (e.g. from heredoc) would
work fine assuming an existing state (during `update`), but would cause
`Provider produced invalid plan` on initial create.

Ex:

```
resource "grafana_message_template" "heredoc_template" {
  name     = "Heredoc Notification Template Group"
  template = <<-EOT
{{define "custom.message" }}
 template content
{{ end }}
EOT
}
```

This was due to `ModifyPlan` breaking [data
consistency](https://github.com/hashicorp/terraform/blob/main/docs/resource-instance-change-lifecycle.md#planresourcechange)
guarantees when trimming whitespace. Plan values for a configured
attribute must be equal to the prior state or the config value.

Since we normalize the templates before sending them to the API, we need
to create a custom type to define a semantic equals that ignores
leading/trailing whitespace.

examples/resources/grafana_message_template/_acc_template_heredoc.tf
@@ -1,0 +1,8 @@
+resource "grafana_message_template" "heredoc_template" {
+  name     = "Heredoc Notification Template Group"
+  template = <<-EOT
+{{define "custom.message" }}
+ template content
+{{ end }}
+EOT
+}

internal/resources/grafana/resource_alerting_message_template.go
@@ -3,7 +3,6 @@ package grafana
 import (
 	"context"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/go-openapi/runtime"
@@ -42,11 +41,11 @@ func resourceMessageTemplate() *common.Resource {
 }
 
 type messageTemplateModel struct {
-	ID                types.String `tfsdk:"id"`
-	OrgID             types.String `tfsdk:"org_id"`
-	Name              types.String `tfsdk:"name"`
-	Template          types.String `tfsdk:"template"`
-	DisableProvenance types.Bool   `tfsdk:"disable_provenance"`
+	ID                types.String  `tfsdk:"id"`
+	OrgID             types.String  `tfsdk:"org_id"`
+	Name              types.String  `tfsdk:"name"`
+	Template          templateValue `tfsdk:"template"`
+	DisableProvenance types.Bool    `tfsdk:"disable_provenance"`
 }
 
 type messageTemplateResource struct {
@@ -94,6 +93,7 @@ This resource requires Grafana 9.1.0 or later.
 			"template": schema.StringAttribute{
 				Required:    true,
 				Description: "The content of the notification template group.",
+				CustomType:  templateType{},
 			},
 			"disable_provenance": schema.BoolAttribute{
 				Optional:    true,
@@ -109,23 +109,29 @@ This resource requires Grafana 9.1.0 or later.
 }
 
 func (r *messageTemplateResource) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
-	// Normalize template to trimmed value so plan matches state when only whitespace differs
-	if req.Plan.Raw.IsNull() {
+	if req.State.Raw.IsNull() || req.Plan.Raw.IsNull() {
 		return
 	}
-	var plan messageTemplateModel
-	diags := req.Plan.Get(ctx, &plan)
-	resp.Diagnostics.Append(diags...)
+	var plan, state messageTemplateModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 	if plan.Template.IsNull() || plan.Template.IsUnknown() {
 		return
 	}
-	trimmed := strings.TrimSpace(plan.Template.ValueString())
-	if trimmed != plan.Template.ValueString() {
-		plan.Template = types.StringValue(trimmed)
-		resp.Diagnostics.Append(resp.Plan.Set(ctx, plan)...)
+
+	// If planned value is semantically equal to the current value, do not update the plan. This is to avoid diffs
+	// due to trailing whitespace, for example from heredoc strings.
+	semEqual, diags := state.Template.StringSemanticEquals(ctx, plan.Template)
+	resp.Diagnostics.Append(diags...)
+	if diags.HasError() {
+		return
+	}
+	if semEqual {
+		plan.Template = state.Template
+		resp.Diagnostics.Append(resp.Plan.Set(ctx, &plan)...)
 	}
 }
 
@@ -156,7 +162,7 @@ func (r *messageTemplateResource) Create(ctx context.Context, req resource.Creat
 	}
 
 	name := plan.Name.ValueString()
-	content := strings.TrimSpace(plan.Template.ValueString())
+	content := plan.Template.Normalized()
 	disableProvenance := plan.DisableProvenance.ValueBool()
 
 	var putErr error
@@ -229,7 +235,7 @@ func (r *messageTemplateResource) Update(ctx context.Context, req resource.Updat
 		return
 	}
 	name := split[0].(string)
-	content := strings.TrimSpace(plan.Template.ValueString())
+	content := plan.Template.Normalized()
 	disableProvenance := plan.DisableProvenance.ValueBool()
 
 	var putErr error
@@ -326,7 +332,7 @@ func (r *messageTemplateResource) read(ctx context.Context, id string) (*message
 		ID:                types.StringValue(MakeOrgResourceID(orgID, tmpl.Name)),
 		OrgID:             types.StringValue(strconv.FormatInt(orgID, 10)),
 		Name:              types.StringValue(tmpl.Name),
-		Template:          types.StringValue(strings.TrimSpace(tmpl.Template)),
+		Template:          templateValue{StringValue: types.StringValue(tmpl.Template)},
 		DisableProvenance: types.BoolValue(false), // API does not return provenance
 	}, diags
 }

internal/resources/grafana/resource_alerting_message_template_type.go
@@ -1,0 +1,58 @@
+package grafana
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+var _ basetypes.StringTypable = templateType{}
+
+type templateType struct {
+	basetypes.StringType
+}
+
+func (t templateType) Equal(o attr.Type) bool {
+	other, ok := o.(templateType)
+	if !ok {
+		return false
+	}
+	return t.StringType.Equal(other.StringType)
+}
+
+func (t templateType) String() string {
+	return "templateType"
+}
+
+func (t templateType) ValueFromString(ctx context.Context, in basetypes.StringValue) (basetypes.StringValuable, diag.Diagnostics) {
+	return templateValue{
+		StringValue: in,
+	}, nil
+}
+
+func (t templateType) ValueFromTerraform(ctx context.Context, in tftypes.Value) (attr.Value, error) {
+	attrValue, err := t.StringType.ValueFromTerraform(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+
+	stringValue, ok := attrValue.(basetypes.StringValue)
+	if !ok {
+		return nil, fmt.Errorf("unexpected value type of %T", attrValue)
+	}
+
+	stringValuable, diags := t.ValueFromString(ctx, stringValue)
+	if diags.HasError() {
+		return nil, fmt.Errorf("unexpected error converting StringValue to StringValuable: %v", diags)
+	}
+
+	return stringValuable, nil
+}
+
+func (t templateType) ValueType(context.Context) attr.Value {
+	return templateValue{}
+}

internal/resources/grafana/resource_alerting_message_template_value.go
@@ -1,0 +1,47 @@
+package grafana
+
+import (
+	"context"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+)
+
+var _ basetypes.StringValuableWithSemanticEquals = templateValue{}
+
+type templateValue struct {
+	basetypes.StringValue
+}
+
+func (v templateValue) Normalized() string {
+	return strings.TrimSpace(v.StringValue.ValueString())
+}
+
+func (v templateValue) Equal(o attr.Value) bool {
+	other, ok := o.(templateValue)
+	if !ok {
+		return false
+	}
+	return v.StringValue.Equal(other.StringValue)
+}
+
+func (v templateValue) Type(context.Context) attr.Type {
+	return templateType{}
+}
+
+func (v templateValue) StringSemanticEquals(_ context.Context, newValuable basetypes.StringValuable) (bool, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	other, ok := newValuable.(templateValue)
+	if !ok {
+		diags.AddError(
+			"Semantic Equality Check Error",
+			"unexpected value type for template semantic equality",
+		)
+		return false, diags
+	}
+
+	return v.Normalized() == other.Normalized(), diags
+}

internal/resources/grafana/resource_alerting_message_template_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/grafana/terraform-provider-grafana/v4/internal/testutils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccMessageTemplate_basic(t *testing.T) {
@@ -73,6 +74,81 @@ EOT`,
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "name", "A Different Template"),
 					resource.TestCheckResourceAttr("grafana_message_template.my_template", "template", "{{define \"custom.message\" }}\n template content\n{{ end }}"),
 					alertingMessageTemplateCheckExists.destroyed(&models.NotificationTemplate{Name: "My Notification Template Group"}, nil),
+				),
+			},
+		},
+	})
+}
+
+func TestAccMessageTemplate_heredoc(t *testing.T) {
+	// Intended to prevent regression on whitespace trimming for newly created templates using heredoc.
+	testutils.CheckOSSTestsEnabled(t, ">=9.0.0")
+
+	var tmpl models.NotificationTemplate
+
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV5ProviderFactories: testutils.ProtoV5ProviderFactories,
+		// Implicitly tests deletion.
+		CheckDestroy: alertingMessageTemplateCheckExists.destroyed(&tmpl, nil),
+		Steps: []resource.TestStep{
+			// Test creation.
+			{
+				Config: testutils.TestAccExample(t, "resources/grafana_message_template/_acc_template_heredoc.tf"),
+				Check: resource.ComposeTestCheckFunc(
+					alertingMessageTemplateCheckExists.exists("grafana_message_template.heredoc_template", &tmpl),
+					resource.TestCheckResourceAttr("grafana_message_template.heredoc_template", "name", "Heredoc Notification Template Group"),
+					resource.TestCheckResourceAttr("grafana_message_template.heredoc_template", "template", "{{define \"custom.message\" }}\n template content\n{{ end }}\n"),
+					testutils.CheckLister("grafana_message_template.heredoc_template"),
+				),
+			},
+			// Test import.
+			{
+				ResourceName: "grafana_message_template.heredoc_template",
+				ImportState:  true,
+				//ImportStateVerify: true,
+				ImportStateCheck: func(is []*terraform.InstanceState) error {
+					expected := "{{define \"custom.message\" }}\n template content\n{{ end }}"
+					if is[0].Attributes["template"] != expected {
+						return fmt.Errorf("expected template to be %q, got %q", expected, is[0].Attributes["template"])
+					}
+					return nil
+				},
+				ImportStateVerifyIgnore: []string{"disable_provenance"},
+			},
+			// Test update without heredoc template doesn't change
+			{
+				Config: `
+resource "grafana_message_template" "heredoc_template" {
+  name     = "Heredoc Notification Template Group"
+  template = "{{define \"custom.message\" }}\n template content\n{{ end }}"
+}`,
+				Check: resource.ComposeTestCheckFunc(
+					alertingMessageTemplateCheckExists.exists("grafana_message_template.heredoc_template", &tmpl),
+					resource.TestCheckResourceAttr("grafana_message_template.heredoc_template", "name", "Heredoc Notification Template Group"),
+					resource.TestCheckResourceAttr("grafana_message_template.heredoc_template", "template", "{{define \"custom.message\" }}\n template content\n{{ end }}\n"),
+				),
+			},
+			// Test update content.
+			{
+				Config: testutils.TestAccExampleWithReplace(t, "resources/grafana_message_template/_acc_template_heredoc.tf", map[string]string{
+					"template content": "different content",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					alertingMessageTemplateCheckExists.exists("grafana_message_template.heredoc_template", &tmpl),
+					resource.TestCheckResourceAttr("grafana_message_template.heredoc_template", "name", "Heredoc Notification Template Group"),
+					resource.TestCheckResourceAttr("grafana_message_template.heredoc_template", "template", "{{define \"custom.message\" }}\n different content\n{{ end }}\n"),
+				),
+			},
+			// Test rename.
+			{
+				Config: testutils.TestAccExampleWithReplace(t, "resources/grafana_message_template/_acc_template_heredoc.tf", map[string]string{
+					"Heredoc Notification Template Group": "A Different Template",
+				}),
+				Check: resource.ComposeTestCheckFunc(
+					alertingMessageTemplateCheckExists.exists("grafana_message_template.heredoc_template", &tmpl),
+					resource.TestCheckResourceAttr("grafana_message_template.heredoc_template", "name", "A Different Template"),
+					resource.TestCheckResourceAttr("grafana_message_template.heredoc_template", "template", "{{define \"custom.message\" }}\n template content\n{{ end }}\n"),
+					alertingMessageTemplateCheckExists.destroyed(&models.NotificationTemplate{Name: "Heredoc Notification Template Group"}, nil),
 				),
 			},
 		},